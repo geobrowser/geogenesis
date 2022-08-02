dcde9cb47ba9d974b4aee15d110d497247e81990
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-08-02 15:52:15 -0600

    Flex with some sign-in/out animations

---
+++ packages/frontend/modules/ui/animate.tsx
@@ -0,0 +1,33 @@
+import { motion, AnimationProps } from 'framer-motion';
+
+type AnimationType = 'fade' | 'slide-up';
+
+interface AnimateProps {
+    children: React.ReactNode;
+    animation: AnimationType;
+    className?: string;
+    delay?: number;
+}
+
+const animationMap: Record<AnimationType, AnimationProps> = {
+    fade: {
+        initial: { opacity: 0 },
+        animate: { opacity: 1 },
+        exit: { opacity: 0 },
+    },
+    'slide-up': {
+        initial: { opacity: 0, y: 30 },
+        animate: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: 30 },
+    },
+};
+
+export function Animate(props: AnimateProps) {
+    const animationProps = animationMap[props.animation];
+
+    return (
+        <motion.div className={props.className} {...animationProps}>
+            {props.children}
+        </motion.div>
+    );
+}
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,16 +1,24 @@
 import { ConnectButton, useConnectModal } from '@rainbow-me/rainbowkit';
+import { AnimatePresence } from 'framer-motion';
 import type { NextPage } from 'next';
 import { useAccount } from 'wagmi';
+import { Animate } from '~/modules/ui/animate';
 
 const Home: NextPage = () => {
     const { isConnected } = useAccount();
 
-    return !isConnected ? (
-        <div className='space-y-4'>
-            <h1 className='text-lg font-medium'>Sign in to your wallet to start publishing content in Geo</h1>
-        </div>
-    ) : (
-        <p>Do something cool...</p>
+    return (
+        <AnimatePresence exitBeforeEnter>
+            {!isConnected ? (
+                <Animate key='Sign in' className='space-y-4' animation='fade'>
+                    <h1 className='text-lg font-medium'>Sign in to your wallet to start publishing content in Geo</h1>
+                </Animate>
+            ) : (
+                <Animate key='Signed in' animation='fade'>
+                    <p>Do something cool...</p>
+                </Animate>
+            )}
+        </AnimatePresence>
     );
 };
 
