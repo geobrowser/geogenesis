5d8c0a3990618ae9b338a2e7ec7159c4b486a185
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-08-03 10:32:03 -0600

    Move publishing functionality out of component into the content service

--- packages/frontend/modules/editor/content.ts
+++ packages/frontend/modules/editor/content.ts
@@ -1,19 +1,59 @@
-import { makeAutoObservable } from 'mobx';
+import { addresses, GeoDocument__factory } from '@geogenesis/contracts'
+import { makeAutoObservable } from 'mobx'
+import { ContractTransaction, Event, Signer } from 'ethers'
+import { Chain } from 'wagmi'
 
-export class Content {
-    content: string = '';
-
-    constructor() {
-        makeAutoObservable(this);
-    }
-
-    setContent(content: string) {
-        this.content = content;
-    }
-
-    publish() {
-        console.log(this.content);
-    }
+function isSupportedChain(chainId: string): chainId is keyof typeof addresses {
+  return chainId in addresses
 }
 
-export const contentService = new Content();
+async function findEvent(
+  tx: ContractTransaction,
+  name: string
+): Promise<Event> {
+  const receipt = await tx.wait()
+  const event = receipt.events?.find((event) => event.event === name)
+  if (!event) throw new Error(`Event '${name}' wasn't emitted`)
+  return event
+}
+
+export class Content {
+  content: string = ''
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  setContent(content: string) {
+    this.content = content
+  }
+
+  async publish(signer: Signer | undefined, chain: Chain | undefined) {
+    if (!signer || !chain) return
+
+    const chainId = String(chain.id)
+
+    if (!isSupportedChain(chainId)) return
+
+    const contractAddress = addresses[chainId].GeoDocument.address
+
+    const contract = GeoDocument__factory.connect(contractAddress, signer)
+
+    console.log('Minting...')
+
+    const mintTx = await contract.mint({
+      contentHash:
+        'bafkreibrl5n5w5wqpdcdxcwaazheualemevr7ttxzbutiw74stdvrfhn2m',
+      nextVersionId: 0,
+      previousVersionId: 0,
+    })
+
+    const transferEvent = await findEvent(mintTx, 'Transfer')
+
+    if (transferEvent.args) {
+      console.log(`Successfully minted token ${transferEvent.args.tokenId}`)
+    }
+  }
+}
+
+export const contentService = new Content()
