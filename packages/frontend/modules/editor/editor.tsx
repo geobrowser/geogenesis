89c9b83bf9087b0a95b6870b44eba040a730a6b7
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-08-03 10:37:46 -0600

    Doesn't need to be an observer

--- packages/frontend/modules/editor/editor.tsx
+++ packages/frontend/modules/editor/editor.tsx
@@ -1,25 +1,26 @@
-import { useEditor, EditorContent } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
-import { observer } from 'mobx-react-lite';
-import { Content } from './content';
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { memo } from 'react'
+import { Content } from './content'
 
 interface Props {
-    contentService: Content;
+  contentService: Content
 }
 
 // Don't want to rerender the editor over and over
-export const Editor = observer(({ contentService }: Props) => {
-    const editor = useEditor({
-        extensions: [StarterKit],
-        content: '<p>In the hole in a ground there lived a hobbit.</p>',
-        editorProps: {
-            attributes: {
-                placeholder: 'In a hole in the ground there lived a hobbit...',
-                class: 'prose prose-sm sm:prose prose-stone lg:prose-md xl:prose-lg mx-auto min-h-full focus:outline-none font-mono focus:ring rounded px-4 py-2',
-            },
-        },
-        onUpdate: ({ editor }) => contentService.setContent(editor.getHTML()),
-    });
+export const Editor = memo(function Editor({ contentService }: Props) {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: '<p>In the hole in a ground there lived a hobbit.</p>',
+    editorProps: {
+      attributes: {
+        placeholder: 'In a hole in the ground there lived a hobbit...',
+        class:
+          'prose prose-sm sm:prose prose-stone lg:prose-md xl:prose-lg mx-auto min-h-full focus:outline-none font-mono focus:ring rounded px-4 py-2',
+      },
+    },
+    onUpdate: ({ editor }) => contentService.setContent(editor.getHTML()),
+  })
 
-    return <EditorContent editor={editor} />;
-});
+  return <EditorContent editor={editor} />
+})
