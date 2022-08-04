478590db63d2c4321095fad61cec77c9ee065852
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-08-04 13:15:52 -0600

    Use hook for reading the publishService instance

--- packages/frontend/modules/ui/editor.tsx
+++ packages/frontend/modules/ui/editor.tsx
@@ -1,7 +1,9 @@
-import { publishService } from '~/modules/api/publish-service'
+import { usePublishService } from '~/modules/api/publish-service'
 import { Editor as BaseEditor } from '~/modules/editor/editor'
 
 export function Editor() {
+  const publishService = usePublishService()
+
   return (
     <div className="rounded-xl px-10 py-6">
       <BaseEditor publishService={publishService} />
