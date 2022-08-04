6703b1252c13b3568da4a3db7d8f816685d23be6
Byron <26263630+baiirun@users.noreply.github.com>
2022-08-04 13:27:16 -0600

    Migrate all publish state functionality to a new PublishService (#10)
    * Migrate all publish state functionality to a new PublishService
    
    * Use hook for reading the publishService instance

--- packages/frontend/modules/ui/editor.tsx
+++ packages/frontend/modules/ui/editor.tsx
@@ -1,10 +1,12 @@
-import { contentService } from '~/modules/editor/content'
+import { usePublishService } from '~/modules/api/publish-service'
 import { Editor as BaseEditor } from '~/modules/editor/editor'
 
 export function Editor() {
+  const publishService = usePublishService()
+
   return (
     <div className="rounded-xl px-10 py-6">
-      <BaseEditor contentService={contentService} />
+      <BaseEditor publishService={publishService} />
     </div>
   )
 }
--- packages/frontend/modules/ui/publish.tsx
+++ packages/frontend/modules/ui/publish.tsx
@@ -1,19 +1,18 @@
 import { motion } from 'framer-motion'
 import { useNetwork, useSigner } from 'wagmi'
-import { useMachine } from '@xstate/react'
-// This can come through context or something dependency injected as well
-import { contentService, publishMachine } from '~/modules/editor/content'
+import { usePublishService } from '~/modules/api/publish-service'
 import { useRouter } from 'next/router'
+import { observer } from 'mobx-react-lite'
 
-export function PublishButton() {
+export const PublishButton = observer(() => {
   const router = useRouter()
   const { chain } = useNetwork()
   const { data: signer } = useSigner()
-  const [current, send] = useMachine(publishMachine)
+  const publishService = usePublishService()
 
   const onPublish = async () => {
-    // @ts-expect-error signer type mismatch
-    const tokenId = await contentService.publish(signer, chain, send)
+    // @ts-expect-error type mismatch for signer
+    const tokenId = await publishService.publish(signer, chain)
     router.replace(`/token/${tokenId}`)
   }
 
@@ -23,9 +22,9 @@ export function PublishButton() {
       className="rounded-xl px-4 bg-blue-700 text-slate-100 font-bold shadow-lg"
       onClick={onPublish}
     >
-      {current.matches('idle') && 'Publish'}
-      {current.matches('uploading') && 'Uploading...'}
-      {current.matches('minting') && 'Minting...'}
+      {publishService.publishState === 'idle' && 'Publish'}
+      {publishService.publishState === 'uploading' && 'Uploading...'}
+      {publishService.publishState === 'minting' && 'Minting...'}
     </motion.button>
   )
-}
+})
