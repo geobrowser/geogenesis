cda24a2b0c1253c987a423e7236fd6a414399c42
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-08-08 12:05:39 -0600

    Make icons links

--- packages/frontend/modules/ui/navbar.tsx
+++ packages/frontend/modules/ui/navbar.tsx
@@ -1,21 +1,33 @@
+import Link from 'next/link'
 import { Avatar } from './icons/avatar'
 import { Editor } from './icons/editor'
+import { GeoLarge } from './icons/geo-large'
 import { GeoSmall } from './icons/geo-small'
 import { Stack } from './icons/stack'
-import { Star } from './icons/star'
 
 export function Navbar() {
   return (
     // Stacking context needs to be higher than the action bar
-    <nav className="bg-geo-white-100 shadow-lg absolute left-0 h-screen w-24 z-20 flex flex-col items-center justify-center">
-      {/* <h2>Logo</h2> */}
+    <nav className="bg-geo-white-100 shadow-lg absolute left-0 h-screen w-24 z-20 flex flex-col items-center justify-between">
+      <GeoLarge />
       <div className="flex flex-col items-center">
-        <Editor />
-        <Stack />
+        <Link href="/new">
+          <a>
+            <Editor />
+          </a>
+        </Link>
+        <Link href="/token/3">
+          <a>
+            <Stack />
+          </a>
+        </Link>
         <GeoSmall />
         <Avatar />
-        <Star />
+        {/* <Star /> */}
       </div>
+
+      {/* TODO: Byron fix position HACK */}
+      <div />
     </nav>
   )
 }
