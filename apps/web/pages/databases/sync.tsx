78c50c8750de97605cc9395cbf97cc658984c208
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-09-09 10:09:03 -0600

    Remove unused styled component

--- apps/web/pages/databases/sync.tsx
+++ apps/web/pages/databases/sync.tsx
@@ -1,8 +1,5 @@
-import styled from '@emotion/styled';
 import { Facts, MockApi, useSharedObservable } from '@geogenesis/database';
 import { Spacer } from '~/modules/design-system/spacer';
-
-const FactLayout = styled.div``;
 
 // Would probably be dependency injected with Context in the real implementation
 const factsStore = new Facts(new MockApi());
@@ -21,13 +18,13 @@ export default function SyncExample() {
   return (
     <div>
       {snapshot.map(fact => (
-        <FactLayout key={fact.id}>
+        <div key={fact.id}>
           <p>id: {fact.id}</p>
           <p>entityId: {fact.entityId}</p>
           <p>attribute: {fact.attribute}</p>
           <p>value: {fact.value}</p>
           <Spacer height={8} />
-        </FactLayout>
+        </div>
       ))}
 
       <Spacer height={20} />
