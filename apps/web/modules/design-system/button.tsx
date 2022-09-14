26883819088f8e05368ae55716ea80ca764e83b2
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-09-14 11:03:52 -0600

    Fix padding and add optional icon to button

--- apps/web/modules/design-system/button.tsx
+++ apps/web/modules/design-system/button.tsx
@@ -4,10 +4,10 @@ import { typography } from './theme/typography';
 
 const StyledButton = styled.button<Pick<Props, 'variant'>>(props => ({
   ...typography.button,
-  // padding: '12px 9.5px', // TODO: Spacing tokens
   backgroundColor: props.variant === 'primary' ? colors.ctaPrimary : colors.white,
   color: props.variant === 'primary' ? colors.white : colors.text,
   border: props.variant === 'primary' ? 'none' : `1px solid ${colors['grey-02']}`,
+  padding: '12px 9.5px', // TODO: Spacing tokens
   borderRadius: '6px', // TODO: Spacing tokens
   cursor: 'pointer',
 }));
@@ -22,6 +22,7 @@ interface Props {
 export function Button({ children, onClick, icon, variant = 'primary' }: Props) {
   return (
     <StyledButton variant={variant} onClick={onClick}>
+      {icon ? <p>Icon goes here</p> : null}
       {children}
     </StyledButton>
   );
