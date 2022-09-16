ede02f965abce3db0c723039a48164ccd954e3bb
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-09-16 11:49:57 -0600

    Use system tokens for input values

--- apps/web/modules/design-system/input.tsx
+++ apps/web/modules/design-system/input.tsx
@@ -3,18 +3,20 @@ import styled from '@emotion/styled';
 export const Input = styled.input(props => ({
   ...props.theme.typography.input,
   border: `1px solid ${props.theme.colors['grey-02']}`,
-  borderRadius: '6px',
-  padding: '9px 12px',
+  borderRadius: props.theme.radius,
 
-  transition: 'all 200ms ease-in-out',
+  // TODO: Figure out what's going on here. Design padding doesn't match the design system spacing increments.
+  padding: `${props.theme.space * 2 + 0.5}px ${props.theme.space * 2.5}px`,
+
+  transition: '100ms all ease-in-out',
 
   '::placeholder': {
     color: props.theme.colors['grey-03'],
   },
 
-  // ':active': {
-  //   outline: `1px solid ${props.theme.colors.ctaPrimary}`,
-  // },
+  ':hover': {
+    outline: `1px solid ${props.theme.colors.ctaPrimary}`,
+  },
 
   ':focus': {
     outline: `2px solid ${props.theme.colors.ctaPrimary}`,
