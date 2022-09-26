a5255e05517590f4fd4fcb878768493ac6c32f67
Byron Guina <byron.guina@gmail.com>
2022-09-26 13:04:35 -0600

    Rename helper function for loading network triples

--- apps/web/modules/state/triple-store.tsx
+++ apps/web/modules/state/triple-store.tsx
@@ -40,7 +40,7 @@ export class TripleStore {
   };
 
   // TODO: Should this live in the store or should the triples be passed in?
-  getNetworkTriples = async () => {
+  loadNetworkTriples = async () => {
     this.triples$.next(await this.api.getNetworkTriples());
   };
 
