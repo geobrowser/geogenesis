2b353f05b616912faba779cb6ea951218fb9cfe8
Byron <26263630+baiirun@users.noreply.github.com>
2022-09-27 08:47:33 -0600

    Use dynamic import to force CSR for TripleTable (#55)

--- apps/web/pages/triples.tsx
+++ apps/web/pages/triples.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
 import { Log__factory } from '@geogenesis/contracts';
 import debounce from 'lodash.debounce';
+import dynamic from 'next/dynamic';
 import { useState } from 'react';
 import { useSigner } from 'wagmi';
-import { TripleTable } from '~/modules/components/triple-table';
 import { Button } from '~/modules/design-system/button';
 import { Input } from '~/modules/design-system/input';
 import { Spacer } from '~/modules/design-system/spacer';
@@ -14,6 +14,17 @@ import { Network } from '~/modules/services/network';
 import { StorageClient } from '~/modules/services/storage';
 import { useTriples } from '~/modules/state/hook';
 import { TripleStore } from '~/modules/state/triple-store';
+
+// We're dynamically importing the TripleTable so we can disable SSR. There are ocassionally hydration
+// mismatches in dev (maybe prod?) that happen when reloading a page when the table has optimistic data
+// but the server does not have the data yet, e.g., we're waiting for blocks to sync or the transaction
+// did not go through.
+//
+// I _think_ this only happens in dev as Next might be doing SSR/HMR under the hood for static pages,
+// but could be happening in prod. Doing dynamic import for now until we can investigate more.
+const TripleTable = dynamic(() => import('~/modules/components/triple-table'), {
+  ssr: false,
+});
 
 const PageHeader = styled.div({
   display: 'flex',
