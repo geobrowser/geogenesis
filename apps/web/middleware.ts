5227eb67aa5dfd4e300c91c07a5ca083f9955163
Byron <26263630+baiirun@users.noreply.github.com>
2022-10-24 12:14:09 -0600

    Rewrite requests for /blog urls (#83)

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,10 +1,19 @@
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 // only run the rewrite on the root route
 export const config = {
-  matcher: '/',
+  matcher: ['/', '/blog/:path*'],
 };
 
-export function middleware() {
-  return NextResponse.rewrite('https://geo.framer.website/');
+const MARKETING_PAGE_URL = 'https://geo.framer.website/';
+const BLOG_URL = 'https://geo-blog.vercel.app';
+
+export function middleware(request: NextRequest) {
+  const pathname = new URL(request.url).pathname;
+
+  if (pathname === '/') return NextResponse.rewrite(MARKETING_PAGE_URL);
+  if (pathname.startsWith('/blog')) return NextResponse.rewrite(`${BLOG_URL}${pathname}`);
+
+  return NextResponse.next();
 }
