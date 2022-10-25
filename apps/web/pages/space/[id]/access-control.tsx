31e95002f52c31ef3512dcea7eb623e15281de2f
Byron Guina <byron.guina@gmail.com>
2022-10-25 15:25:25 -0600

    Add or revoke admins

--- apps/web/pages/space/[id]/access-control.tsx
+++ apps/web/pages/space/[id]/access-control.tsx
@@ -12,41 +12,34 @@ export default function AccessControl() {
   const { isAdmin } = useAccessControl(spaceId);
   const { data: signer } = useSigner();
 
-  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const onSubmit = (type: 'editor' | 'admin') => async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const address = formData.get('address');
 
     if (signer) {
       const contract = Space__factory.connect(spaceId, signer);
-      const tx = await contract.grantRole(await contract.EDITOR_ROLE(), address as string);
+      const roleToChange = type === 'editor' ? await contract.EDITOR_ROLE() : await contract.ADMIN_ROLE();
+      const tx = await contract.grantRole(roleToChange, address as string);
       await tx.wait();
     }
   };
 
-  const onRevoke = async (address: string) => {
+  const onRevoke = async (address: string, type: 'editor' | 'admin') => {
     if (signer) {
       const contract = Space__factory.connect(spaceId, signer);
-      const tx = await contract.revokeRole(await contract.EDITOR_ROLE(), address);
+      const roleToChange = type === 'editor' ? await contract.EDITOR_ROLE() : await contract.ADMIN_ROLE();
+      const tx = await contract.revokeRole(roleToChange, address);
       await tx.wait();
     }
   };
 
   return isAdmin ? (
     <div>
-      <form onSubmit={onSubmit}>
-        <input name="address" placeholder="Address..." />
-        <button>Add address</button>
+      <form onSubmit={onSubmit('editor')}>
+        <input name="address" placeholder="Editor address..." />
+        <button>Add editor</button>
       </form>
-
-      <h1 style={{ marginTop: 16 }}>Admins</h1>
-      <ul>
-        {store.spaces
-          .find(space => space.id == spaceId)
-          ?.admins.map(editor => (
-            <li key={editor.id}>{editor.id}</li>
-          ))}
-      </ul>
 
       <h1 style={{ marginTop: 16 }}>Editors</h1>
       <ul>
@@ -55,7 +48,29 @@ export default function AccessControl() {
           ?.editors.map(editor => (
             <li key={editor.id}>
               <span>{editor.id}</span>
-              <button onClick={() => onRevoke(editor.id)}>Revoke</button>
+              <button onClick={() => onRevoke(editor.id, 'editor')}>Revoke</button>
+            </li>
+          ))}
+      </ul>
+
+      <hr style={{ width: '100%', borderBottom: '1px solid grey', margin: '32px 0px' }} />
+
+      <h1 style={{ color: 'red' }}>
+        DANGER: ADDING ADMINS GIVES THEM PERMISSIONS TO ADD ADMINS AND EDITORS. THERE BE DRAGONS.
+      </h1>
+      <form onSubmit={onSubmit('admin')}>
+        <input name="address" placeholder="Admin address..." />
+        <button>Add admin</button>
+      </form>
+
+      <h1 style={{ marginTop: 16 }}>Admins</h1>
+      <ul>
+        {store.spaces
+          .find(space => space.id == spaceId)
+          ?.admins.map(admin => (
+            <li key={admin.id}>
+              <span>{admin.id}</span>
+              <button onClick={() => onRevoke(admin.id, 'admin')}>Revoke</button>
             </li>
           ))}
       </ul>
