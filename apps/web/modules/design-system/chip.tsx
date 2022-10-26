35dab53d9c64bb3da3b4a4f5f17a492bcb5d0e6c
Byron Guina <byron.guina@gmail.com>
2022-10-26 13:38:06 -0600

    Change chip to multi-line design

--- apps/web/modules/design-system/chip.tsx
+++ apps/web/modules/design-system/chip.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const Chip = styled.span(props => ({
   ...props.theme.typography.metadataMedium,
-  borderRadius: props.theme.radius * 7,
+  borderRadius: props.theme.radius,
   border: `1px solid ${props.theme.colors.text}`,
   padding: `${props.theme.space}px ${props.theme.space * 2}px`,
   display: 'inline-block',
