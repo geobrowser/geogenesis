8ddd0587b85c4d5b2a6cd4005e5cca2f2e6cc19b
Byron Guina <byron.guina@gmail.com>
2022-11-07 14:38:58 -0700

    Use enum for typography weights

--- apps/web/modules/design-system/theme/typography.ts
+++ apps/web/modules/design-system/theme/typography.ts
@@ -6,6 +6,13 @@ export type TypographyThemeValue = TypographyScale[TypographyTheme];
 export type TypographyName = keyof TypographyScale[TypographyTheme];
 export type TypographyValue = TypographyThemeValue[TypographyName];
 
+const WEIGHTS = {
+  regular: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+} as const;
+
 const baseTypography = {
   light: {
     mainPage: {
@@ -13,100 +20,100 @@ const baseTypography = {
       fontSize: '2.75rem',
       lineHeight: '2.875rem',
       letterSpacing: '0.37px',
-      fontWeight: 600,
+      fontWeight: WEIGHTS.semibold,
     },
     largeTitle: {
       color: colors.light.text,
       fontSize: '2rem',
       lineHeight: '2.5rem',
       letterSpacing: '0.25px',
-      fontWeight: 600,
+      fontWeight: WEIGHTS.semibold,
     },
     mediumTitle: {
       color: colors.light.text,
       fontSize: '1.5rem',
       lineHeight: '1.8125rem',
-      fontWeight: 600,
+      fontWeight: WEIGHTS.semibold,
     },
     smallTitle: {
       color: colors.light.text,
       fontSize: '1.1875rem',
       lineHeight: '1.3125rem',
-      fontWeight: 600,
+      fontWeight: WEIGHTS.semibold,
     },
     body: {
       color: colors.light.text,
       fontSize: '1.1875rem',
       lineHeight: '1.8125rem',
-      fontWeight: 400,
+      fontWeight: WEIGHTS.regular,
     },
     tableCell: {
       color: colors.light.text,
       fontSize: '1.1875rem',
       lineHeight: '1.3125rem',
-      fontWeight: 400,
+      fontWeight: WEIGHTS.regular,
     },
     textLink: {
       color: colors.light.text,
       fontSize: '1.1875rem',
       lineHeight: '1.8125rem',
-      fontWeight: 400,
+      fontWeight: WEIGHTS.regular,
       textDecoration: 'underline',
     },
     quote: {
       color: colors.light.text,
       fontSize: '0.9375rem',
       lineHeight: '1.375rem',
-      fontWeight: 700,
+      fontWeight: WEIGHTS.bold,
     },
     listItem: {
       color: colors.light.text,
       fontSize: '1.125rem',
       lineHeight: '1.8125rem',
-      fontWeight: 400,
+      fontWeight: WEIGHTS.regular,
     },
     button: {
       color: colors.light.text,
       fontSize: '1.0625rem',
       lineHeight: '1.1875rem',
       letterSpacing: '-0.17px',
-      fontWeight: 500,
+      fontWeight: WEIGHTS.medium,
     },
     smallButton: {
       color: colors.light.text,
       fontSize: '0.75rem',
       lineHeight: '0.75rem',
-      fontWeight: 500,
+      fontWeight: WEIGHTS.medium,
     },
     input: {
       color: colors.light.text,
       fontSize: '1.0625rem',
       lineHeight: '1.125rem',
-      fontWeight: 400,
+      fontWeight: WEIGHTS.regular,
     },
     metadata: {
       color: colors.light.text,
       fontSize: '1rem',
       lineHeight: '0.9375rem',
-      fontWeight: 400,
+      fontWeight: WEIGHTS.regular,
     },
     breadcrumb: {
       color: colors.light.text,
       fontSize: '0.875rem',
       lineHeight: '0.9375rem',
-      fontWeight: 500,
+      fontWeight: WEIGHTS.medium,
     },
     footnote: {
       color: colors.light.text,
       fontSize: '0.678rem',
       lineHeight: '0.8125rem',
-      fontWeight: 400,
+      fontWeight: WEIGHTS.regular,
     },
     navlink: {
       color: colors.light.text,
       fontSize: '0.678rem',
       lineHeight: '0.75rem',
-      fontWeight: 400,
+      fontWeight: WEIGHTS.medium,
     },
   },
 };
@@ -116,31 +123,31 @@ export const typography = {
     ...baseTypography.light,
     bodySemibold: {
       ...baseTypography.light.body,
-      fontWeight: 600,
+      fontWeight: WEIGHTS.semibold,
     },
     textLinkSemibold: {
       ...baseTypography.light.textLink,
-      fontWeight: 600,
+      fontWeight: WEIGHTS.semibold,
     },
     listSemibold: {
       ...baseTypography.light.listItem,
-      fontWeight: 600,
+      fontWeight: WEIGHTS.semibold,
     },
     quoteMedium: {
       ...baseTypography.light.quote,
-      fontWeight: 500,
+      fontWeight: WEIGHTS.medium,
     },
     inputMedium: {
       ...baseTypography.light.input,
-      fontWeight: 500,
+      fontWeight: WEIGHTS.medium,
     },
     metadataMedium: {
       ...baseTypography.light.metadata,
-      fontWeight: 500,
+      fontWeight: WEIGHTS.medium,
     },
     footnoteMedium: {
       ...baseTypography.light.footnote,
-      fontWeight: 500,
+      fontWeight: WEIGHTS.medium,
     },
   },
 };
