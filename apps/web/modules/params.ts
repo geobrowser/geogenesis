3a0424696ad8b3cb81da6eae5b33ab2582c92a9b
Byron Guina <byron.guina@gmail.com>
2022-11-08 10:46:56 -0700

    Write tests for url config

--- apps/web/modules/params.ts
+++ apps/web/modules/params.ts
@@ -24,6 +24,12 @@ export function stringifyQueryParameters({ query, pageNumber }: InitialTripleSto
 export function getConfigFromUrl(url: string): AppConfig {
   const params = new URLSearchParams(url.split('?')[1]);
   const env: AppEnv = (params.get('env') as AppEnv) ?? 'production';
+
+  if (!(env in configOptions)) {
+    console.log(`Invalid environment "${env}", defaulting to production`);
+    return configOptions.production;
+  }
+
   const config = configOptions[env];
   return getConfig(config.chainId);
 }
