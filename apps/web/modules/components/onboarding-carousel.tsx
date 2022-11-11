92f8739e081bccc71ee1e38d1ea3ba8ad7339f40
Byron Guina <byron.guina@gmail.com>
2022-11-11 12:28:20 -0700

    Position dialog relative to Row container

--- apps/web/modules/components/onboarding-carousel.tsx
+++ apps/web/modules/components/onboarding-carousel.tsx
@@ -1,8 +1,6 @@
 import styled from '@emotion/styled';
-import { keyframes } from '@emotion/react';
-import { ForwardedRef, forwardRef, useEffect, useState } from 'react';
+import { ForwardedRef, forwardRef, useEffect, useRef, useState } from 'react';
 import { Button } from '../design-system/button';
-import { Dialog } from '../design-system/dialog';
 import { Copy } from '../design-system/icons/copy';
 import { Entity } from '../design-system/icons/entity';
 import { Facts } from '../design-system/icons/facts';
@@ -10,6 +8,8 @@ import { RightArrowLong } from '../design-system/icons/right-arrow-long';
 import { Target } from '../design-system/icons/target';
 import { Spacer } from '../design-system/spacer';
 import { Text } from '../design-system/text';
+import { useRect } from '@radix-ui/react-use-rect';
+import { Dialog } from './onboarding-dialog';
 
 const Row = styled.div(({ theme }) => ({
   position: 'relative',
@@ -18,20 +18,28 @@ const Row = styled.div(({ theme }) => ({
   gap: theme.space * 2,
 }));
 
-interface OnboardButtonProps {
-  isActive: boolean;
-  onClick: () => void;
-  children: React.ReactNode;
+interface DialogContentProps {
+  left?: number;
+  top?: number;
 }
 
-const DialogContent = styled.div(({ theme }) => ({
+const DialogContent = styled.div<DialogContentProps>(({ theme, left = 0, top = 0 }) => ({
+  position: 'absolute',
+  top: top + theme.space * 3,
+  left: left,
   display: 'flex',
   flexDirection: 'column',
   border: `1px solid ${theme.colors.text}`,
   borderRadius: theme.radius,
   padding: theme.space * 5,
-  maxWidth: 1060,
+  width: 1060,
 }));
+
+interface OnboardButtonProps {
+  isActive: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+}
 
 const OnboardingButton = forwardRef(function OnboardingButton(
   { isActive, onClick, children }: OnboardButtonProps,
@@ -48,45 +56,66 @@ const OnboardingButton = forwardRef(function OnboardingButton(
   );
 });
 
+type DialogChoice = 'collect' | 'organize' | 'empower' | 'solve';
+
+const DIALOG_CONTENT: Record<DialogChoice, { title: string; description: string }> = {
+  collect: {
+    title: 'We are here',
+    description: `Geo is built on the principle that data should be owned and created by its users in a decentralized and
+    verifiable way. The first and most crucial step is to collect data for a wide range of spaces as
+    triples/facts, which can then be used to structure meaningful content.`,
+  },
+  organize: {
+    title: 'We are here',
+    description: `Geo is built on the principle that data should be owned and created by its users in a decentralized and
+    verifiable way. The first and most crucial step is to collect data for a wide range of spaces as
+    triples/facts, which can then be used to structure meaningful content.`,
+  },
+  empower: {
+    title: 'We are here',
+    description: `Geo is built on the principle that data should be owned and created by its users in a decentralized and
+    verifiable way. The first and most crucial step is to collect data for a wide range of spaces as
+    triples/facts, which can then be used to structure meaningful content.`,
+  },
+  solve: {
+    title: 'We are here',
+    description: `Geo is built on the principle that data should be owned and created by its users in a decentralized and
+    verifiable way. The first and most crucial step is to collect data for a wide range of spaces as
+    triples/facts, which can then be used to structure meaningful content.`,
+  },
+};
+
 export function OboardingCarousel() {
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [step, setStep] = useState<'collect' | 'organize' | 'empower' | 'solve'>('collect');
+  const [step, setStep] = useState<DialogChoice>('collect');
+  const rowRef = useRef<HTMLDivElement>(null);
+  const rowRect = useRect(rowRef.current);
 
   // There's a hydration bug if the dialog defaults to open on first render in SSR when using
   // a portal. We render the dialog a second after page load to get around it.
   // https://github.com/radix-ui/primitives/issues/1386
   useEffect(() => {
-    setTimeout(() => setDialogOpen(true), 1000);
+    setTimeout(() => setDialogOpen(true), 100);
   }, []);
 
+  const rowLeft = rowRect?.left ?? 0;
+  const rowTop = (rowRect?.top ?? 0) + (rowRect?.height ?? 0);
+
   return (
-    <Row>
-      <Dialog
-        trigger={
-          <OnboardingButton onClick={() => setStep('collect')} isActive={step === 'collect'}>
-            <Facts color={step === 'collect' ? 'white' : `grey-04`} />
-            <Spacer width={8} />
-            Collect data
-          </OnboardingButton>
-        }
-        open={dialogOpen}
-        onOpenChange={setDialogOpen}
-      >
-        <DialogContent>
-          <Text variant="mediumTitle">We are here</Text>
+    <Row ref={rowRef}>
+      <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+        <DialogContent left={rowLeft} top={rowTop}>
+          <Text variant="mediumTitle">{DIALOG_CONTENT[step].title}</Text>
           <Spacer height={4} />
-          <Text>
-            Geo is built on the principle that data should be owned and created by its users in a decentralized and
-            verifiable way. The first and most crucial step is to collect data for a wide range of spaces as
-            triples/facts, which can then be used to structure meaningful content.
-          </Text>
+          <Text>{DIALOG_CONTENT[step].description}</Text>
         </DialogContent>
       </Dialog>
-      {/* <OnboardingButton onClick={() => setStep('collect')} isActive={step === 'collect'}>
+
+      <OnboardingButton onClick={() => setStep('collect')} isActive={step === 'collect'}>
         <Facts color={step === 'collect' ? 'white' : `grey-04`} />
         <Spacer width={8} />
         Collect data
-      </OnboardingButton> */}
+      </OnboardingButton>
 
       <RightArrowLong color="grey-04" />
 
--- apps/web/modules/components/onboarding-dialog.tsx
+++ apps/web/modules/components/onboarding-dialog.tsx
@@ -1,29 +1,21 @@
 import { useTheme } from '@emotion/react';
-import styled from '@emotion/styled';
 import * as PopoverPrimitive from '@radix-ui/react-popover';
-
-interface ContentProps {
-  children: React.ReactNode;
-}
 
 interface Props {
   children: React.ReactNode;
   open: boolean;
   onOpenChange: (open: boolean) => void;
   defaultOpen?: boolean;
-  trigger?: JSX.Element;
+  alignOffset?: number;
 }
 
-export function Dialog({ children, open, onOpenChange, trigger, defaultOpen = false }: Props) {
+export function Dialog({ children, open, onOpenChange, defaultOpen = false }: Props) {
   const { space } = useTheme();
 
   return (
     <PopoverPrimitive.Root onOpenChange={onOpenChange} open={open} defaultOpen={defaultOpen}>
-      <PopoverPrimitive.Trigger asChild>{trigger}</PopoverPrimitive.Trigger>
       <PopoverPrimitive.Portal>
         <PopoverPrimitive.Content
-          align="start"
-          sideOffset={space * 3}
           onOpenAutoFocus={event => event.preventDefault()}
           onInteractOutside={event => event.preventDefault()}
         >
