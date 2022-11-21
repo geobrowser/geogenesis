3fe8a01b5ab2de189723a1a05cc47df83755f97b
Byron Guina <byron.guina@gmail.com>
2022-11-21 07:42:13 -0700

    Default to testnet for now

--- apps/web/modules/params.ts
+++ apps/web/modules/params.ts
@@ -23,7 +23,7 @@ export function stringifyQueryParameters({ query, pageNumber }: InitialTripleSto
 
 export function getConfigFromUrl(url: string): AppConfig {
   const params = new URLSearchParams(url.split('?')[1]);
-  const env: AppEnv = (params.get('env') as AppEnv) ?? 'production';
+  const env: AppEnv = (params.get('env') as AppEnv) ?? 'testnet';
 
   if (!(env in configOptions)) {
     console.log(`Invalid environment "${env}", defaulting to production`);
