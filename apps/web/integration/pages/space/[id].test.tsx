1a886cd8c4993036cad458b832770ed773c6be84
Byron Guina <byron.guina@gmail.com>
2022-12-08 14:50:49 -0700

    Remove unused import

--- apps/web/integration/pages/space/[id].test.tsx
+++ apps/web/integration/pages/space/[id].test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
 import { makeStubTriple } from '~/modules/services/mock-network';
