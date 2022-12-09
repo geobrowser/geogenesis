ee92be3d9eca9fe490cf08c2453137ab50a960a3
Byron Guina <byron.guina@gmail.com>
2022-12-09 16:23:18 -0700

    Use our own values for chains

--- apps/web/modules/wallet.tsx
+++ apps/web/modules/wallet.tsx
@@ -1,9 +1,8 @@
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { polygon, polygonMumbai } from 'wagmi/chains';
+import { Chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
-// import { configOptions } from './config';
+import { configOptions } from './config';
 
 // const LOCAL_CHAIN: Chain = {
 //   id: Number(configOptions.development.chainId),
@@ -35,35 +34,35 @@ import { publicProvider } from 'wagmi/providers/public';
 //   },
 // };
 
-// const TESTNET_CHAIN: Chain = {
-//   id: Number(configOptions.testnet.chainId),
-//   name: 'Polygon Mumbai', // Human-readable name
-//   network: 'mumbai', // Internal network name
-//   nativeCurrency: {
-//     name: 'Polygon Mumbai',
-//     symbol: 'MATIC',
-//     decimals: 18,
-//   },
-//   rpcUrls: {
-//     default: configOptions.testnet.rpc,
-//   },
-// };
+const TESTNET_CHAIN: Chain = {
+  id: Number(configOptions.testnet.chainId),
+  name: 'Polygon Mumbai', // Human-readable name
+  network: 'mumbai', // Internal network name
+  nativeCurrency: {
+    name: 'Polygon Mumbai',
+    symbol: 'MATIC',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: configOptions.testnet.rpc,
+  },
+};
 
-// const DEFAULT_CHAIN: Chain = {
-//   id: Number(configOptions.production.chainId),
-//   name: 'Polygon', // Human-readable name
-//   network: 'polygon', // Internal network name
-//   nativeCurrency: {
-//     name: 'Polygon Mumbai',
-//     symbol: 'MATIC',
-//     decimals: 18,
-//   },
-//   rpcUrls: {
-//     default: configOptions.production.rpc,
-//   },
-// };
+const DEFAULT_CHAIN: Chain = {
+  id: Number(configOptions.production.chainId),
+  name: 'Polygon', // Human-readable name
+  network: 'polygon', // Internal network name
+  nativeCurrency: {
+    name: 'Polygon Mumbai',
+    symbol: 'MATIC',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: configOptions.production.rpc,
+  },
+};
 
-const { chains, provider, webSocketProvider } = configureChains([polygon, polygonMumbai], [publicProvider()]);
+const { chains, provider, webSocketProvider } = configureChains([DEFAULT_CHAIN, TESTNET_CHAIN], [publicProvider()]);
 
 const { connectors } = getDefaultWallets({
   appName: 'Geo Genesis',
