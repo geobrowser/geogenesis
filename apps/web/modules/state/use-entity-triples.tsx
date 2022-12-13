0bbadcefcd4edcca1bea6467e03c86fca6a5036c
Byron Guina <byron.guina@gmail.com>
2022-12-13 16:49:48 -0700

    Initial create page

--- apps/web/modules/state/use-entity-triples.tsx
+++ apps/web/modules/state/use-entity-triples.tsx
@@ -1,11 +1,12 @@
 import { useSelector } from '@legendapp/state/react';
+import { Action } from '../types';
 import { useEntityStore } from './entity-store-provider';
 
 export function useEntityTriples() {
   const { create, publish, entityNames$, triples$, actions$, update, remove } = useEntityStore();
   const entityNames = useSelector(entityNames$);
   const triples = useSelector(triples$);
-  const actions = useSelector(actions$);
+  const actions = useSelector<Action[]>(actions$);
 
   return {
     triples,
---
+++ apps/web/pages/space/[id]/create-entity.tsx
@@ -0,0 +1,30 @@
+import { GetServerSideProps } from 'next';
+import { useMemo } from 'react';
+import { EditableEntityPage } from '~/modules/components/entity/editable-entity-page';
+import { createEntityId } from '~/modules/services/create-id';
+import { EntityStoreProvider } from '~/modules/state/entity-store-provider';
+
+interface Props {
+  spaceId: string;
+}
+
+export default function CreateEntity({ spaceId }: Props) {
+  const newId = useMemo(() => createEntityId(), []);
+
+  return (
+    <EntityStoreProvider spaceId={spaceId} initialEntityNames={{}} initialTriples={[]}>
+      <EditableEntityPage id={newId} name="" space={spaceId} triples={[]} entityNames={{}} />
+    </EntityStoreProvider>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async context => {
+  const spaceId = context.query.id as string;
+
+  return {
+    props: {
+      spaceId,
+      key: spaceId,
+    },
+  };
+};
