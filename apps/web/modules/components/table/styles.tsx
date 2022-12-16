7cb50240612e330f10d20c9c15bb4aa966d5f31d
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2022-12-16 14:47:54 -0800

    Continue extracting shared styles from Triple Table

--- apps/web/modules/components/table/styles.tsx
+++ apps/web/modules/components/table/styles.tsx
@@ -7,6 +7,11 @@ export const SpaceHeaderContainer = styled.div({
   width: '100%',
 });
 
+export const EmptyTableText = styled.td(props => ({
+  ...props.theme.typography.tableCell,
+  padding: props.theme.space * 2.5,
+}));
+
 export const SpaceInfo = styled.div(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
@@ -40,3 +45,38 @@ export const SpaceImageContainer = styled.div(props => ({
   width: props.theme.space * 14,
   height: props.theme.space * 14,
 }));
+
+export const Table = styled.table(props => ({
+  width: '100%',
+  borderStyle: 'hidden',
+  borderCollapse: 'collapse',
+  backgroundColor: props.theme.colors.white,
+}));
+
+export const SpaceHeader = styled.th<{ width: number }>(props => ({
+  border: `1px solid ${props.theme.colors['grey-02']}`,
+  padding: props.theme.space * 2.5,
+  textAlign: 'left',
+  width: props.width,
+}));
+
+export const TableRow = styled.tr(props => ({
+  ':hover': {
+    backgroundColor: props.theme.colors.bg,
+  },
+}));
+
+// Using a container to wrap the table to make styling borders around
+// the table easier. Otherwise we need to do some pseudoselector shenanigans
+// or use box-shadow instead of border.
+export const Container = styled.div(props => ({
+  padding: 0,
+  border: `1px solid ${props.theme.colors['grey-02']}`,
+  borderRadius: props.theme.radius,
+  overflow: 'hidden',
+}));
+
+// Negative margin so table row height matches a single line of text
+export const ChipCellContainer = styled.div({
+  margin: '-1px 0',
+});
