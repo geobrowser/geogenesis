4a1ac4c4818dbc8462a20568a18d2fa1c7cb89aa
Byron Guina <byron.guina@gmail.com>
2023-01-03 18:23:25 -0700

    Example using Result for an async operation

--- apps/web/modules/entity/autocomplete.tsx
+++ apps/web/modules/entity/autocomplete.tsx
@@ -1,25 +1,40 @@
 import { computed, observable, ObservableComputed } from '@legendapp/state';
 import { useSelector } from '@legendapp/state/react';
+import { pipe, R } from '@mobily/ts-belt';
 import { useEffect, useMemo } from 'react';
 import { Services } from '~/modules/services';
 import { INetwork } from '~/modules/services/network';
 import { makeOptionalComputed } from '~/modules/utils';
 
+export type EntityAutocompleteResult = {
+  id: string;
+  name: string | null;
+};
+
 class EntityAutocomplete {
   query$ = observable('');
-  results$: ObservableComputed<{ id: string; name: string | null }[]>;
+  results$: ObservableComputed<EntityAutocompleteResult[]>;
   abortController: AbortController = new AbortController();
 
   constructor({ api }: { api: INetwork }) {
-    this.results$ = makeOptionalComputed(
-      [],
+    const networkData$ = makeOptionalComputed(
+      R.Ok<EntityAutocompleteResult[]>([]),
       computed(async () => {
         this.abortController.abort();
         this.abortController = new AbortController();
 
-        return await api.fetchEntities(this.query$.get(), this.abortController);
+        return pipe(
+          await R.fromPromise(api.fetchEntities(this.query$.get(), this.abortController)),
+
+          // Right now we don't do anything with the error state.
+          R.catchError(() => R.Error('Failed to fetch autocomplete entities'))
+        );
       })
     );
+
+    // If the result of the computed fetch is an error we default to an empty array.
+    // Otherwise return the data.
+    this.results$ = computed(() => R.getWithDefault(networkData$.get(), []));
   }
 
   onQueryChange = (query: string) => {
