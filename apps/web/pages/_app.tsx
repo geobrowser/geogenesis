5391fc4dad799968ca5a9864d5e80b7939a49f9f
Byron Guina <byron.guina@gmail.com>
2023-01-04 17:28:05 -0700

    Re-implement _app

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,40 +1,40 @@
-// import { css, Global } from '@emotion/react';
-// import styled from '@emotion/styled';
+import { css, Global } from '@emotion/react';
+import styled from '@emotion/styled';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Navbar } from '~/modules/components/navbar/navbar';
-// import { colors } from '~/modules/design-system/theme/colors';
+import { colors } from '~/modules/design-system/theme/colors';
 import { Providers } from '~/modules/providers';
-// import { FlowBar } from '~/modules/components/flow-bar';
-// import { useActionsStore } from '~/modules/action';
-// import { useRouter } from 'next/router';
-// import { Analytics } from '@vercel/analytics/react';
+import { FlowBar } from '~/modules/components/flow-bar';
+import { useActionsStore } from '~/modules/action';
+import { useRouter } from 'next/router';
+import { Analytics } from '@vercel/analytics/react';
 
 import 'modern-normalize';
 import '../styles/styles.css';
 
-// const globalStyles = css`
-//   html {
-//     overflow-y: overlay;
-//   }
+const globalStyles = css`
+  html {
+    overflow-y: overlay;
+  }
 
-//   body {
-//     font-family: Calibre, sans-serif;
-//     text-rendering: 'optimizeLegibility';
-//     background-color: ${colors.light.bg};
-//   }
-// `;
+  body {
+    font-family: Calibre, sans-serif;
+    text-rendering: 'optimizeLegibility';
+    background-color: ${colors.light.bg};
+  }
+`;
 
-// const Layout = styled.main(props => ({
-//   paddingTop: props.theme.space * 10,
-//   paddingBottom: props.theme.space * 20,
-//   maxWidth: 1200,
-//   margin: '0 auto',
+const Layout = styled.main(props => ({
+  paddingTop: props.theme.space * 10,
+  paddingBottom: props.theme.space * 20,
+  maxWidth: 1200,
+  margin: '0 auto',
 
-//   '@media (max-width: 1200px)': {
-//     padding: `${props.theme.space * 10}px 2ch 4ch 2ch`,
-//   },
-// }));
+  '@media (max-width: 1200px)': {
+    padding: `${props.theme.space * 10}px 2ch 4ch 2ch`,
+  },
+}));
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -43,34 +43,34 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Geo Genesis</title>
       </Head>
-      {/* <Global styles={globalStyles} /> */}
+      <Global styles={globalStyles} />
       <Navbar />
-      {/* <Layout> */}
-      <Component {...pageProps} />
-      {/* <Analytics /> */}
-      {/* </Layout> */}
-      {/* <GlobalFlowBar /> */}
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+      <GlobalFlowBar />
     </Providers>
   );
 }
 
-// const FlowbarContainer = styled.div({
-//   position: 'relative',
-//   display: 'flex',
-//   flexDirection: 'column',
-//   alignItems: 'center',
-// });
+const FlowbarContainer = styled.div({
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
 
-// function GlobalFlowBar() {
-//   const router = useRouter();
-//   const { id: spaceId } = router.query as { id: string | undefined };
-//   const { actions, publish } = useActionsStore(spaceId);
+function GlobalFlowBar() {
+  const router = useRouter();
+  const { id: spaceId } = router.query as { id: string | undefined };
+  const { actions, publish } = useActionsStore(spaceId);
 
-//   return (
-//     <FlowbarContainer>
-//       <FlowBar actions={actions} onPublish={publish} spaceId={spaceId} />
-//     </FlowbarContainer>
-//   );
-// }
+  return (
+    <FlowbarContainer>
+      <FlowBar actions={actions} onPublish={publish} spaceId={spaceId} />
+    </FlowbarContainer>
+  );
+}
 
 export default MyApp;
