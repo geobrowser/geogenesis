d18e88078ae6ed08ba0032775050be4cb7201ec0
Byron Guina <byron.guina@gmail.com>
2023-01-06 15:29:16 -0700

    Add tests for Value.entityName helper

--- apps/web/modules/value/value.ts
+++ apps/web/modules/value/value.ts
@@ -1,9 +1,5 @@
 import { Triple } from '../types';
 
 export function entityName(triple: Triple) {
-  return triple?.value?.type === 'string'
-    ? triple.value.value
-    : triple?.value?.type === 'entity'
-    ? triple.value.name
-    : null;
+  return triple?.value?.type === 'entity' ? triple.value.name : null;
 }
