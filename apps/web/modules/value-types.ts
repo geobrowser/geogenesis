4bc0d7d4dcc312fd46d74680bcf83f0b3a61efc2
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2023-01-09 14:08:58 -0800

    Lil types

--- apps/web/modules/value-types.ts
+++ apps/web/modules/value-types.ts
@@ -1,6 +1,7 @@
 import { SYSTEM_IDS } from './constants';
+import { TripleValueType } from './types';
 
-export const valueTypes = {
+export const valueTypes: Record<string, TripleValueType> = {
   [SYSTEM_IDS.TEXT]: 'string',
   [SYSTEM_IDS.RELATION]: 'entity',
 };
