d2e086737981512c91b6d9e7d9a027eaec3a659c
Byron Guina <byron.guina@gmail.com>
2023-01-10 13:25:08 -0700

    Set TS types for valid valueTypes

--- apps/web/modules/value-types.ts
+++ apps/web/modules/value-types.ts
@@ -1,7 +1,11 @@
 import { SYSTEM_IDS } from '@geogenesis/ids';
 import { TripleValueType } from './types';
 
-export const valueTypes: Record<string, TripleValueType> = {
+type IDs = typeof SYSTEM_IDS;
+type ValueTypeKeys = Pick<IDs, 'TEXT' | 'RELATION'>;
+type IdsOfValueTypes = IDs[keyof ValueTypeKeys];
+
+export const valueTypes: Record<IdsOfValueTypes, TripleValueType> = {
   [SYSTEM_IDS.TEXT]: 'string',
   [SYSTEM_IDS.RELATION]: 'entity',
 };
