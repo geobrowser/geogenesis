b42e813c81ad8b29fcb35c43ee40c7917ad51533
Byron Guina <byron.guina@gmail.com>
2023-01-10 13:32:49 -0700

    Just use string smh my head
    Kinda infects the codebase a bit where we need to use the stricter type everywhere. It's fine just to keep it as string for now.

--- apps/web/modules/value-types.ts
+++ apps/web/modules/value-types.ts
@@ -1,11 +1,7 @@
 import { SYSTEM_IDS } from '@geogenesis/ids';
 import { TripleValueType } from './types';
 
-type IDs = typeof SYSTEM_IDS;
-type ValueTypeKeys = Pick<IDs, 'TEXT' | 'RELATION'>;
-type IdsOfValueTypes = IDs[keyof ValueTypeKeys];
-
-export const valueTypes: Record<IdsOfValueTypes, TripleValueType> = {
+export const valueTypes: Record<string, TripleValueType> = {
   [SYSTEM_IDS.TEXT]: 'string',
   [SYSTEM_IDS.RELATION]: 'entity',
 };
