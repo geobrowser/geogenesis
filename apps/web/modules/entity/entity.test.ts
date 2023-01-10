e02a58be44353566008e7881700e04043356b3c3
Byron Guina <byron.guina@gmail.com>
2023-01-10 09:09:52 -0700

    Add tests for Entity.name and new Entity.xTriple helpers

--- apps/web/modules/entity/entity.test.ts
+++ apps/web/modules/entity/entity.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { SYSTEM_IDS } from '../constants';
 import { Triple } from '../types';
-import { description, types } from './entity';
+import { description, descriptionTriple, name, nameTriple, types } from './entity';
 
 const triplesWithSystemDescriptionAttribute: Triple[] = [
   {
@@ -83,7 +83,7 @@ describe('Entity description helpers', () => {
     expect(description(triplesWithSystemDescriptionAttribute)).toBe('banana');
   });
 
-  it('Entity.description should parse description from triples where description is the expected system Description and value is a reference to another Entity', () => {
+  it('Entity.description should return null where description is the expected system Description and value is a reference to another Entity', () => {
     expect(description(triplesWithSystemDescriptionAttributeAndValueIsEntity)).toBe(null);
   });
 
@@ -91,8 +91,86 @@ describe('Entity description helpers', () => {
     expect(description(triplesWithNonSystemDescriptionAttribute)).toBe('banana');
   });
 
-  it('Entity.description should parse description from triples where description is not the expected system Description and value is a reference to another Entity', () => {
+  it('Entity.description should return null where description is not the expected system Description and value is a reference to another Entity', () => {
     expect(description(triplesWithNonSystemDescriptionAttributeAndValueIsEntity)).toBe(null);
+  });
+
+  it('Entity.descriptionTriple should return the Description triple', () => {
+    expect(descriptionTriple(triplesWithSystemDescriptionAttribute)).toBe(triplesWithSystemDescriptionAttribute[0]);
+  });
+
+  it('Entity.descriptionTriple should return undefined if there is no Description triple', () => {
+    expect(descriptionTriple([])).toBe(undefined);
+  });
+});
+
+const triplesWithSystemNameAttribute: Triple[] = [
+  {
+    id: '',
+    entityId: 'entityId',
+    attributeId: SYSTEM_IDS.NAME,
+    attributeName: 'Name',
+    entityName: 'banana',
+    space: 'spaceId',
+    value: {
+      id: 'valueId',
+      type: 'string',
+      value: 'banana',
+    },
+  },
+];
+
+const triplesWithSystemNameAttributeAndNameIsEntity: Triple[] = [
+  {
+    id: '',
+    entityId: 'entityId',
+    attributeId: SYSTEM_IDS.NAME,
+    attributeName: 'Name',
+    entityName: 'banana',
+    space: 'spaceId',
+    value: {
+      id: 'valueId',
+      type: 'entity',
+      name: 'banana',
+    },
+  },
+];
+
+const triplesWithNonSystemNameAttribute: Triple[] = [
+  {
+    id: '',
+    entityId: 'entityId',
+    attributeId: 'not-name',
+    attributeName: 'Name',
+    entityName: 'banana',
+    space: 'spaceId',
+    value: {
+      id: 'valueId',
+      type: 'string',
+      value: 'banana',
+    },
+  },
+];
+
+describe('Entity name helpers', () => {
+  it('Entity.name should parse name from triples where name attribute is the the expected system Name', () => {
+    expect(name(triplesWithSystemNameAttribute)).toBe('banana');
+  });
+
+  it('Entity.name should parse name from triples where name is the expected system Name and value is a reference to another Entity', () => {
+    expect(name(triplesWithSystemNameAttributeAndNameIsEntity)).toBe(null);
+  });
+
+  it('Entity.name should return null where name is not the expected system Name', () => {
+    expect(name(triplesWithNonSystemNameAttribute)).toBe(null);
+  });
+
+  it('Entity.nameTriple should return the Name triple', () => {
+    expect(nameTriple(triplesWithSystemNameAttribute)).toBe(triplesWithSystemNameAttribute[0]);
+  });
+
+  it('Entity.nameTriple should return undefined if there is no Name triple', () => {
+    expect(nameTriple([])).toBe(undefined);
   });
 });
 
