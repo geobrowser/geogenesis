a1d930b4bef97cb15b5a977b02bb5c5bf82857b8
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2023-01-12 09:38:39 -0800

    initialSchemaTriples added to EntityStoreProvider

--- apps/web/pages/space/[id]/create-entity.tsx
+++ apps/web/pages/space/[id]/create-entity.tsx
@@ -13,7 +13,7 @@ export default function CreateEntity({ spaceId }: Props) {
 
   return (
     <EntityStoreProvider id={newId} spaceId={spaceId} initialTriples={[]} initialSchemaTriples={[]}>
-      <EditableEntityPage id={newId} name="" space={spaceId} triples={[]} />
+      <EditableEntityPage id={newId} name="" space={spaceId} triples={[]} schemaTriples={[]} />
     </EntityStoreProvider>
   );
 }
