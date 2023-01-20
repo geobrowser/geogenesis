7c951fda0c063733f420479b84a6a2601999cdf1
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2023-01-20 11:35:08 -0800

    Mock network and tests must handle new entity table column structure

--- apps/web/integration/pages/space/[id].test.tsx
+++ apps/web/integration/pages/space/[id].test.tsx
@@ -1,7 +1,10 @@
+import { SYSTEM_IDS } from '@geogenesis/ids';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
 import { Providers } from '~/modules/providers';
+import { Triple } from '~/modules/triple';
+import { Triple as TripleType } from '~/modules/types';
 import SpacePage from '~/pages/space/[id]';
 
 describe('Space page', () => {
@@ -43,6 +46,17 @@ describe('Space page', () => {
   });
 
   it('Should render non-empty table', () => {
+    const nameTriple: TripleType = {
+      ...Triple.empty('1', '1'),
+      attributeName: 'name',
+      attributeId: SYSTEM_IDS.NAME,
+      value: {
+        id: '1',
+        type: 'string',
+        value: 'Alice',
+      },
+    };
+
     render(
       <Providers>
         <SpacePage
@@ -50,7 +64,7 @@ describe('Space page', () => {
           spaceName="Banana"
           spaceImage={null}
           initialTypes={[]}
-          initialColumns={[{ id: '1', name: 'Alice' }]}
+          initialColumns={[{ id: '1', triples: [nameTriple] }]}
           initialRows={[
             {
               '1': {
