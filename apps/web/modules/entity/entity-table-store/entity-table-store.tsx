6501eca127b63e9cca881bdc5d1fe4d4cfa6b37c
Byron Guina <byron.guina@gmail.com>
2023-01-26 13:05:16 -0700

    Correctly merge entities that have changed locally
    This is a separate workflow from entities that are created locally. Since changed entities may not have had _all_ of their triples changed, we could be missing data required for rendering the table when we do the merge.
    
    Now we fetch entity triples if we do not have the data for it already.

--- apps/web/modules/entity/entity-table-store/entity-table-store.tsx
+++ apps/web/modules/entity/entity-table-store/entity-table-store.tsx
@@ -1,7 +1,8 @@
 import { computed, observable, Observable, ObservableComputed } from '@legendapp/state';
 import { A, pipe } from '@mobily/ts-belt';
 import produce from 'immer';
-import { ActionsStore } from '~/modules/action';
+import { Action, ActionsStore } from '~/modules/action';
+import { Triple } from '~/modules/triple';
 import { Entity, EntityTable } from '..';
 import { INetwork } from '../../services/network';
 import { Column, FilterState, Row, Triple as TripleType } from '../../types';
@@ -18,6 +19,7 @@ interface IEntityTableStore {
   hasPreviousPage$: ObservableComputed<boolean>;
   hydrated$: Observable<boolean>;
   hasNextPage$: ObservableComputed<boolean>;
+  ActionStore: ActionsStore;
   setQuery(query: string): void;
   setPageNumber(page: number): void;
 }
@@ -64,6 +66,7 @@ export class EntityTableStore implements IEntityTableStore {
   hasPreviousPage$: ObservableComputed<boolean>;
   hasNextPage$: ObservableComputed<boolean>;
   space: string;
+  ActionStore: ActionsStore;
   abortController: AbortController = new AbortController();
 
   constructor({
@@ -78,6 +81,7 @@ export class EntityTableStore implements IEntityTableStore {
     pageSize = DEFAULT_PAGE_SIZE,
   }: IEntityTableStoreConfig) {
     this.api = api;
+    this.ActionStore = ActionStore;
     this.hydrated$ = observable(false);
     this.rows$ = observable(initialRows);
     this.selectedType$ = observable(initialSelectedType);
@@ -126,21 +130,67 @@ export class EntityTableStore implements IEntityTableStore {
             abortController: this.abortController,
           });
 
-          // We need to do the same for columns and columnsSchema :thinking:
-          const localEntities = pipe(
-            ActionStore.actions$.get(),
-            actions => Entity.mergeActionsWithEntities(actions, Entity.entitiesFromTriples(serverRows)),
+          /**
+           * There are several edge-cases we need to handle in order to correctly merge local changes
+           * with server data in the entity table:
+           * 1. An entity is created locally and is given the selected type
+           * 2. An entity is edited locally and is given the selected type
+           *
+           * Since the table aggregation code expects triples, we may end up in a situation where
+           * the type for an entity has changed, but the name hasn't. In this case there is no local
+           * version of the name triple, so we need to fetch it along with any other triples the table
+           * needs to render the columnSchema.
+           */
+          const changedEntitiesIdsFromAnotherType = pipe(
+            this.ActionStore.actions$.get()[space],
+            actions => Triple.fromActions(actions, []),
+            triples => Entity.entitiesFromTriples(triples),
+            A.filter(e => e.types.some(t => t.id === this.selectedType$.get()?.entityId)),
+            A.map(t => t.id)
+          );
 
-            // HACK: This doesn't work reliably since the entity name might not be unique. We need to use
-            // the type id here, but right now that breaks entity editing if you are editing the type field.
+          // Fetch any entities that have been changed locally and have the selected type to make sure we have all
+          // of the triples necessary to build the table.
+          const serverTriplesForEntitiesChangedLocally = await Promise.all(
+            changedEntitiesIdsFromAnotherType.map(id =>
+              this.api.fetchTriples({
+                space,
+                query: '',
+                skip: 0,
+                first: 100,
+                filter: [
+                  {
+                    field: 'entity-id',
+                    value: id,
+                  },
+                ],
+              })
+            )
+          );
+
+          // Merge any local changes to triples in an entity with the table rows from the server.
+          const entitiesCreatedOrChangedLocally = pipe(
+            this.ActionStore.actions$.get(),
+            actions => Entity.mergeActionsWithEntities(actions, Entity.entitiesFromTriples(serverRows)),
             A.filter(e => e.types.some(t => t.id === this.selectedType$.get()?.entityId))
           );
 
-          const localEntitiesIds = new Set(localEntities.map(e => e.id));
+          const localEntitiesIds = new Set(entitiesCreatedOrChangedLocally.map(e => e.id));
 
           const { rows, hasNextPage } = EntityTable.fromColumnsAndRows(
             space,
-            [...localEntities.flatMap(e => e.triples), ...serverRows.filter(sr => !localEntitiesIds.has(sr.id))],
+            [
+              // These are entities that were created locally and have the selected type
+              ...entitiesCreatedOrChangedLocally.flatMap(e => e.triples),
+
+              // These are entities that have a new type locally and may exist on the server.
+              // We need to fetch all triples associated with this entity in order to correctly
+              // populate the table.
+              ...serverTriplesForEntitiesChangedLocally.flatMap(e => e.triples),
+
+              // These are entities that have been fetched from the server and have the selected type
+              ...serverRows.filter(sr => !localEntitiesIds.has(sr.id)),
+            ],
             serverColumns,
             columnsSchema
           );
