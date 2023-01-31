69e09042849cc6aeef59e67ff809560a0ba9647f
Byron Guina <byron.guina@gmail.com>
2023-01-31 15:06:35 -0700

    feat: expose root space and add image

--- apps/web/modules/constants.ts
+++ apps/web/modules/constants.ts
@@ -5,3 +5,6 @@ export const PLACEHOLDER_IMAGES = {
   Health: 'https://images.unsplash.com/photo-1535914254981-b5012eebbd15',
   Values: 'https://images.unsplash.com/photo-1637008336770-b95d637fd5fa',
 };
+
+export const ROOT_SPACE_IMAGE =
+  'https://cdn.discordapp.com/attachments/882371689244143687/1070102478802133052/root.png';
