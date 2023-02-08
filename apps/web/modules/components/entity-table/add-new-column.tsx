069b81dbf0865f3d952e5d7c1b5f446558adf6b7
Byron <26263630+baiirun@users.noreply.github.com>
2023-02-08 12:58:24 -0700

    fix: correct add column button styling (#211)
    * Correct add column button styling
    
    * This element doesn't need to be a table element
    
    * Add viewport meta tag
    
    * Fix button position on safari
    
    * We were already setting viewport information in _app

--- apps/web/modules/components/entity-table/add-new-column.tsx
+++ apps/web/modules/components/entity-table/add-new-column.tsx
@@ -1,39 +1,8 @@
-import styled from '@emotion/styled';
 import { memo } from 'react';
 import { useActionsStoreContext } from '~/modules/action';
 import { Plus } from '~/modules/design-system/icons/plus';
 import { Triple } from '~/modules/types';
 import { useEditEvents } from '../entity/edit-events';
-
-const StyledIconButton = styled.button(props => ({
-  all: 'unset',
-  backgroundColor: props.theme.colors.white,
-  padding: `0 ${props.theme.space * 3}px`,
-  transition: 'colors 0.15s ease-in-out',
-  position: 'absolute',
-  right: 0,
-  top: 0,
-  bottom: 2,
-  zIndex: 1,
-  borderTopRightRadius: props.theme.radius,
-  borderLeft: `1px solid ${props.theme.colors['grey-02']}`,
-
-  '&:hover': {
-    cursor: 'pointer',
-    backgroundColor: props.theme.colors['grey-01'],
-    color: props.theme.colors.text,
-  },
-
-  '&:active': {
-    color: props.theme.colors.text,
-    outlineColor: props.theme.colors.ctaPrimary,
-  },
-
-  '&:focus': {
-    color: props.theme.colors.text,
-    outlineColor: props.theme.colors.ctaPrimary,
-  },
-}));
 
 interface Props {
   selectedType: Triple;
@@ -57,8 +26,11 @@ export const AddNewColumn = memo(function AddNewColumn({ selectedType, space }: 
   });
 
   return (
-    <StyledIconButton onClick={() => send({ type: 'ADD_NEW_COLUMN' })}>
+    <button
+      className="absolute bg-white p-[13.5px] top-0 right-0 border-l border-b border-grey-02 transition-colors ease-in-out duration-150 hover:cursor-pointer hover:bg-grey-01 hover:text-text active:text-text active:outline-ctaPrimary focus:text-text focus:outline-ctaPrimary"
+      onClick={() => send({ type: 'ADD_NEW_COLUMN' })}
+    >
       <Plus />
-    </StyledIconButton>
+    </button>
   );
 });
