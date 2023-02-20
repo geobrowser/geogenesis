76ac479888edb25fc4c41fb718484c7283faaebe
Byron Guina <byron.guina@gmail.com>
2023-02-20 15:54:40 -0700

    Use resizable container to animate container height

--- apps/web/modules/components/entity/presence/entity-others-toast.tsx
+++ apps/web/modules/components/entity/presence/entity-others-toast.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { useAccount } from 'wagmi';
 import BoringAvatar from 'boring-avatars';
 import pluralize from 'pluralize';
 import { EntityPresenceContext } from './entity-presence-provider';
@@ -9,17 +8,17 @@ import { Spacer } from '~/modules/design-system/spacer';
 import { ChevronDownSmall } from '~/modules/design-system/icons/chevron-down-small';
 import { CloseSmall } from '~/modules/design-system/icons/close-small';
 import { AnimatePresence, motion } from 'framer-motion';
+import { ResizableContainer } from '~/modules/design-system/resizable-container';
 
 function shortAddress(address: string) {
   return `${address.slice(0, 6)}...${address.slice(-6)}`;
 }
 
 export function EntityOthersToast() {
-  const account = useAccount();
   const [open, setOpen] = useState(true);
   const [isExpanded, setIsExpanded] = useState(false);
   const others = EntityPresenceContext.useOthers();
-  const [me, setMe] = EntityPresenceContext.useMyPresence();
+  const [me] = EntityPresenceContext.useMyPresence();
 
   const shouldShow = open && others.length > 0;
 
@@ -27,9 +26,9 @@ export function EntityOthersToast() {
     <AnimatePresence>
       {shouldShow ? (
         <motion.div
-          initial={{ y: 90 }}
-          animate={{ y: 0 }}
-          exit={{ y: 90 }}
+          initial={{ opacity: 0, scale: 0.95, y: 15 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          exit={{ opacity: 0, scale: 0.95, y: 15 }}
           transition={{ duration: 0.15, ease: 'easeOut' }}
           className="absolute right-8 bottom-8 bg-white rounded p-3 border border-grey-02 shadow-lg w-60"
         >
@@ -51,25 +50,34 @@ export function EntityOthersToast() {
 
           <Spacer height={8} />
 
-          {isExpanded && (
-            <ul className="space-y-3 mb-2">
-              {others.map((other, i) => (
-                <motion.li
-                  key={other.connectionId}
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ delay: 0.02 * i, ease: 'easeOut' }}
-                  className="flex items-center gap-2"
-                >
-                  <BoringAvatar size={16} name={other.presence.address} variant="pixel" />
-                  <Text variant="metadata" color="grey-04">
-                    {shortAddress(other.presence.address ?? '')}
-                  </Text>
-                </motion.li>
-              ))}
-              {/* TODO: You segment */}
-            </ul>
-          )}
+          <ResizableContainer duration={0.15}>
+            {isExpanded ? (
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                <ul key="presence-user-list" className="space-y-3 mb-2 overflow-hidden">
+                  {others.map((other, i) => (
+                    <li key={other.connectionId} className="flex items-center gap-2">
+                      <BoringAvatar size={16} name={other.presence.address} variant="pixel" />
+                      <Text variant="metadata" color="grey-04">
+                        {shortAddress(other.presence.address ?? '')}
+                      </Text>
+                    </li>
+                  ))}
+                  <li className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <BoringAvatar size={16} name={me.address} variant="pixel" />
+                      <Text variant="metadata" color="grey-04">
+                        {shortAddress(me.address ?? '')}
+                      </Text>
+                    </div>
+                    <Text className="rounded bg-grey-02 px-1" color="grey-04" variant="footnoteMedium">
+                      You
+                    </Text>
+                  </li>
+                </ul>
+                <Spacer height={4} />
+              </motion.div>
+            ) : null}
+          </ResizableContainer>
 
           <SmallButton variant="secondary" onClick={() => setIsExpanded(!isExpanded)}>
             <span style={{ rotate: isExpanded ? '180deg' : '0deg' }}>
