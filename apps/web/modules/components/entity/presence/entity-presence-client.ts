3d4629100d387139f361b2b730f331104afd539b
Byron Guina <byron.guina@gmail.com>
2023-02-21 12:58:56 -0700

    Mock key

--- apps/web/modules/components/entity/presence/entity-presence-client.ts
+++ apps/web/modules/components/entity/presence/entity-presence-client.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@liveblocks/client';
 
 export const client = createClient({
-  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY ?? '',
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY ?? 'pk_bananas',
 });
