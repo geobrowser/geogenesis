d4ba263ca991a067e1ea8d39f358156fb5834a07
Byron Guina <byron.guina@gmail.com>
2023-02-21 09:20:11 -0700

    Use env var for public key

--- apps/web/modules/components/entity/presence/entity-presence-client.ts
+++ apps/web/modules/components/entity/presence/entity-presence-client.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@liveblocks/client';
 
 export const client = createClient({
-  publicApiKey: 'pk_dev_uzTd3iY-UETYh2z3jc12Wcd-bCeCxZioO27x_xxquWwmm1DlRv-JsQlNokIMNw0T',
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY ?? '',
 });
