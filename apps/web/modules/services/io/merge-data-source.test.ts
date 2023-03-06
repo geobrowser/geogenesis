689c81d604578e538978cf8633a1d1ada348eade
Byron Guina <byron.guina@gmail.com>
2023-03-06 11:14:39 -0700

    Initial implementation of merged local/remote data sources

---
+++ apps/web/modules/services/io/merge-data-source.test.ts
@@ -0,0 +1,178 @@
+import { describe } from 'vitest';
+import { ActionsStore } from '~/modules/action';
+import { Entity } from '~/modules/entity';
+import { CreateTripleAction, StringValue, Triple } from '~/modules/types';
+import { MockNetwork, makeStubTriple } from '../mock-network';
+import { MergeDataSource } from './merge-data-source';
+
+describe('MergeDataSource merges local triples with network triples', () => {
+  // Right now we don't filter locally created triples in fetchTriples. This means that we may return extra
+  // triples that do not match the passed in query + filter.
+  it('merges local and network triples', async () => {
+    const stubTriple = makeStubTriple('Alice');
+
+    const api = new MockNetwork({ triples: [stubTriple] });
+    const store = new ActionsStore({ api: api });
+    store.remove(stubTriple);
+
+    const changedLocalTriple: Triple = {
+      ...stubTriple,
+      entityName: 'Bob',
+      value: { ...stubTriple.value, value: 'Bob' } as StringValue,
+    };
+    store.create(changedLocalTriple);
+
+    const mergedNetwork = new MergeDataSource({ api, store });
+    const triples = await mergedNetwork.fetchTriples({
+      space: stubTriple.space,
+      query: '',
+      first: 10,
+      skip: 0,
+      filter: [],
+    });
+
+    const changedLocalTripleAsAction: CreateTripleAction = {
+      type: 'createTriple',
+      ...changedLocalTriple,
+    };
+
+    expect(triples.triples).toEqual([changedLocalTripleAsAction]);
+  });
+});
+
+describe('MergeDataSource merges local entities with network entities', () => {
+  it('without a query', async () => {
+    const stubTriple = makeStubTriple('Alice');
+
+    const api = new MockNetwork({ triples: [stubTriple] });
+    const store = new ActionsStore({ api: api });
+    store.remove(stubTriple);
+
+    const changedLocalTriple: Triple = {
+      ...stubTriple,
+      entityName: 'Bob',
+      value: { ...stubTriple.value, value: 'Bob' } as StringValue,
+    };
+    store.create(changedLocalTriple);
+
+    const mergedNetwork = new MergeDataSource({ api, store });
+    const entities = await mergedNetwork.fetchEntities({
+      space: stubTriple.space,
+      query: '',
+      filter: [],
+    });
+
+    const changedLocalTripleAsAction: CreateTripleAction = {
+      type: 'createTriple',
+      ...changedLocalTriple,
+    };
+
+    expect(entities).toEqual(Entity.entitiesFromTriples([changedLocalTripleAsAction]));
+  });
+
+  it('with a query', async () => {
+    const stubTriple = makeStubTriple('Alice');
+
+    const api = new MockNetwork({ triples: [stubTriple] });
+    const store = new ActionsStore({ api: api });
+    store.remove(stubTriple);
+
+    const changedLocalTriple: Triple = {
+      ...stubTriple,
+      entityName: 'Bob',
+      value: { ...stubTriple.value, value: 'Bob' } as StringValue,
+    };
+    store.create(changedLocalTriple);
+
+    const mergedNetwork = new MergeDataSource({ api, store });
+    const entities = await mergedNetwork.fetchEntities({
+      space: stubTriple.space,
+      query: 'Bob',
+      filter: [],
+    });
+
+    const changedLocalTripleAsAction: CreateTripleAction = {
+      type: 'createTriple',
+      ...changedLocalTriple,
+    };
+
+    expect(entities).toEqual(Entity.entitiesFromTriples([changedLocalTripleAsAction]));
+  });
+});
+
+// @TODO: Use update instead of create/remove
+describe('MergeDataSource merges local entity with network entity', () => {
+  // This should take the local version of the entity
+  it('local entity and network entity both exist', async () => {
+    const stubTriple = makeStubTriple('Alice');
+
+    const api = new MockNetwork({ triples: [stubTriple] });
+    const store = new ActionsStore({ api: api });
+    store.remove(stubTriple);
+
+    const changedLocalTriple: Triple = {
+      ...stubTriple,
+      entityName: 'Bob',
+      value: { ...stubTriple.value, value: 'Bob' } as StringValue,
+    };
+    store.create(changedLocalTriple);
+
+    const mergedNetwork = new MergeDataSource({ api, store });
+    const entity = await mergedNetwork.fetchEntity(stubTriple.id);
+
+    const changedLocalTripleAsAction: CreateTripleAction = {
+      type: 'createTriple',
+      ...changedLocalTriple,
+    };
+
+    expect(entity).toEqual(Entity.entitiesFromTriples([changedLocalTripleAsAction])[0]);
+  });
+
+  // This should take the local entity
+  it('local entity exists and network entity does not exist', async () => {
+    const stubTriple = makeStubTriple('Devin');
+
+    const api = new MockNetwork({ triples: [stubTriple] });
+    const store = new ActionsStore({ api: api });
+
+    const changedLocalTriple: Triple = makeStubTriple('Bob');
+    store.create(changedLocalTriple);
+
+    const mergedNetwork = new MergeDataSource({ api, store });
+    const entity = await mergedNetwork.fetchEntity(changedLocalTriple.id);
+
+    const changedLocalTripleAsAction: CreateTripleAction = {
+      type: 'createTriple',
+      ...changedLocalTriple,
+    };
+
+    expect(entity).toEqual(Entity.entitiesFromTriples([changedLocalTripleAsAction])[0]);
+  });
+
+  it('local entity does not exist and network entity exists', async () => {
+    const stubTriple = makeStubTriple('Devin');
+
+    const api = new MockNetwork({ triples: [stubTriple] });
+    const store = new ActionsStore({ api: api });
+
+    const mergedNetwork = new MergeDataSource({ api, store });
+    const entity = await mergedNetwork.fetchEntity(stubTriple.id);
+
+    console.log('entites', Entity.entitiesFromTriples([stubTriple]));
+    console.log('entity', entity);
+
+    expect(entity).toEqual(Entity.entitiesFromTriples([stubTriple])[0]);
+  });
+
+  it('local entity does not exist and network does not exist', async () => {
+    const stubTriple = makeStubTriple('Devin');
+
+    const api = new MockNetwork({ triples: [stubTriple] });
+    const store = new ActionsStore({ api: api });
+
+    const mergedNetwork = new MergeDataSource({ api, store });
+    const entity = await mergedNetwork.fetchEntity('Banana');
+
+    expect(entity).toEqual(null);
+  });
+});
---
+++ apps/web/modules/services/io/merge-data-source.ts
@@ -0,0 +1,116 @@
+import { A, G, pipe } from '@mobily/ts-belt';
+import { ActionsStore } from '~/modules/action';
+import { Entity } from '~/modules/entity';
+import { Triple } from '~/modules/triple';
+import { OmitStrict } from '~/modules/types';
+import { INetwork } from '../network';
+
+interface MergeDataSourceOptions {
+  api: INetwork;
+  store: ActionsStore;
+}
+
+interface IMergeDataSource extends OmitStrict<INetwork, 'publish' | 'uploadFile'> {}
+
+export class MergeDataSource implements IMergeDataSource {
+  private api: INetwork;
+  private store: ActionsStore;
+
+  constructor({ api, store }: MergeDataSourceOptions) {
+    this.api = api;
+    this.store = store;
+  }
+
+  // Right now we don't filter locally created triples in fetchTriples. This means that we may return extra
+  // triples that do not match the passed in query + filter.
+  fetchTriples = async (options: Parameters<INetwork['fetchTriples']>[0]) => {
+    const networkTriples = await this.api.fetchTriples(options);
+
+    if (!options.space) return networkTriples;
+
+    const actions = this.store.actions$.get()[options.space] ?? [];
+
+    // We want to merge any local actions with the network triples
+    // @TODO: Do local actions need to have filters applied to them? Right now we aren't doing
+    // this in our app code for local triples. This might mean that we render local triples that
+    // don't map to the selected filter.
+    const updatedTriples = Triple.fromActions(actions, networkTriples.triples);
+    const mergedTriplesWithName = Triple.withLocalNames(actions, updatedTriples);
+
+    return {
+      triples: mergedTriplesWithName,
+    };
+  };
+
+  fetchEntities = async (options: Parameters<INetwork['fetchEntities']>[0]) => {
+    const networkEntities = await this.api.fetchEntities(options);
+
+    // @TODO: Do local actions need to have filters applied to them? Right now we aren't doing
+    // this in our app code for local entities. This might mean that we render local entities that
+    // don't map to the selected filter.
+    const localEntities = pipe(
+      this.store.actions$.get(),
+      actions => Entity.mergeActionsWithEntities(actions, networkEntities),
+      A.filter(e => {
+        if (!G.isString(e.name)) {
+          return false;
+        }
+
+        const lowerName = e.name.toLowerCase();
+        const lowerQuery = options.query ? options.query.toLowerCase() : '';
+        return lowerName.startsWith(lowerQuery) || lowerName.includes(lowerQuery);
+      })
+    );
+
+    // We want to favor the local version of an entity if it exists on the network already.
+    const localEntityIds = new Set(localEntities.map(e => e.id));
+
+    // This will put the local entities first, and then the network entities that don't exist locally.
+    // This might not be the ideal UX.
+    return [...localEntities, ...networkEntities.filter(e => !localEntityIds.has(e.id))];
+  };
+
+  /**
+   * Merge the local version of an entity with the network version of an entity if they exist.
+   * This is necessary because the local version of an entity might have actions that haven't been
+   * published yet.
+   *
+   * States:
+   * * Local entity exists, network entity exists: Merge the local entity with the network entity
+   * * Local entity exists, network entity doesn't exist: Return the local entity
+   * * Local entity doesn't exist, network entity exists: Return the network entity
+   * * Local entity doesn't exist, network entity doesn't exist: Return null
+   *
+   */
+  fetchEntity = async (id: Parameters<INetwork['fetchEntity']>[0]) => {
+    const maybeNetworkEntity = await this.api.fetchEntity(id);
+
+    const globalActions = Triple.fromActions(
+      Object.values(this.store.actions$.get()).flatMap(a => a),
+      []
+    );
+
+    if (!globalActions.some(a => a.entityId === id)) return maybeNetworkEntity;
+
+    // Need to find the local version of this entity if it exists and merge it with the network entity
+    // if it exists.
+    const entity = pipe(
+      this.store.actions$.get(),
+      actions => Entity.mergeActionsWithEntities(actions, maybeNetworkEntity ? [maybeNetworkEntity] : []),
+      A.find(e => e.id === id)
+    );
+
+    if (!entity) {
+      return null;
+    }
+
+    return entity;
+  };
+
+  columns = async (options: Parameters<INetwork['columns']>[0]) => this.api.columns(options);
+  rows = async (options: Parameters<INetwork['rows']>[0]) => this.api.rows(options);
+
+  fetchSpaces = async () => this.api.fetchSpaces();
+
+  fetchProfile = async () => null;
+}
