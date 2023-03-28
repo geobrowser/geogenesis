be3dd84fb73d179a12746ca896f5618d58df4ecd
sandren <20441876+sandren@users.noreply.github.com>
2023-03-28 09:45:22 -0700

    fix: only persist unpublished actions

--- apps/web/modules/persistence.tsx
+++ apps/web/modules/persistence.tsx
@@ -3,10 +3,17 @@ import { useState, useEffect } from 'react';
 
 import { useActionsStore } from './action';
 import { useLocalStorage } from './hooks/use-local-storage';
+import type { SpaceActions } from './action/actions-store';
 
 export const Persistence = () => {
   const [isInitialRender, setIsInitialRender] = useState<boolean>(true);
   const { actions, restore } = useActionsStore();
+
+  const unpublishedActions: SpaceActions = {};
+  Object.keys(actions).forEach(key => {
+    unpublishedActions[key] = actions[key].filter(a => !a.hasBeenPublished);
+  });
+
   const [storedActions, setStoredActions] = useLocalStorage('storedActions', {});
 
   // Restore actions on first render, save actions each render thereafter
@@ -16,7 +23,7 @@ export const Persistence = () => {
       restore(storedActions);
     } else {
       console.info('💾 saving actions');
-      setStoredActions(actions);
+      setStoredActions(unpublishedActions);
     }
     // note: finely tuned dependency because `storedActions` is only used on initial render
     // no need to save stored actions a second time when storedactions are updated
