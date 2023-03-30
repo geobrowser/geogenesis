6eaa4ea627c35fb52c4b0decfdb6ed6beb894e36
Byron Guina <byron.guina@gmail.com>
2023-03-30 16:30:32 -0600

    Fix responsiveness on entity page

--- apps/web/modules/components/entity/entity-page-content-container.tsx
+++ apps/web/modules/components/entity/entity-page-content-container.tsx
@@ -1,10 +1,12 @@
+import * as React from 'react';
+
 interface Props {
   children: React.ReactNode;
 }
 
 export function EntityPageContentContainer({ children }: Props) {
   return (
-    <div className="m-auto flex min-h-full w-[784px] flex-col items-center">
+    <div className="m-auto flex min-h-full max-w-[784px] flex-col items-center">
       <div className="w-full">{children}</div>
     </div>
   );
