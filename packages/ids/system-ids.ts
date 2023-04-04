085b2426132601f08416a3ba9c1555056c7c60e1
Byron Guina <byron.guina@gmail.com>
2023-04-04 14:57:20 -0600

    feat: bootstrap avatar and cover schemas

--- packages/ids/system-ids.ts
+++ packages/ids/system-ids.ts
@@ -32,25 +32,9 @@ export const DEFAULT_TYPE = 'aeebbd5e-4d79-4d24-ae99-239e9142d9ed'
 
 export const PERSON_ATTRIBUTE = '626e4ad5-61c3-49ae-af5e-3c80e53cf890'
 
-/**
- * Addresses for important contracts on Polygon mainnet.
- *
- * Note: If you want to test deployments on a different network (e.g. local or Mumbai),
- * you can update these addresses to point to the correct contracts on that network.
- */
+export const AVATAR_ATTRIBUTE = '235ba0e8-dc7e-4bdd-a1e1-6d0d4497f133'
 
-// This represents the beacon proxy for the first set of deployed permissioned spaces.
-// We should use this beacon proxy for all new permissioned spaces.
-export const PERMISSIONED_SPACE_BEACON_ADDRESS =
-  '0xe44Be15e413169Ad49fB24CBF8db192BE5A9A8bF'
-
-// This represents the Space contract acting as the registry for all permissioned spaces.
-// This is the address for the Root Space.
-export const PERMISSIONED_SPACE_REGISTRY_ADDRESS =
-  '0x170b749413328ac9a94762031a7A05b00c1D2e34'
-
-// This represents the beacon proxy for all permissionless spaces.
-export const PERMISSIONLESS_SPACE_BEACON_ADDRESS = ''
+export const COVER_ATTRIBUTE = '34f53507-2e6b-42c5-a844-43981a77cfa2'
 
 /* Example Usage: SF_Config -> FOREIGN_TYPES -> Some_Entity */
 export const FOREIGN_TYPES = 'be745973-05a9-4cd0-a46d-1c5538270faf'
@@ -82,6 +66,26 @@ export const MARKDOWN_CONTENT = 'f88047ce-bd8d-4fbf-83f6-58e84ee533e4'
 Block Entity -> TYPES -> TABLE_BLOCK
 Block Entity -> ROW_TYPE -> Some_Type_ID */
 export const ROW_TYPE = '577bd9fb-b29e-4e2b-b5f8-f48aedbd26ac'
+
+/**
+ * Addresses for important contracts on Polygon mainnet.
+ *
+ * Note: If you want to test deployments on a different network (e.g. local or Mumbai),
+ * you can update these addresses to point to the correct contracts on that network.
+ */
+
+// This represents the beacon proxy for the first set of deployed permissioned spaces.
+// We should use this beacon proxy for all new permissioned spaces.
+export const PERMISSIONED_SPACE_BEACON_ADDRESS =
+  '0xe44Be15e413169Ad49fB24CBF8db192BE5A9A8bF'
+
+// This represents the Space contract acting as the registry for all permissioned spaces.
+// This is the address for the Root Space.
+export const PERMISSIONED_SPACE_REGISTRY_ADDRESS =
+  '0x170b749413328ac9a94762031a7A05b00c1D2e34'
+
+// This represents the beacon proxy for all permissionless spaces.
+export const PERMISSIONLESS_SPACE_BEACON_ADDRESS = ''
 
 // This represents the PermissionlessSpace contract acting as the registry for all
 // permissionless spaces.
@@ -118,7 +122,3 @@ export const PERMISSIONLESS_SPACE_REGISTRY_ADDRESS = ''
     },
   ]
 */
-
-export const AVATAR_ATTRIBUTE = '235ba0e8-dc7e-4bdd-a1e1-6d0d4497f133'
-
-export const COVER_ATTRIBUTE = '34f53507-2e6b-42c5-a844-43981a77cfa2'
