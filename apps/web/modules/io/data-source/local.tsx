8b42c2f10d061f9f5ecd1531b0a2009d1e7050bb
Byron Guina <byron.guina@gmail.com>
2023-04-05 11:36:01 -0600

    Update flowbar to use new local store

--- apps/web/modules/io/data-source/local.tsx
+++ apps/web/modules/io/data-source/local.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import { createContext, useContext, useMemo } from 'react';
-import { ObservableComputed, computed, observable, observe } from '@legendapp/state';
+import { ObservableComputed, computed, observable } from '@legendapp/state';
 import { ActionsStore, useActionsStoreContext } from '../../action';
 import { Entity as IEntity, Triple as ITriple } from '../../types';
 import { makeOptionalComputed } from '../../utils';
 import { pipe } from '@mobily/ts-belt';
 import { Triple } from '../../triple';
 import { Entity } from '../../entity';
+import { useSelector } from '@legendapp/state/react';
 
 export class LocalStore {
-  store: ActionsStore;
+  private store: ActionsStore;
   triples$: ObservableComputed<ITriple[]> = observable([]);
   entities$: ObservableComputed<IEntity[]> = observable([]);
+  spaces$: ObservableComputed<string[]> = observable<string[]>([]);
 
   constructor({ store }: { store: ActionsStore }) {
     this.store = store;
@@ -32,13 +34,13 @@ export class LocalStore {
       })
     );
 
-    observe(() => {
-      const entities = this.entities$.get();
-      const triples = this.triples$.get();
-
-      console.log('entities', entities);
-      console.log('triples', triples);
-    });
+    this.spaces$ = makeOptionalComputed(
+      [],
+      computed(() => {
+        const allSpaces = this.triples$.get().map(t => t.space);
+        return [...new Set(allSpaces)];
+      })
+    );
   }
 }
 
@@ -58,7 +60,7 @@ export function LocalStoreProvider({ children }: Props) {
   return <LocalStoreContext.Provider value={store}>{children}</LocalStoreContext.Provider>;
 }
 
-export function useGeoStoreContext() {
+export function useLocalStoreContext() {
   const value = useContext(LocalStoreContext);
 
   if (!value) {
@@ -66,4 +68,18 @@ export function useGeoStoreContext() {
   }
 
   return value;
+}
+
+export function useLocalStore() {
+  const { entities$, triples$, spaces$ } = useLocalStoreContext();
+
+  const entities = useSelector(entities$);
+  const triples = useSelector(triples$);
+  const spaces = useSelector(spaces$);
+
+  return {
+    entities,
+    triples,
+    spaces,
+  };
 }
