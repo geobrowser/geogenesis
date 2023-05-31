d2bc701af0c00365e5b895f6c540b4874b3b8a17
Byron Guina <byron.guina@gmail.com>
2023-05-31 09:27:00 -0600

    update copy

--- apps/web/modules/components/entity-page/entity-page-delete-entity-modal.tsx
+++ apps/web/modules/components/entity-page/entity-page-delete-entity-modal.tsx
@@ -10,24 +10,24 @@ interface Props {
 }
 
 export function EntityPageDeleteEntityModal({ trigger }: Props) {
+  const onCancel = () => {};
+
   return (
     <Root>
       <Trigger asChild>{trigger}</Trigger>
       <Portal>
-        <Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-text opacity-20" />
-        <Content className="fixed top-[25%] left-[50%] max-h-[85vh] w-[90vw] max-w-[455px] translate-x-[-50%] translate-y-[-25%] rounded bg-white p-4 focus:outline-none">
+        <Overlay className="fixed inset-0 bg-text opacity-20" />
+        <Content className="fixed top-[25%] left-[50%] max-h-[85vh] w-[90vw] max-w-[455px] translate-x-[-50%] translate-y-[-25%] rounded bg-white p-4 shadow-card focus:outline-none">
           <h1 className="text-center text-metadataMedium">Delete entity</h1>
           <Spacer height={16} />
           <p>
-            This will delete all attributes and values assigned to this entity. Deleting this entity may have unexpected
-            side-effects as any entities referencing this entity will be broken.
+            This will delete all triples in this entity. Deleting this entity may have unexpected side-effects as any
+            entities referencing this entity will be broken.
           </p>
           <Spacer height={32} />
           <div className="flex items-center justify-between">
             <button className="text-button text-grey-04 transition-colors duration-75 hover:text-text">Cancel</button>
-            <Button variant="secondary" icon="trash">
-              Delete
-            </Button>
+            <Button variant="tertiary">Delete</Button>
           </div>
         </Content>
       </Portal>
