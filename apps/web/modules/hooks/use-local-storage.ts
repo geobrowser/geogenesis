37992848a3a376504759cd2e7be0ff1a9fe612c8
Byron Guina <byron.guina@gmail.com>
2023-06-06 09:34:04 -0600

    Revert "feat: sync local storage across tabs (#359)"
    This reverts commit ee55528c4a14b17faacdacc76aa012117bfd4d9c.

--- apps/web/modules/hooks/use-local-storage.ts
+++ apps/web/modules/hooks/use-local-storage.ts
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-export function useLocalStorage<T>(key: string, initialValue: T, callback?: (value: T) => void) {
+export function useLocalStorage<T>(key: string, initialValue: T) {
   const [storedValue, setStoredValue] = useState<T>(() => {
     if (typeof window === 'undefined') {
       return initialValue;
@@ -9,41 +9,20 @@ export function useLocalStorage<T>(key: string, initialValue: T, callback?: (val
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
-      console.error(`Cannot read key: ${key} from local storage during initialization`, error);
+      console.log(error);
       return initialValue;
     }
   });
-
   const setValue = (value: T | ((val: T) => T)) => {
     try {
       const valueToStore = value instanceof Function ? value(storedValue) : value;
       setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
-      console.error(`Cannot set key: ${key} in local storage`, error);
+      console.log(error);
     }
   };
-
-  // Sync state across tabs
-  useEffect(() => {
-    const readLocalStore = (event: StorageEvent) => {
-      if (event?.key && event.key !== key) {
-        return;
-      }
-
-      try {
-        const item = window.localStorage.getItem(key);
-        const value = item ? JSON.parse(item) : initialValue;
-        callback?.(value);
-      } catch (error) {
-        console.error(`Cannot read key: ${key} from local storage`, error);
-        return initialValue;
-      }
-    };
-
-    window.addEventListener('storage', readLocalStore);
-    return () => window.removeEventListener('storage', readLocalStore);
-  }, [initialValue, callback, key]);
-
   return [storedValue, setValue] as const;
 }
