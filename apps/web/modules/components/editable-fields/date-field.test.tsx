c0cdec7e1b02ab23a86fa13cb83c781ad17aabc6
Byron Guina <byron.guina@gmail.com>
2023-06-21 14:02:20 -0600

    unused import

--- apps/web/modules/components/editable-fields/date-field.test.tsx
+++ apps/web/modules/components/editable-fields/date-field.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { DateField } from './date-field';
 
