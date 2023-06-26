783e7d4b031b20ec3b425a0a95ae31f4ff113289
Byron <26263630+baiirun@users.noreply.github.com>
2023-06-26 14:12:38 -0600

    fix: use staging subgraph until prod is fixed (#379)

--- apps/web/modules/config/config.ts
+++ apps/web/modules/config/config.ts
@@ -34,7 +34,7 @@ export const options: Record<AppEnv, AppConfig> = {
     chainId: '137',
     rpc: 'https://polygon-rpc.com',
     ipfs: 'https://api.thegraph.com/ipfs',
-    subgraph: 'https://api.thegraph.com/subgraphs/name/baiirun/geo',
+    subgraph: 'https://api.thegraph.com/subgraphs/name/baiirun/geo-staging',
   },
 };
 
