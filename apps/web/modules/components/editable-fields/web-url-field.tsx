62e3d06287ba458f2adbbc89f6cdf4e91e1cf33d
Byron Guina <byron.guina@gmail.com>
2023-06-29 10:59:04 -0600

    add web url value type in bootstrap

--- apps/web/modules/components/editable-fields/web-url-field.tsx
+++ apps/web/modules/components/editable-fields/web-url-field.tsx
@@ -1,20 +1,23 @@
 import { cva } from 'class-variance-authority';
 import * as React from 'react';
 
-const webUrlFieldStyles = cva('w-full bg-transparent placeholder:text-grey-02 focus:outline-none', {
-  variants: {
-    variant: {
-      body: 'text-body',
-      tableCell: 'text-tableCell',
+const webUrlFieldStyles = cva(
+  'w-full bg-transparent placeholder:text-grey-02 focus:outline-none no-underline hover:underline',
+  {
+    variants: {
+      variant: {
+        body: 'text-body',
+        tableCell: 'text-tableCell',
+      },
+      editable: {
+        false: 'text-ctaPrimary hover:text-ctaHover underline transition-colors duration-75 truncate',
+      },
     },
-    editable: {
-      false: 'text-ctaPrimary hover:text-ctaHover underline transition-colors duration-75 truncate',
+    defaultVariants: {
+      variant: 'body',
     },
-  },
-  defaultVariants: {
-    variant: 'body',
-  },
-});
+  }
+);
 
 interface Props {
   isEditing?: boolean;
