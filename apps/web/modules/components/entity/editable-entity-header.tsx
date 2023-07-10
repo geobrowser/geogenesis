beadb80d61f16e978fa2bc2a620c9749675edb38
Byron Guina <byron.guina@gmail.com>
2023-07-10 11:17:51 -0600

    align type list on entity pages

--- apps/web/modules/components/entity/editable-entity-header.tsx
+++ apps/web/modules/components/entity/editable-entity-header.tsx
@@ -76,7 +76,7 @@ export function EditableHeading({
             the entity name. We can't perfectly match the height of the normal <Text /> field with the textarea, so we
             have to manually adjust the spacing here to remove the layout shift.
           */}
-          <Spacer height={2} />
+          <Spacer height={3.5} />
         </div>
       ) : (
         <div>
