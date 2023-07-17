01894c9b88209c0e3944209b3648103d33c84d82
Byron Guina <byron.guina@gmail.com>
2023-07-17 12:42:01 -0600

    idk

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -12,24 +12,42 @@ import '../styles/tiptap.css';
 //       <link rel="preload" as="image" href={DEFAULT_OPENGRAPH_IMAGE} />
 
 //       <link rel="apple-touch-icon" sizes="76x76" href="/static/apple-touch-icon.png" />
-//       <link rel="shortcut icon" type="image/png" href="/static/favicon.png" />
-//       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-//       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-//       <link rel="manifest" href="/static/site.webmanifest" />
-//       <link rel="mask-icon" href="/static/favicon-16x16.png" color="#FBFBFB" />
-//       <meta name="msapplication-TileColor" content="#FBFBFB" />
 
 //       {/* Less essential */}
 //       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://geobrowser.io'),
+  metadataBase: new URL(process.env.ENV_URL!),
   title: 'Geo Genesis',
   themeColor: [
     { media: '(prefers-color-scheme: dark)', color: '#FBFBFB' },
     { media: '(prefers-color-scheme: light)', color: '#FBFBFB' },
   ],
   description: "Browse and organize the world's public knowledge and information in a decentralized way.",
+  manifest: '/static/site.webmanifest',
+  icons: {
+    icon: '/static/favicon.png',
+    shortcut: '/static/favicon.png',
+    apple: '/static/apple-icon.png',
+    other: [
+      {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/static/apple-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/static/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/static/favicon-16x16.png',
+      },
+    ],
+  },
   twitter: {
     card: 'summary_large_image',
     title: 'Geo Genesis',
@@ -71,6 +89,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" as="image" href={DEFAULT_OPENGRAPH_IMAGE} />
+      </head>
       <body>
         <div className="relative">
           <Providers>
