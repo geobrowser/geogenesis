ceaffebb5ae6f8826cc9b900e45c8206d4e6e140
Byron Guina <byron.guina@gmail.com>
2023-07-25 18:07:42 -0600

    add toggle animations

--- apps/web/modules/components/navbar/navbar-actions.tsx
+++ apps/web/modules/components/navbar/navbar-actions.tsx
@@ -1,11 +1,17 @@
+'use client';
+
 import * as React from 'react';
 import { useAccount } from 'wagmi';
+import { motion } from 'framer-motion';
 
 import { GeoConnectButton } from '~/modules/wallet';
 import { Avatar } from '~/modules/design-system/avatar';
 import { Menu } from '~/modules/design-system/menu';
 import { useQuery } from '@tanstack/react-query';
 import { Services } from '~/modules/services';
+import { useEditable } from '~/modules/stores/use-editable';
+import { EyeSmall } from '~/modules/design-system/icons/eye-small';
+import { BulkEdit } from '~/modules/design-system/icons/bulk-edit';
 
 function useUserProfile(address?: string) {
   const { network } = Services.useServices();
@@ -33,6 +39,8 @@ export function NavbarActions() {
 
   return (
     <div className="flex items-center gap-4">
+      <ModeToggle />
+
       <Menu
         trigger={
           <div className="relative h-7 w-7 overflow-hidden rounded-full">
@@ -56,5 +64,49 @@ function AvatarMenuItem({ children }: { children: React.ReactNode }) {
     <div className="flex select-none items-center justify-between bg-white py-2 px-3 text-button text-grey-04 hover:bg-bg hover:text-text hover:outline-none aria-disabled:cursor-not-allowed aria-disabled:text-grey-04">
       {children}
     </div>
+  );
+}
+
+function ModeToggle() {
+  const { editable, setEditable } = useEditable();
+
+  return (
+    <button
+      onClick={() => setEditable(!editable)}
+      className="flex w-[66px] items-center justify-between rounded-[47px] bg-divider p-1"
+    >
+      <div className="relative flex h-5 w-7 items-center justify-center rounded-[44px]">
+        {editable && (
+          <motion.div
+            transition={{
+              type: 'spring',
+              duration: 0.15,
+              bounce: 0,
+            }}
+            layoutId="edit-toggle"
+            className="absolute h-5 w-7 rounded-[44px] bg-white shadow-dropdown"
+          />
+        )}
+        <motion.div className={`z-10 transition-colors duration-500 ${editable ? 'text-text' : 'text-grey-03'}`}>
+          <EyeSmall />
+        </motion.div>
+      </div>
+      <div className="flex h-5 w-7 items-center justify-center rounded-[44px]">
+        {!editable && (
+          <motion.div
+            transition={{
+              type: 'spring',
+              duration: 0.15,
+              bounce: 0,
+            }}
+            layoutId="edit-toggle"
+            className="absolute h-5 w-7 rounded-[44px] bg-white shadow-dropdown"
+          />
+        )}
+        <motion.div className={`z-10 transition-colors duration-500 ${!editable ? 'text-text' : 'text-grey-03'}`}>
+          <BulkEdit />
+        </motion.div>
+      </div>
+    </button>
   );
 }
---
+++ apps/web/modules/design-system/icons/eye-small.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import { colors, ColorName } from '~/modules/design-system/theme/colors';
+
+interface Props {
+  color?: ColorName;
+}
+
+export function EyeSmall({ color }: Props) {
+  const themeColor = color ? colors.light[color] : 'currentColor';
+
+  return (
+    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <mask id="path-1-inside-1_13611_139414" fill="white">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M-2.07127e-06 6C1.41899 8.28856 3.57974 9.74999 6 9.74999C8.42025 9.74999 10.581 8.28856 12 6C10.581 3.71144 8.42025 2.25 6 2.25C3.57974 2.25 1.41899 3.71144 -2.07127e-06 6Z"
+        />
+      </mask>
+      <path
+        d="M-2.07127e-06 6L-0.84989 5.47303L-1.17663 6L-0.84989 6.52696L-2.07127e-06 6ZM12 6L12.8499 6.52696L13.1766 6L12.8499 5.47303L12 6ZM6 8.74999C4.01296 8.74999 2.13772 7.55006 0.849886 5.47303L-0.84989 6.52696C0.700267 9.02706 3.14653 10.75 6 10.75L6 8.74999ZM11.1501 5.47303C9.86228 7.55006 7.98704 8.74999 6 8.74999L6 10.75C8.85347 10.75 11.2997 9.02706 12.8499 6.52696L11.1501 5.47303ZM6 3.25C7.98704 3.25 9.86228 4.44994 11.1501 6.52696L12.8499 5.47303C11.2997 2.97294 8.85347 1.25 6 1.25L6 3.25ZM0.849886 6.52696C2.13772 4.44994 4.01296 3.25 6 3.25L6 1.25C3.14653 1.25 0.700268 2.97294 -0.84989 5.47303L0.849886 6.52696Z"
+        fill={themeColor}
+        mask="url(#path-1-inside-1_13611_139414)"
+      />
+      <circle cx="6" cy="6" r="1.75" stroke={themeColor} />
+    </svg>
+  );
+}
