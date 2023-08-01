0d2ff78c0fad9a2ed9e1fb4d632dceadb7b4bebb
Byron Guina <byron.guina@gmail.com>
2023-08-01 09:48:18 -0600

    initial skeleton for profile page

--- apps/web/app/profile/[id]/page.tsx
+++ apps/web/app/profile/[id]/page.tsx
@@ -1,9 +1,40 @@
+import { EntityStoreProvider } from '~/core/state/entity-page-store';
+
+import { EditableHeading } from '~/partials/entity-page/editable-entity-header';
+import { EntityPageContentContainer } from '~/partials/entity-page/entity-page-content-container';
+import { EntityPageCover } from '~/partials/entity-page/entity-page-cover';
+
 export const runtime = 'edge';
 
-export default function ProfilePage() {
+const MOCK_PROFILE = {
+  id: '0x123',
+  name: 'John Doe',
+  avatarUrl: 'https://avatars.githubusercontent.com/u/10001?v=4',
+  coverUrl: 'https://avatars.githubusercontent.com/u/10001?v=4',
+  spaceId: '0x123',
+};
+
+export default async function ProfilePage() {
+  const profile = await fetchProfile();
+
   return (
-    <div>
-      <h1>Profile Page</h1>
-    </div>
+    <EntityStoreProvider
+      id={profile.id}
+      spaceId={profile.spaceId}
+      initialTriples={[]}
+      initialSchemaTriples={[]}
+      initialBlockIdsTriple={null}
+      initialBlockTriples={[]}
+    >
+      <EntityPageCover avatarUrl={profile.avatarUrl} coverUrl={profile.coverUrl} />
+
+      <EntityPageContentContainer>
+        <EditableHeading spaceId={profile.spaceId} entityId={profile.id} name={profile.name} triples={[]} space />
+      </EntityPageContentContainer>
+    </EntityStoreProvider>
   );
+}
+
+async function fetchProfile() {
+  return MOCK_PROFILE;
 }
