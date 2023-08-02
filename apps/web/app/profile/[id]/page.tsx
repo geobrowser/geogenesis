dfad6e0b3cc1b97d64960db2a3852ea926f00bb4
Byron Guina <byron.guina@gmail.com>
2023-08-02 12:58:06 -0600

    PR cleanup

--- apps/web/app/profile/[id]/page.tsx
+++ apps/web/app/profile/[id]/page.tsx
@@ -1,7 +1,5 @@
-import { cookies } from 'next/headers';
-
-import { Cookie } from '~/core/cookie';
-
+// import { cookies } from 'next/headers';
+// import { Cookie } from '~/core/cookie';
 import { setOnboardingDismissedCookie } from '~/partials/profile/actions';
 
 import { ProfilePageComponent } from './component';
@@ -15,14 +13,12 @@ interface Props {
 
 export default async function ProfilePage({ params }: Props) {
   const profile = await getProfilePage({ params });
-  const hasDismissedOnboarding = cookies().get(Cookie.HAS_DISMISSED_PERSONAL_SPACE_ONBOARDING_KEY)?.value === 'true';
+  // @TODO: Disabling cookie interactions for now until we get later on in the social
+  // work. This is so we can test onboarding feedback more frequently.
+  // const hasDismissedOnboarding = cookies().get(Cookie.HAS_DISMISSED_PERSONAL_SPACE_ONBOARDING_KEY)?.value === 'true';
 
   return (
-    <ProfilePageComponent
-      {...profile}
-      onDismissForever={setOnboardingDismissedCookie}
-      hasDismissedOnboarding={hasDismissedOnboarding}
-    />
+    <ProfilePageComponent {...profile} onDismissForever={setOnboardingDismissedCookie} hasDismissedOnboarding={false} />
   );
 }
 
