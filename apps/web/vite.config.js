412aa71eafbe6dd95107ba8767e9d4d37c6a0a34
Byron Guina <byron.guina@gmail.com>
2023-08-15 15:04:05 -0600

    fix linting errors

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -12,6 +12,13 @@ export default defineConfig({
       '~': path.resolve(__dirname, './'),
     },
     setupFiles: ['./setupTests.ts'],
-    exclude: ['**/node_modules/**', '**/dist/**', '**/e2e/*', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*']
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/*',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+    ],
   },
 });
