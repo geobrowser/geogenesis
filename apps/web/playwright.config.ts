18a4624ab017f575c6d819e4c0e14830e1428309
Byron Guina <byron.guina@gmail.com>
2023-08-16 09:57:22 -0600

    mocked wallet
    add test env

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm start',
+    command: 'NODE_ENV=test pnpm start',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
