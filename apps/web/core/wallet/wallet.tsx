e06126fa26a87a8c1ae8725179acb3e170e68291
Byron Guina <byron.guina@gmail.com>
2023-08-18 17:27:36 -0600

    feat: add higher resolution favicon for walletconnect v2

--- apps/web/core/wallet/wallet.tsx
+++ apps/web/core/wallet/wallet.tsx
@@ -103,7 +103,7 @@ const createRealWalletConfig = () => {
             name: 'Geo Genesis',
             description: "Browse and organize the world's public knowledge and information in a decentralized way.",
             url: 'https://geobrowser.io',
-            icons: ['https://geobrowser.io/static/favicon-32x32.png'],
+            icons: ['https://geobrowser.io/static/favicon-64x64.png'],
           },
         },
       }),
