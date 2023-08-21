6ad6b703d1ffc103103184fb388e4c59b9d23c70
Byron <26263630+baiirun@users.noreply.github.com>
2023-08-21 12:41:03 -0600

    feat: e2e test for editable state (#458)

--- apps/web/core/wallet/wallet.tsx
+++ apps/web/core/wallet/wallet.tsx
@@ -58,7 +58,7 @@ const getMockWalletClient = () =>
   createWalletClient({
     transport: http(polygon.rpcUrls.default.http[0]),
     chain: polygon,
-    account: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+    account: '0x66703c058795B9Cb215fbcc7c6b07aee7D216F24',
     key: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
     pollingInterval: 100,
   });
