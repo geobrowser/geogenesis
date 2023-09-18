ebf52ef670db3444a45423cbaefa561c66666d2a
Byron Guina <byron.guina@gmail.com>
2023-09-18 10:07:06 -0600

    feat: governance metadata boxes

--- apps/web/app/space/[id]/governance/page.tsx
+++ apps/web/app/space/[id]/governance/page.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 import { ServerSideEnvParams } from '~/core/types';
 
 import { SpaceLayout } from '../space-layout';
@@ -11,7 +13,32 @@ export default async function GovernancePage({ params, searchParams }: Props) {
   return (
     // @ts-expect-error async JSX function
     <SpaceLayout params={params} searchParams={searchParams}>
-      Hello world
+      <div className="flex items-center gap-5">
+        <GovernanceMetadataBox>
+          <h2 className="text-metadata text-grey-04">Voting period</h2>
+          <p className="text-mediumTitle">24h</p>
+        </GovernanceMetadataBox>
+        <GovernanceMetadataBox>
+          <h2 className="text-metadata text-grey-04">Pass threshold</h2>
+          <p className="text-mediumTitle">51%</p>
+        </GovernanceMetadataBox>
+        <GovernanceMetadataBox>
+          <h2 className="text-metadata text-grey-04">Active proposals</h2>
+          <p className="text-mediumTitle">0</p>
+        </GovernanceMetadataBox>
+        <GovernanceMetadataBox>
+          <h2 className="text-metadata text-grey-04">Accepted vs. rejected</h2>
+          <p className="flex items-center gap-3 text-mediumTitle">
+            <span>0</span>
+            <div className="h-4 w-px bg-grey-02" />
+            <span>0</span>
+          </p>
+        </GovernanceMetadataBox>
+      </div>
     </SpaceLayout>
   );
+}
+
+function GovernanceMetadataBox({ children }: { children: React.ReactNode }) {
+  return <div className="flex w-full flex-col items-center gap-1 rounded border border-grey-02 py-3">{children}</div>;
 }
