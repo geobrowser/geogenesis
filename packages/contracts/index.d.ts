1fdaacfd7651abf32e139f13382113837b4640bb
jonathanprozzi <jonathanprozzi@gmail.com>
2023-09-22 11:03:23 -0400

    added the ABIs to the contracts package export

--- packages/contracts/index.d.ts
+++ packages/contracts/index.d.ts
@@ -1,2 +1,21 @@
 import SpaceAbi from './build/abis/Space.json'
-export { SpaceAbi }
+import MainVotingPluginSetupAbi from './build/abis/MainVotingPluginSetup.json'
+import MainVotingPluginAbi from './build/abis/MainVotingPlugin.json'
+import MemberAccessPluginSetupAbi from './build/abis/MemberAccessPluginSetup.json'
+import MemberAccessPluginAbi from './build/abis/MemberAccessPlugin.json'
+import PersonalSpaceAdminPluginSetupAbi from './build/abis/PersonalSpaceAdminPluginSetup.json'
+import PersonalSpaceAdminPluginAbi from './build/abis/PersonalSpaceAdminPlugin.json'
+import SpacePluginSetupAbi from './build/abis/SpacePluginSetup.json'
+import SpacePluginAbi from './build/abis/SpacePlugin.json'
+
+export {
+  SpaceAbi,
+  MainVotingPluginSetupAbi,
+  MainVotingPluginAbi,
+  MemberAccessPluginSetupAbi,
+  MemberAccessPluginAbi,
+  PersonalSpaceAdminPluginAbi,
+  PersonalSpaceAdminPluginSetupAbi,
+  SpacePluginAbi,
+  SpacePluginSetupAbi,
+}
