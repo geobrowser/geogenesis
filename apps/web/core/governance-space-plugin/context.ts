af66c9ff15d23c986522f24701ab626903815490
jonathanprozzi <jonathanprozzi@gmail.com>
2023-09-29 10:13:41 -0400

    updated constants and added all plugin values to the plugin state in context

--- apps/web/core/governance-space-plugin/context.ts
+++ apps/web/core/governance-space-plugin/context.ts
@@ -1,6 +1,10 @@
 import { Context, ContextCore } from '@aragon/sdk-client-common';
 
-import { DEFAULT_GEO_SPACE_PLUGIN_ADDRESS, DEFAULT_GEO_SPACE_PLUGIN_REPO_ADDRESS } from '../constants';
+import {
+  DEFAULT_GEO_MAIN_VOTING_PLUGIN_REPO_ADDRESS,
+  DEFAULT_GEO_MEMBER_ACCESS_PLUGIN_REPO_ADDRESS,
+  DEFAULT_GEO_SPACE_PLUGIN_REPO_ADDRESS,
+} from '../constants';
 import { GeoPluginContextState, GeoPluginOverriddenState } from './internal/types';
 import { GeoPluginContextParams } from './types';
 
@@ -26,29 +30,60 @@ export class GeoPluginContext extends ContextCore {
     super.set(contextParams);
     // set the default values for the new params
     this.setDefaults();
+
+    // Space Plugin:
     if (contextParams.geoSpacePluginAddress) {
       this.state.geoSpacePluginAddress = contextParams.geoSpacePluginAddress;
       this.overriden.geoSpacePluginAddress = true;
     }
 
-    if (contextParams.geoSpacePluginRepoAddress) {
-      this.state.geoSpacePluginRepoAddress = contextParams.geoSpacePluginRepoAddress;
-      this.overriden.geoSpacePluginRepoAddress = true;
+    // Member Access Plugin:
+    if (contextParams.geoMemberAccessPluginAddress) {
+      this.state.geoMemberAccessPluginAddress = contextParams.geoMemberAccessPluginAddress;
+      this.overriden.geoMemberAccessPluginAddress = true;
+    }
+
+    // Main Voting Plugin:
+    if (contextParams.geoMainVotingPluginAddress) {
+      this.state.geoMainVotingPluginAddress = contextParams.geoMainVotingPluginAddress;
+      this.overriden.geoMainVotingPluginAddress = true;
     }
   }
 
   private setDefaults() {
-    if (!this.overriden.geoSpacePluginAddress) {
-      this.state.geoSpacePluginAddress = DEFAULT_GEO_SPACE_PLUGIN_ADDRESS;
+    // Optional: Set any settings that may have a default value here
+
+    if (!this.overriden.geoSpacePluginRepoAddress) {
       this.state.geoSpacePluginRepoAddress = DEFAULT_GEO_SPACE_PLUGIN_REPO_ADDRESS;
     }
+    if (!this.overriden.geoMemberAccessPluginRepoAddress) {
+      this.state.geoMemberAccessPluginRepoAddress = DEFAULT_GEO_MEMBER_ACCESS_PLUGIN_REPO_ADDRESS;
+    }
+    if (!this.overriden.geoMainVotingPluginRepoAddress) {
+      this.state.geoMainVotingPluginRepoAddress = DEFAULT_GEO_MAIN_VOTING_PLUGIN_REPO_ADDRESS;
+    }
   }
-
   get geoSpacePluginAddress(): string {
     return this.state.geoSpacePluginAddress;
   }
 
+  get geoMemberAccessPluginAddress(): string {
+    return this.state.geoMemberAccessPluginAddress;
+  }
+
+  get geoMainVotingPluginAddress(): string {
+    return this.state.geoMainVotingPluginAddress;
+  }
+
   get geoSpacePluginRepoAddress(): string {
     return this.state.geoSpacePluginRepoAddress;
+  }
+
+  get geoMemberAccessPluginRepoAddress(): string {
+    return this.state.geoMemberAccessPluginRepoAddress;
+  }
+
+  get geoMainVotingPluginRepoAddress(): string {
+    return this.state.geoMainVotingPluginRepoAddress;
   }
 }
