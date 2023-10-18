cde4b292674c5b4504b1b9e3bc2cd20fdd4edc92
Byron Guina <byron.guina@gmail.com>
2023-10-18 15:17:56 -0600

    fix: add middleware back

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -9,11 +9,11 @@ const MARKETING_PAGE_URL = 'https://geo.framer.website/';
 const BLOG_URL = 'https://geo-blog.vercel.app';
 
 export function middleware(request: NextRequest) {
-  // const url = request.nextUrl.clone();
-  // const pathname = new URL(url).pathname;
+  const url = request.nextUrl.clone();
+  const pathname = new URL(url).pathname;
 
-  // if (pathname === '/') return NextResponse.rewrite(MARKETING_PAGE_URL);
-  // if (pathname.startsWith('/blog')) return NextResponse.rewrite(`${BLOG_URL}${pathname}`);
+  if (pathname === '/') return NextResponse.rewrite(MARKETING_PAGE_URL);
+  if (pathname.startsWith('/blog')) return NextResponse.rewrite(`${BLOG_URL}${pathname}`);
 
   return NextResponse.next();
 }
