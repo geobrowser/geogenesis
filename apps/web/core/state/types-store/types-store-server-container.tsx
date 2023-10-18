275a41255ce7a45cdffd2990d9accacce37e233a
Byron Guina <byron.guina@gmail.com>
2023-10-18 13:33:56 -0600

    fix: read from correct subgraph for types provider

--- apps/web/core/state/types-store/types-store-server-container.tsx
+++ apps/web/core/state/types-store/types-store-server-container.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { Environment } from '~/core/environment';
 import { Subgraph } from '~/core/io';
 import { fetchForeignTypeTriples, fetchSpaceTypeTriples } from '~/core/io/fetch-types';
 
@@ -7,16 +8,32 @@ import { TypesStoreProvider } from './types-store';
 
 interface Props {
   spaceId: string;
-  endpoint: string;
   children: React.ReactNode;
 }
 
-export async function TypesStoreServerContainer({ spaceId, endpoint, children }: Props) {
-  const space = await Subgraph.fetchSpace({ id: spaceId, endpoint });
+export async function TypesStoreServerContainer({ spaceId, children }: Props) {
+  let config = Environment.getConfig(process.env.NEXT_PUBLIC_APP_ENV);
+
+  let space = await Subgraph.fetchSpace({ endpoint: config.subgraph, id: spaceId });
+  let usePermissionlessSubgraph = false;
+
+  if (!space) {
+    space = await Subgraph.fetchSpace({ endpoint: config.permissionlessSubgraph, id: spaceId });
+    if (space) usePermissionlessSubgraph = true;
+  }
+
+  if (usePermissionlessSubgraph) {
+    config = {
+      ...config,
+      subgraph: config.permissionlessSubgraph,
+    };
+  }
+
+  console.time('TypesStoreServerContainer: Fetch types');
 
   const [types, foreignTypes] = await Promise.all([
-    fetchSpaceTypeTriples(Subgraph.fetchTriples, spaceId, endpoint),
-    space ? fetchForeignTypeTriples(Subgraph.fetchTriples, space, endpoint) : [],
+    fetchSpaceTypeTriples(Subgraph.fetchTriples, spaceId, config.subgraph),
+    space ? fetchForeignTypeTriples(Subgraph.fetchTriples, space, config.subgraph) : [],
   ]);
 
   return (
