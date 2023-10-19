83e7330832e57b981e99561b0ae2ad172ecebec1
Byron <26263630+baiirun@users.noreply.github.com>
2023-10-19 08:38:41 -0700

    feat: remove API route fetching wrapper (#536)
    * fix: cyclical entity store imports
    
    * fix: import in test
    
    * fix: use log instead of error for no env passed in
    
    fix
    
    * fix: don't use API fetch wrapper for now
    
    Next seems to have problems in our middleware when using relative paths to call our API handlers. We need to pass an absolute path, but Next doesn't provide any mechanism of getting the absolute path on the server at request time.
    
    * fix: update orphan relative call

--- apps/web/core/io/api/geo-fetch.ts
+++
@@ -1,6 +0,0 @@
-export async function fetcher<T>(relativeUrl: string) {
-  const url = new URL(relativeUrl, process.env.ENV_URL);
-
-  const response = await fetch(url);
-  return (await response.json()) as T;
-}
--- apps/web/core/io/api/space.ts
+++ apps/web/core/io/api/space.ts
@@ -1,6 +1,7 @@
+import { Environment } from '~/core/environment';
 import { Space } from '~/core/types';
 
-import { fetcher } from './geo-fetch';
+import { fetchSpace } from '../subgraph';
 
 interface NetworkData {
   space: Space | null;
@@ -8,5 +9,17 @@ interface NetworkData {
 }
 
 export async function space(spaceId: string): Promise<NetworkData> {
-  return fetcher<NetworkData>(`/api/space/${spaceId}`);
+  const config = Environment.getConfig(process.env.NEXT_PUBLIC_APP_ENV);
+  let space = await fetchSpace({ endpoint: config.subgraph, id: spaceId });
+  let isPermissionlessSpace = false;
+
+  if (!space) {
+    space = await fetchSpace({ endpoint: config.permissionlessSubgraph, id: spaceId });
+    if (space) isPermissionlessSpace = true;
+  }
+
+  return {
+    space,
+    isPermissionlessSpace,
+  };
 }
