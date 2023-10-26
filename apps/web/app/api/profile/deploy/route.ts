f04137b2bd42f0f1f5b408ce07ce0bccd4c9ab93
Byron Guina <byron.guina@gmail.com>
2023-10-26 13:05:32 -0600

    fix: move await to the initialization

--- apps/web/app/api/profile/deploy/route.ts
+++ apps/web/app/api/profile/deploy/route.ts
@@ -46,7 +46,7 @@ export async function GET(request: Request) {
 
   const geoEntityIdFromOnchainId = `${userAddress}–${profileId}`;
 
-  const createProfileEffect = makeProfileEffect(requestId, {
+  const createProfileEffect = await makeProfileEffect(requestId, {
     account: userAddress as `0x${string}`,
     username,
     avatarUri,
@@ -54,7 +54,7 @@ export async function GET(request: Request) {
     profileId: geoEntityIdFromOnchainId,
   });
 
-  const profileEffect = await Effect.runPromise(Effect.either(await createProfileEffect));
+  const profileEffect = await Effect.runPromise(Effect.either(createProfileEffect));
 
   if (Either.isLeft(profileEffect)) {
     const error = profileEffect.left;
