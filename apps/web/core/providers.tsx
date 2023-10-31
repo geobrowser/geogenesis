63c5228679a4cde442bb07f25d91f6c803e2e2b0
Byron Guina <byron.guina@gmail.com>
2023-10-31 10:33:26 -0600

    fix: wonkiness

--- apps/web/core/providers.tsx
+++ apps/web/core/providers.tsx
@@ -6,25 +6,25 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import * as React from 'react';
 
 import { Services } from './services';
+import { ActionsStoreProvider } from './state/actions-store/actions-store-provider';
 import { AragonSDKProvider } from './state/aragon-dao-store';
 import { DiffProvider } from './state/diff-store';
 import { LocalStoreProvider } from './state/local-store';
 import { SpaceStoreProvider } from './state/space-store';
 import { StatusBarContextProvider } from './state/status-bar-store';
 import { WalletProvider } from './wallet';
-import { ActionsStoreProvider } from './state/actions-store/actions-store-provider';
 
 const queryClient = new QueryClient();
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = React.useState(false);
-  React.useEffect(() => setMounted(true), []);
 interface Props {
   onConnectionChange: (type: 'connect' | 'disconnect', address: string) => Promise<void>;
   children: React.ReactNode;
 }
 
 export function Providers({ children, onConnectionChange }: Props) {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <WalletProvider onConnectionChange={onConnectionChange}>
