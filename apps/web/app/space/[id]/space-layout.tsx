43fa827a65d00f9edf02a7099503409c99c27880
Byron Guina <byron.guina@gmail.com>
2023-11-02 12:12:08 -0600

    feat: remove reading images from root space for space pages

--- apps/web/app/space/[id]/space-layout.tsx
+++ apps/web/app/space/[id]/space-layout.tsx
@@ -43,8 +43,8 @@ export async function SpaceLayout({ params, children, usePermissionlessSpace }: 
 
   const props = await getData(params.id, config);
 
-  const avatarUrl = Entity.avatar(props.triples) ?? props.serverAvatarUrl;
-  const coverUrl = Entity.cover(props.triples) ?? props.serverCoverUrl;
+  const avatarUrl = Entity.avatar(props.triples);
+  const coverUrl = Entity.cover(props.triples);
 
   return (
     <TypesStoreServerContainer spaceId={params.id}>
@@ -136,8 +136,6 @@ const getData = async (spaceId: string, config: AppConfig) => {
   }
 
   const spaceName = space?.attributes[SYSTEM_IDS.NAME] ?? null;
-  const serverAvatarUrl = space?.attributes[SYSTEM_IDS.IMAGE_ATTRIBUTE] ?? null;
-  const serverCoverUrl = Entity.cover(entity?.triples);
 
   const blockIdsTriple = entity?.triples.find(t => t.attributeId === SYSTEM_IDS.BLOCKS) || null;
   const blockIds: string[] = blockIdsTriple ? JSON.parse(Value.stringValue(blockIdsTriple) || '[]') : [];
@@ -162,8 +160,6 @@ const getData = async (spaceId: string, config: AppConfig) => {
     name: entity?.name ?? spaceName ?? '',
     description: Entity.description(entity?.triples ?? []),
     spaceId,
-    serverAvatarUrl,
-    serverCoverUrl,
 
     // For entity page editor
     blockIdsTriple,
