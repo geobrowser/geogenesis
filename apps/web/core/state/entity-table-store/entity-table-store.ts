13dcda2734a6e80732b512c091b4f4aad1b9029a
Byron Guina <byron.guina@gmail.com>
2023-11-16 09:38:22 -0700

    fix: another unused one

--- apps/web/core/state/entity-table-store/entity-table-store.ts
+++ apps/web/core/state/entity-table-store/entity-table-store.ts
@@ -186,7 +186,6 @@ export class EntityTableStore implements IEntityTableStore {
 
     this.hasPreviousPage$ = computed(() => this.pageNumber$.get() > 0);
 
-    // @ts-expect-error legendstate btw
     this.hasNextPage$ = computed(() => networkData$.get().hasNextPage);
 
     this.unpublishedColumns$ = computed(() => {
