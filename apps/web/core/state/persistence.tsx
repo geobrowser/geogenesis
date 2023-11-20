64773baa529fb78ffa417bb1255855a2ba15f921
Byron Guina <byron.guina@gmail.com>
2023-11-20 10:05:26 -0700

    feat: remove localStorage migration path in Persistence

--- apps/web/core/state/persistence.tsx
+++ apps/web/core/state/persistence.tsx
@@ -1,38 +1,19 @@
 'use client';
 
 import * as React from 'react';
-import { useEffect, useState } from 'react';
 
-import { useLocalStorage } from '~/core/hooks/use-local-storage';
-
-import { useActionsStore } from '../hooks/use-actions-store';
 import { Action } from '../utils/action';
 import { actionsAtom } from './actions-store/actions-store';
 import { db } from './actions-store/indexeddb';
 import { store } from './jotai-provider';
 
 export const Persistence = () => {
-  const [isInitialRender, setIsInitialRender] = useState<boolean>(true);
-  const { restore } = useActionsStore();
-  const [storedActions, setStoredActions] = useLocalStorage('storedActions', {});
-
-  // Move actions from localStorage to IndexedDB
-  useEffect(() => {
-    if (isInitialRender) {
-      if (Object.keys(storedActions).length > 0) {
-        restore(storedActions);
-        setStoredActions({});
-      }
-    }
-    // note: finely tuned dependency because `storedActions` is only used on initial render
-    // `storedActions` will only change once as a result of clearing them in this effect
-  }, [isInitialRender]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  useEffect(() => {
-    setIsInitialRender(false);
-  }, []);
-
   React.useEffect(() => {
+    // Subscribe to the actions store and write all actions to indexeddb when
+    // they are changed.
+    //
+    // We only store unpublished, squashed actions in indexeddb to avoid the
+    // database growing indefinitely.
     const unsub = store.sub(actionsAtom, async () => {
       const newActions = store.get(actionsAtom);
 
