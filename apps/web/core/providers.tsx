8ad52178345b2c0cabfca24781e20c60525755f5
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-11-20 18:16:35 +0000

    feat: add global Jotai provider

--- apps/web/core/providers.tsx
+++ apps/web/core/providers.tsx
@@ -9,6 +9,7 @@ import { Services } from './services';
 import { ActionsStoreProvider } from './state/actions-store/actions-store-provider';
 import { ActiveProposalProvider } from './state/active-proposal-store';
 import { DiffProvider } from './state/diff-store';
+import { JotaiProvider } from './state/jotai-provider';
 import { LocalStoreProvider } from './state/local-store';
 import { SpaceStoreProvider } from './state/space-store';
 import { StatusBarContextProvider } from './state/status-bar-store';
@@ -24,21 +25,23 @@ interface Props {
 export function Providers({ children, onConnectionChange }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
-      <WalletProvider onConnectionChange={onConnectionChange}>
-        <Services.Provider>
-          <ActionsStoreProvider>
-            <SpaceStoreProvider>
-              <LocalStoreProvider>
-                <StatusBarContextProvider>
-                  <DiffProvider>
-                    <ActiveProposalProvider>{children}</ActiveProposalProvider>
-                  </DiffProvider>
-                </StatusBarContextProvider>
-              </LocalStoreProvider>
-            </SpaceStoreProvider>
-          </ActionsStoreProvider>
-        </Services.Provider>
-      </WalletProvider>
+      <JotaiProvider>
+        <WalletProvider onConnectionChange={onConnectionChange}>
+          <Services.Provider>
+            <ActionsStoreProvider>
+              <SpaceStoreProvider>
+                <LocalStoreProvider>
+                  <StatusBarContextProvider>
+                    <DiffProvider>
+                      <ActiveProposalProvider>{children}</ActiveProposalProvider>
+                    </DiffProvider>
+                  </StatusBarContextProvider>
+                </LocalStoreProvider>
+              </SpaceStoreProvider>
+            </ActionsStoreProvider>
+          </Services.Provider>
+        </WalletProvider>
+      </JotaiProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
