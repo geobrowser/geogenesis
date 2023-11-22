52eccee2051699b8f42e8d031b87107ecedf8edf
Byron Guina <byron.guina@gmail.com>
2023-11-22 11:58:29 -0700

    feat: use hash from transaction wait function

--- apps/web/core/io/publish/publish.ts
+++ apps/web/core/io/publish/publish.ts
@@ -212,11 +212,10 @@ export async function registerGeoProfile(wallet: WalletClient, spaceId: `0x${str
   });
 
   const tx = await writeContract(contractConfig);
-
-  await waitForTransaction({
+  const waited = await waitForTransaction({
     hash: tx.hash,
   });
 
-  console.log(`Geo profile created. Transaction hash: ${tx.hash}`);
+  console.log(`Geo profile created. Transaction hash: ${waited.transactionHash}`);
   return contractConfig.result as string;
 }
