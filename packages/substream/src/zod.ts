39dd8102d70ce60dbd7c78990c5851695b1761de
Byron Guina <byron.guina@gmail.com>
2023-11-27 14:46:23 -0700

    fix: datauri in full entry should be of type ZodAction

--- packages/substream/src/zod.ts
+++ packages/substream/src/zod.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const ZodEntry = z.object({
   id: z.string(),
@@ -6,52 +6,52 @@ export const ZodEntry = z.object({
   uri: z.string(),
   author: z.string(),
   space: z.string(),
-});
-export type Entry = z.infer<typeof ZodEntry>;
+})
+export type Entry = z.infer<typeof ZodEntry>
 
 export const ZodAction = z.object({
-  type: z.enum(["createTriple", "deleteTriple"]),
+  type: z.enum(['createTriple', 'deleteTriple']),
   entityId: z.string(),
   attributeId: z.string(),
   entityName: z.string().nullish(),
   value: z
     .object({
-      type: z.enum(["number", "string", "entity", "image", "date", "url"]),
+      type: z.enum(['number', 'string', 'entity', 'image', 'date', 'url']),
       id: z.string(),
       value: z.string().nullish(),
     })
     .refine((data) => data.id || data.value, {
-      message: "Either id or value must be provided",
+      message: 'Either id or value must be provided',
     }),
-});
+})
 
-export type Action = z.infer<typeof ZodAction>;
+export type Action = z.infer<typeof ZodAction>
 
 export const ZodUriData = z.object({
   name: z.string().optional(),
   type: z.string(),
   version: z.string(),
-  actions: z.array(z.any()), // Parsing immediately after receiving data
-});
-export type UriData = z.infer<typeof ZodUriData>;
+  actions: z.array(ZodAction), // Parsing immediately after receiving data
+})
+export type UriData = z.infer<typeof ZodUriData>
 
 export const ZodFullEntry = ZodEntry.extend({
   uriData: ZodUriData,
-});
-export type FullEntry = z.infer<typeof ZodFullEntry>;
+})
+export type FullEntry = z.infer<typeof ZodFullEntry>
 
 export const ZodRoleChange = z.object({
   id: z.string(),
-  role: z.enum(["ADMIN", "MEMBER", "MODERATOR"]),
+  role: z.enum(['ADMIN', 'MEMBER', 'MODERATOR']),
   account: z.string(),
   sender: z.string(),
   space: z.string(),
-});
-export type RoleChange = z.infer<typeof ZodRoleChange>;
+})
+export type RoleChange = z.infer<typeof ZodRoleChange>
 
 export const ZodEntryStreamResponse = z.object({
   entries: z.array(ZodEntry).min(1),
-});
+})
 
 export const ZodRoleChangeStreamResponse = z.object({
   roleChanges: z
@@ -62,8 +62,8 @@ export const ZodRoleChangeStreamResponse = z.object({
           revoked: ZodRoleChange.optional(),
         })
         .refine((data) => (data.granted ? !data.revoked : data.revoked), {
-          message: "Only one of granted or revoked must be provided",
+          message: 'Only one of granted or revoked must be provided',
         })
     )
     .min(1),
-});
+})
