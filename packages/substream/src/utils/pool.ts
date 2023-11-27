4eb51b4211767b9c5929d7aea08845e9b4b01ff7
Byron Guina <byron.guina@gmail.com>
2023-11-27 10:27:41 -0700

    feat: remove comment

--- packages/substream/src/utils/pool.ts
+++ packages/substream/src/utils/pool.ts
@@ -1,13 +1,13 @@
-import * as dotenv from "dotenv";
-import * as pg from "pg";
-import { invariant } from "./invariant";
+import * as dotenv from 'dotenv'
+import * as pg from 'pg'
+import { invariant } from './invariant'
 
-dotenv.config();
+dotenv.config()
 
-invariant(process.env.DATABASE_URL, "DATABASE_URL is required");
+invariant(process.env.DATABASE_URL, 'DATABASE_URL is required')
 export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
-  max: 97, // TODO: Confirm with Byron, document if this is a good number
-});
+  max: 97,
+})
 
-pool.on("error", (err) => console.error("Pool Error", err)); // don't let a pg restart kill your app
+pool.on('error', (err) => console.error('Pool Error', err)) // don't let a pg restart kill your app
