5ede716e6a9291da6f06e0fb1965edf2e65d05c2
Byron Guina <byron.guina@gmail.com>
2023-11-29 13:28:37 -0700

    feat: add eslint + prettier to api package

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,37 +1,27 @@
-import http from 'node:http'
-import https from 'node:https'
-import { postgraphile } from 'postgraphile'
-import PgOrderByRelatedPlugin from '@graphile-contrib/pg-order-by-related'
-import ConnectionFilterPlugin from 'postgraphile-plugin-connection-filter'
-import PgSimplifyInflectorPlugin from '@graphile-contrib/pg-simplify-inflector'
+import PgOrderByRelatedPlugin from '@graphile-contrib/pg-order-by-related';
+import PgSimplifyInflectorPlugin from '@graphile-contrib/pg-simplify-inflector';
+import http from 'node:http';
+import https from 'node:https';
+import { postgraphile } from 'postgraphile';
+import ConnectionFilterPlugin from 'postgraphile-plugin-connection-filter';
 
-require('dotenv').config()
+require('dotenv').config();
 
-const postgraphileMiddleware = postgraphile(
-  process.env.DATABASE_URL,
-  'public',
-  {
-    watchPg: true,
-    graphiql: true,
-    enhanceGraphiql: true,
-    graphileBuildOptions: {
-      connectionFilterRelations: true, // default: false
-    },
-    appendPlugins: [
-      PgOrderByRelatedPlugin,
-      ConnectionFilterPlugin,
-      PgSimplifyInflectorPlugin,
-    ],
-  }
-)
+const postgraphileMiddleware = postgraphile(process.env.DATABASE_URL, 'public', {
+  watchPg: true,
+  graphiql: true,
+  enhanceGraphiql: true,
+  graphileBuildOptions: {
+    connectionFilterRelations: true, // default: false
+  },
+  appendPlugins: [PgOrderByRelatedPlugin, ConnectionFilterPlugin, PgSimplifyInflectorPlugin],
+});
 
 const server =
   process.env.NODE_ENV === 'production'
     ? https.createServer(postgraphileMiddleware)
-    : http.createServer(postgraphileMiddleware)
+    : http.createServer(postgraphileMiddleware);
 
 server.listen(process.env.PORT ?? 5001, () => {
-  console.log(
-    `🚀 Server ready at http://localhost:${process.env.PORT || 5001}/graphql`
-  )
-})
+  console.log(`🚀 Server ready at http://localhost:${process.env.PORT || 5001}/graphql`);
+});
