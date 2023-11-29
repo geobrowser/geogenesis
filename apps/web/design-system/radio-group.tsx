945346fa2528a2bc275a2a399305efd70e959822
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-11-29 18:22:34 +0000

    feat: nonprofit onboarding

--- apps/web/design-system/radio-group.tsx
+++ apps/web/design-system/radio-group.tsx
@@ -23,7 +23,7 @@ export const RadioGroup = ({ value, onValueChange, options, ...rest }: RadioGrou
         key={value}
         value={value}
         className={cx(
-          'flex w-full items-center justify-between rounded-lg border border-grey-02 p-1.5 pr-3 text-grey-04 data-[state=checked]:border-text data-[state=checked]:text-text',
+          'flex w-full items-center justify-between rounded-lg border border-grey-02 p-1.5 pr-3 text-grey-04 transition duration-300 hover:border-text hover:text-text data-[state=checked]:border-text data-[state=checked]:text-text',
           disabled && 'cursor-not-allowed opacity-50'
         )}
         disabled={disabled}
@@ -31,7 +31,7 @@ export const RadioGroup = ({ value, onValueChange, options, ...rest }: RadioGrou
       >
         <div className="inline-flex items-center gap-4">
           {image && (
-            <div className="rounded-md relative h-12 w-12 overflow-clip">
+            <div className="relative h-12 w-12 overflow-clip rounded-md">
               <img src={image} alt={label} className="absolute inset-0 h-full w-full" />
             </div>
           )}
