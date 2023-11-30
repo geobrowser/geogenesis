c70fede0ca4efa60f8a9c68710aa3f04b177f6fb
Byron Guina <byron.guina@gmail.com>
2023-11-30 12:43:01 -0700

    feat(wip): add specific errors to IPFS fetching

--- packages/substream/src/utils/ipfs.ts
+++ packages/substream/src/utils/ipfs.ts
@@ -3,13 +3,31 @@ import { Effect, Schedule } from 'effect';
 import { IPFS_GATEWAY } from '../constants/constants.js';
 import type { UriData } from '../zod.js';
 
-export function getFetchIpfsContentEffect(uri: string): Effect.Effect<never, Error, UriData | null> {
+class UnableToParseBase64Error extends Error {
+  _tag: 'UnableToParseBase64Error' = 'UnableToParseBase64Error';
+}
+
+class FailedFetchingIpfsContentError extends Error {
+  _tag: 'FailedFetchingIpfsContentError' = 'FailedFetchingIpfsContentError';
+}
+
+class UnableToParseJsonError extends Error {
+  _tag: 'UnableToParseJsonError' = 'UnableToParseJsonError';
+}
+
+export function getFetchIpfsContentEffect(
+  uri: string
+): Effect.Effect<
+  never,
+  UnableToParseBase64Error | FailedFetchingIpfsContentError | UnableToParseJsonError,
+  UriData | null
+> {
   return Effect.gen(function* (unwrap) {
     if (uri.startsWith('data:application/json;base64,')) {
       const base64 = uri.split(',')[1]!; // we can cast with bang because we know a base64 string will always have a second element
       const decoded = Effect.try({
         try: () => JSON.parse(Buffer.from(base64, 'base64').toString('utf8')) as UriData,
-        catch: () => Error("Can't parse base64 string"),
+        catch: () => new UnableToParseBase64Error("Can't parse base64 string"),
       });
 
       return yield* unwrap(decoded);
@@ -24,7 +42,7 @@ export function getFetchIpfsContentEffect(uri: string): Effect.Effect<never, Err
           return await fetch(url);
         },
         catch: error => {
-          return new Error("Can't fetch IPFS content");
+          return new FailedFetchingIpfsContentError("Can't fetch IPFS content");
         },
       });
 
@@ -37,7 +55,7 @@ export function getFetchIpfsContentEffect(uri: string): Effect.Effect<never, Err
             return (await response.json()) as UriData;
           },
           // @TODO: Specific error
-          catch: () => Error("Can't parse response"),
+          catch: () => new UnableToParseJsonError("Can't parse response"),
         })
       );
     }
