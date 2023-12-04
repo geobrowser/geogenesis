2459e720ac6492d41443315bc2a170d0f215164e
Byron Guina <byron.guina@gmail.com>
2023-12-04 15:26:20 -0700

    feat: move index initialization out of schema initialization

---
+++ packages/substream/src/sql/initIndexes.sql
@@ -0,0 +1,19 @@
+-- 
+-- Create Indexes for Speedy Querying
+-- 
+
+
+
+CREATE INDEX idx_entity_attribute ON public.triples(entity_id, attribute_id);
+
+CREATE INDEX idx_entity_attribute_value_id ON public.triples(entity_id, attribute_id, value_id);
+
+CREATE INDEX idx_entity_value_id ON public.triples(entity_value_id);
+
+CREATE INDEX idx_triple_space ON public.triples(space_id);
+
+CREATE INDEX idx_accounts_space_id ON public.space_admins(account_id, space_id);
+
+CREATE INDEX idx_space_editors ON public.space_editors(account_id, space_id);
+
+CREATE INDEX idx_space_editor_controllers ON public.space_editor_controllers(account_id, space_id);
