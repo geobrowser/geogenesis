4d2f1be6c0a6737bcd5af0a1017d7ece5299e107
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-12-08 09:54:37 -0800

    feat: support multiple filters on new entity (#603)

--- apps/web/partials/blocks/table/table-block.tsx
+++ apps/web/partials/blocks/table/table-block.tsx
@@ -16,7 +16,7 @@ import { useTableBlock } from '~/core/state/table-block-store';
 import { Entity } from '~/core/utils/entity';
 import { NavUtils } from '~/core/utils/utils';
 
-import { IconButton, SmallButton } from '~/design-system/button';
+import { IconButton } from '~/design-system/button';
 import { Create } from '~/design-system/icons/create';
 import { FilterTable } from '~/design-system/icons/filter-table';
 import { FilterTableWithFilters } from '~/design-system/icons/filter-table-with-filters';
@@ -37,6 +37,7 @@ interface Props {
   spaceId: string;
 }
 
+// eslint-disable-next-line react/display-name
 export const TableBlock = React.memo(({ spaceId }: Props) => {
   const { setFilterState } = useTableBlock();
 
@@ -98,8 +99,8 @@ export const TableBlock = React.memo(({ spaceId }: Props) => {
   });
 
   const typeId = type.entityId;
-  const filterId = filterState?.[0]?.columnId ?? null;
-  const filterValue = filterState?.[0]?.value ?? null;
+  const filters: Array<[string, string]> =
+    filterState && filterState.length > 0 ? filterState.map(filter => [filter.columnId, filter.value]) : [];
 
   return (
     <div>
@@ -159,7 +160,7 @@ export const TableBlock = React.memo(({ spaceId }: Props) => {
           <TableBlockContextMenu />
 
           {isEditing && (
-            <Link href={NavUtils.toEntity(spaceId, ID.createEntityId(), typeId, filterId, filterValue)}>
+            <Link href={NavUtils.toEntity(spaceId, ID.createEntityId(), typeId, filters)}>
               <Create />
             </Link>
           )}
