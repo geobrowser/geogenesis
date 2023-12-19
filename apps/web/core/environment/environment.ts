db94cca8568d180c619801b9593dd884121fd9b9
Byron Guina <byron.guina@gmail.com>
2023-12-19 09:02:06 -0700

    feat: use production API

--- apps/web/core/environment/environment.ts
+++ apps/web/core/environment/environment.ts
@@ -44,7 +44,7 @@ export const options: Record<AppEnv, AppConfig> = {
     permissionlessSubgraph: 'https://api.thegraph.com/subgraphs/name/baiirun/geo-permissionless',
     membershipSubgraph: 'https://api.thegraph.com/subgraphs/name/baiirun/geo-membership-workshop',
     profileSubgraph: 'https://api.thegraph.com/subgraphs/name/baiirun/geo-profile-registry',
-    api: 'http://localhost:5001/graphql',
+    api: 'https://steep-hook-production.up.railway.app/graphql',
   },
 };
 
