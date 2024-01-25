efcb958b7eaf3d9e10d524b9fd0e29949c997f3f
Byron Guina <byron.guina@gmail.com>
2024-01-25 13:15:16 -0700

    feat: add dual permissions model docs to protobuf

--- packages/substream/src/pb/schema.rs
+++ packages/substream/src/pb/schema.rs
@@ -205,6 +205,15 @@ pub struct GeoGovernancePluginsCreated {
     pub plugins: ::prost::alloc::vec::Vec<GeoGovernancePluginCreated>,
 }
 /// *
+/// This event represents adding editors to a DAO-based space
+/// 
+/// The data model for DAO-based spaces works slightly differently than in legacy spaces.
+/// This means there will be a period where we need to support both data models depending
+/// on which space/contract we are working with. Eventually these data models will be merged
+/// and usage of the legacy space contracts will be migrated to the DAO-based contracts, but
+/// for now we are appending "V2" to permissions data models to denote it's used for the
+/// DAO-based spaces.
+///
 /// An editor has editing and voting permissions in a DAO-based space. Editors join a space
 /// one of two ways:
 /// 1. They submit a request to join the space as an editor which goes to a vote. The editors
