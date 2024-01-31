c2b84f003b7e62244f561e07f9710b1634d386ad
Byron Guina <byron.guina@gmail.com>
2024-01-31 10:12:36 -0700

    fix: unused encodings

--- apps/web/app/dao/create-proposal.tsx
+++ apps/web/app/dao/create-proposal.tsx
@@ -12,61 +12,6 @@ import { Button } from '~/design-system/button';
 
 import { abi } from './main-voting-abi';
 
-const createProposalInputs = [
-  {
-    internalType: 'bytes',
-    name: '_metadata',
-    type: 'bytes',
-  },
-  {
-    components: [
-      {
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'value',
-        type: 'uint256',
-      },
-      {
-        internalType: 'bytes',
-        name: 'data',
-        type: 'bytes',
-      },
-    ],
-    internalType: 'struct IDAO.Action[]',
-    name: '_actions',
-    type: 'tuple[]',
-  },
-  {
-    internalType: 'uint256',
-    name: '_allowFailureMap',
-    type: 'uint256',
-  },
-  {
-    internalType: 'uint64',
-    name: '',
-    type: 'uint64',
-  },
-  {
-    internalType: 'uint64',
-    name: '',
-    type: 'uint64',
-  },
-  {
-    internalType: 'enum IMajorityVoting.VoteOption',
-    name: '_voteOption',
-    type: 'uint8',
-  },
-  {
-    internalType: 'bool',
-    name: '_tryEarlyExecution',
-    type: 'bool',
-  },
-] as const;
-
 const processProposalInputs = [
   {
     internalType: 'uint32',
@@ -84,46 +29,6 @@ const processProposalInputs = [
     type: 'string',
   },
 ] as const;
-
-function createProposalEncoding() {
-  // @TODO: Add metadata to ipfs. this will include the root object. If the proposal is not a content proposal it will use
-  // a new type of metadata object that has the proposal type and version object
-
-  // 1. Create the proposal metadata for content
-  // 2. Create the proposal metadata for a subspace
-  //    - Add subspace
-  //    - Remove subspace
-  // 3. Create the proposal metadata for a member
-  //    - Add member
-  //    - Remove member
-  // 4. Create the proposal metadata for an editor
-  //    - Add editor
-  //    - Remove editor
-  // 5. Create action for processing a content proposal
-  // 6. Create action for processing a subspace proposal
-  //    - Add subspace
-  //    - Remove subspace
-  // 7. Create action for processing a member proposal
-  //    - Add member
-  //    - Remove member
-  // 8. Create action for processing an editor proposal
-  //    - Add editor
-  //    - Remove editor
-  const encodedParams = encodeAbiParameters(createProposalInputs, [
-    // @HACK to work around encodeAbiParameters not being able to encode
-    // utf8 byte arrays
-    // https://github.com/wevm/viem/issues/926
-    fromBytes(stringToBytes('ipfs://QmTMt24BWFBPX7T3G6EquF8jt9odkeWvdzrFia6bvE3C3d'), 'hex'), // Some random IPFS entry from the subgraph
-    [],
-    BigInt(0),
-    BigInt(0),
-    BigInt(0),
-    VoteOption.Yes,
-    true,
-  ]);
-
-  return encodedParams;
-}
 
 interface Props {
   type:
@@ -171,11 +76,30 @@ export function useCreateContentProposal() {
     publish: writer.write,
   };
 }
-
+// @TODO: Add metadata to ipfs. this will include the root object. If the proposal is not a content proposal it will use
+// a new type of metadata object that has the proposal type and version object
+// 1. Create the proposal metadata for content
+// 2. Create the proposal metadata for a subspace
+//    - Add subspace
+//    - Remove subspace
+// 3. Create the proposal metadata for a member
+//    - Add member
+//    - Remove member
+// 4. Create the proposal metadata for an editor
+//    - Add editor
+//    - Remove editor
+// 5. Create action for processing a content proposal
+// 6. Create action for processing a subspace proposal
+//    - Add subspace
+//    - Remove subspace
+// 7. Create action for processing a member proposal
+//    - Add member
+//    - Remove member
+// 8. Create action for processing an editor proposal
+//    - Add editor
+//    - Remove editor
 export function CreateProposal({ type }: Props) {
   const { storageClient } = Services.useServices();
-
-  // const { write } = useCreateContentProposal();
 
   const { data: wallet } = useWalletClient();
 
