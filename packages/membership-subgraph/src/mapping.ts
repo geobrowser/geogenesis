e43a5d59c5ce6b1bb5afb4c18ff21c0a77242dd3
Byron Guina <byron.guina@gmail.com>
2024-02-15 15:14:14 -0800

    feat: use new membership subgraph
    This change adds logs and some redundancy checks for membership requests

--- packages/membership-subgraph/src/mapping.ts
+++ packages/membership-subgraph/src/mapping.ts
@@ -1,3 +1,4 @@
+import { log } from '@graphprotocol/graph-ts'
 import { MembershipRequested } from '../generated/Membership/Membership'
 import { MembershipRequest } from '../generated/schema'
 import { getChecksumAddress } from './get-checksum-address'
@@ -6,10 +7,30 @@ export function handleMembershipRequested(event: MembershipRequested): void {
   const userAccount = event.params.account
   const space = event.params.space
 
-  const newRequest = new MembershipRequest(userAccount.toHex() + space.toHex())
+  const request = MembershipRequest.load(
+    getChecksumAddress(userAccount) + getChecksumAddress(space)
+  )
+
+  if (request) {
+    log.warning('Membership request already exists for user {} in space {}', [
+      getChecksumAddress(userAccount),
+      getChecksumAddress(space),
+    ])
+    return
+  }
+
+  const newRequest = new MembershipRequest(
+    getChecksumAddress(userAccount) + getChecksumAddress(space)
+  )
+
   newRequest.requestor = getChecksumAddress(userAccount)
   newRequest.space = getChecksumAddress(space)
   newRequest.createdAt = event.block.timestamp
+
+  log.debug('New membership request created for user {} in space {}', [
+    getChecksumAddress(userAccount),
+    getChecksumAddress(space),
+  ])
 
   newRequest.save()
 }
