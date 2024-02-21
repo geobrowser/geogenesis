a010c4a3171318f8a1d00b84ed560e3f94ae34dc
Byron <26263630+baiirun@users.noreply.github.com>
2024-02-21 10:47:07 -0800

    fix: use correct graphql string transform everywhere (#656)

--- apps/web/core/state/table-block-store.tsx
+++ apps/web/core/state/table-block-store.tsx
@@ -100,7 +100,7 @@ export function useTableBlock() {
       return stringValue;
     }
 
-    return TableBlockSdk.createGraphQLStringFromFilters([], selectedType.entityId);
+    return TableBlockSdk.createGraphQLStringFromFiltersV2([], selectedType.entityId);
   }, [filterTriple, selectedType.entityId]);
 
   const { data: filterState, isLoading: isLoadingFilterState } = useQuery({
@@ -119,7 +119,7 @@ export function useTableBlock() {
     // @TODO: ShownColumns changes should trigger a refetch
     queryKey: ['table-block-columns', filterState, selectedType.entityId, entityId],
     queryFn: async ({ signal }) => {
-      const filterString = TableBlockSdk.createGraphQLStringFromFilters(filterState ?? [], selectedType.entityId);
+      const filterString = TableBlockSdk.createGraphQLStringFromFiltersV2(filterState ?? [], selectedType.entityId);
 
       const params: FetchRowsOptions['params'] = {
         filter: filterString,
@@ -248,7 +248,7 @@ export function useTableBlock() {
 
       // We can just set the string as empty if the new state is empty. Alternatively we just delete the triple.
       const newFiltersString =
-        newState.length === 0 ? '' : TableBlockSdk.createGraphQLStringFromFilters(newState, selectedType.entityId);
+        newState.length === 0 ? '' : TableBlockSdk.createGraphQLStringFromFiltersV2(newState, selectedType.entityId);
 
       const entityName = Entity.name(nameTriple ? [nameTriple] : []) ?? '';
 
