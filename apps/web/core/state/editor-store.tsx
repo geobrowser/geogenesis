8a7fc078a591fb9237841a8a8d12c2ae6d11fad6
Byron Guina <byron.guina@gmail.com>
2024-02-21 13:02:16 -0800

    fix: use v1 graphql filters when mapping to entity and local state
    We then map to the v2 filters when making queries

--- apps/web/core/state/editor-store.tsx
+++ apps/web/core/state/editor-store.tsx
@@ -333,7 +333,7 @@ export function useEditorStore() {
               value: {
                 id: ID.createValueId(),
                 type: 'string',
-                value: TableBlockSdk.createGraphQLStringFromFiltersV2(
+                value: TableBlockSdk.createGraphQLStringFromFilters(
                   [
                     {
                       columnId: SYSTEM_IDS.SPACE,
