86c415f64206c26f8a9bcc61e5f43c45703c09a0
Byron Guina <byron.guina@gmail.com>
2024-02-27 13:15:15 -0800

    feat: add initial modal appearance config

--- apps/web/core/wallet/privy.tsx
+++ apps/web/core/wallet/privy.tsx
@@ -13,10 +13,17 @@ import { registerGeoProfile } from '../io/publish';
 const config: PrivyClientConfig = {
   defaultChain: polygon,
   supportedChains: [polygon],
+  embeddedWallets: {
+    noPromptOnSignature: true,
+    createOnLogin: 'users-without-wallets',
+  },
+  appearance: {
+    showWalletLoginFirst: false,
+    logo: '/static/favicon-320x180.png',
+    walletList: ['rainbow', 'coinbase_wallet', 'wallet_connect', 'metamask'],
+  },
 
   // These are handled by wagmi
-  // defaultChain: polygon,
-  // supportedChains: [polygon],
   // embeddedWallets: {
   //   noPromptOnSignature: false,
   //   priceDisplay: {
