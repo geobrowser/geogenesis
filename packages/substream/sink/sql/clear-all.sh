775d16083ff595b150329ef1480edf2ff985bf42
Byron Guina <byron.guina@gmail.com>
2024-03-13 09:43:38 -0700

    feat: add actions index to proposed versions

--- packages/substream/sink/sql/clear-all.sh
+++ packages/substream/sink/sql/clear-all.sh
@@ -3,7 +3,7 @@
 source .env
 
 echo "Running SQL scripts to clear all tables..."
-psql $DATABASE_URL < sink/sql/clearCacheTables.sql
+psql $DATABASE_URL < sink/sql/clear-cache-tables.sql
 psql $DATABASE_URL < sink/sql/clearPublicTables.sql
 echo "SQL scripts executed successfully."
 
