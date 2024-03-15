4677f903856e32f75dbdf48173a0e7d7de15ca22
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-03-15 22:24:07 +0000

    fix: table names

--- apps/web/partials/blocks/table/table-block-editable-title.tsx
+++ apps/web/partials/blocks/table/table-block-editable-title.tsx
@@ -12,7 +12,7 @@ export function TableBlockEditableTitle({ spaceId }: { spaceId: string }) {
       onBlur={e => setName(e.currentTarget.value)}
       defaultValue={name ?? undefined}
       placeholder="Enter a name for this table..."
-      className="w-full appearance-none text-smallTitle text-text outline-none placeholder:text-grey-03"
+      className="w-full shrink-0 grow appearance-none text-smallTitle text-text outline-none placeholder:text-grey-03"
     />
   ) : (
     <h4 className="text-smallTitle">{name}</h4>
