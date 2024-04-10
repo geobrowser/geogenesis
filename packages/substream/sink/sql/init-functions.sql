14dc31186d682551b63b53f7ef4d3aaf49a15b3d
Byron Guina <byron.guina@gmail.com>
2024-04-10 14:05:37 -0700

    feat: add profile and space metadata postgres functions

--- packages/substream/sink/sql/init-functions.sql
+++ packages/substream/sink/sql/init-functions.sql
@@ -132,3 +132,58 @@ BEGIN
     RETURN attribute_count;
 END;
 $$ LANGUAGE plpgsql STRICT STABLE;
+
+CREATE FUNCTION public.spaces_metadata(e_row spaces)
+RETURNS SETOF public.geo_entities AS $$
+BEGIN
+    -- Using CTE to first fetch all types of the given entity
+    RETURN QUERY 
+    -- Get the entity id 
+    WITH space_configuration_entity_ids AS (
+        SELECT t.*
+        FROM triples t
+        WHERE t.space_id = e_row.id
+        AND t.attribute_id = 'type'
+        AND t.value_id = '1d5d0c2a-db23-466c-a0b0-9abe879df457' -- space configuration
+    )
+    SELECT e.*
+    FROM geo_entities e
+    JOIN space_configuration_entity_ids eids ON e.id = eids.entity_id;
+END;
+$$ LANGUAGE plpgsql STRICT STABLE;
+
+-- Map the account id to a geo profile based on the entity id of
+-- the account's onchain profile if it exists
+CREATE FUNCTION public.accounts_geo_profiles(e_row accounts)
+RETURNS SETOF public.geo_entities AS $$
+BEGIN
+    RETURN QUERY 
+    -- Get the onchain profile that matches the account id
+    WITH onchain_profiles_ids AS (
+        SELECT op.*
+        FROM onchain_profiles op
+        WHERE op.account_id = e_row.id
+    )
+    SELECT e.*
+    FROM geo_entities e
+    -- Return the entity id that matches the onchain profile id
+    INNER JOIN onchain_profiles_ids opids ON e.id = opids.id;
+END;
+$$ LANGUAGE plpgsql STRICT STABLE;
+
+-- CREATE FUNCTION public.geo_entities_collection_items(e_row entities)
+-- RETURNS SETOF public.geo_entities AS $$
+-- BEGIN
+--     RETURN QUERY 
+--     -- Get the onchain profile that matches the account id
+--    WITH collection_entities AS (
+--         SELECT t.*
+--         FROM triples t
+--         WHERE t.attribute_id = 'type'
+--         AND t.value_id = 'debbf9ff-62c6-40aa-9b2c-672e3bb15d0e' -- collection type
+--     )
+--     SELECT e.*
+--     FROM geo_entities e
+--     JOIN collection_entities cids ON e.id = cids.entity_id;
+-- END;
+-- $$ LANGUAGE plpgsql STRICT STABLE;
