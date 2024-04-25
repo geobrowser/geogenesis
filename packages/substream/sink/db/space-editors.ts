72178adeee33ec98d4dd6892f2055db00433611d
Byron Guina <byron.guina@gmail.com>
2024-04-25 12:37:20 -0700

    fix: use correct space editors table

--- packages/substream/sink/db/space-editors.ts
+++ packages/substream/sink/db/space-editors.ts
@@ -6,7 +6,7 @@ import { pool } from '../utils/pool';
 export class SpaceEditors {
   static async upsert(spaceEditors: S.space_editors.Insertable[]) {
     return await db
-      .upsert('space_editors', spaceEditors, ['account_id', 'space_id'], {
+      .upsert('space_editors_v2', spaceEditors, ['account_id', 'space_id'], {
         updateColumns: db.doNothing,
       })
       .run(pool);
