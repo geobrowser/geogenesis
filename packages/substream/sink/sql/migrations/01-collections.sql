7d9bc7b45c98cbd276eea207fb48d0fa4205ea8e
Byron Guina <byron.guina@gmail.com>
2024-05-06 14:46:13 -0700

    feat: read order for collection items
    We can't actually order by the order when using materialized views unfortunately, so we'll have to index it together in the sink

--- packages/substream/sink/sql/migrations/01-collections.sql
+++ packages/substream/sink/sql/migrations/01-collections.sql
@@ -60,7 +60,8 @@ $$ LANGUAGE plpgsql STRICT STABLE;
 
 CREATE TYPE public.collection_item AS (
    entity_id text,
-   collection_id text
+   collection_id text,
+   index text
 );
 
 COMMENT ON TYPE public.collection_item IS
@@ -73,14 +74,16 @@ CREATE OR REPLACE FUNCTION public.collections_items(e_row collections)
 RETURNS SETOF public.collection_item AS $$
 BEGIN
     RETURN QUERY
-    SELECT t3.entity_value_id as collection_id, t2.entity_value_id as entity_id
+    SELECT t3.entity_value_id as collection_id, t2.entity_value_id as entity_id, t4.string_value as index
     FROM triples t1
     JOIN triples t2 ON t1.entity_id = t2.entity_id
     JOIN triples t3 ON t1.entity_id = t3.entity_id
+    JOIN triples t4 ON t1.entity_id = t3.entity_id 
     WHERE t1.entity_id = e_row.entity_id
       AND t1.attribute_id = 'types'
       AND t1.entity_value_id = '0e8d692b-94d7-4c64-bcb3-0eb4d55503ef' -- Collection Item type
       AND t2.attribute_id = '53d1e5f2-6f23-4bf2-9e88-42b02f437970' -- entity id of the collection item's entity value
-      AND t3.attribute_id = '487e084b-4132-4b05-b15a-b6e147d58244'; -- entity id of the collection
+      AND t3.attribute_id = '487e084b-4132-4b05-b15a-b6e147d58244' -- entity id of the collection
+      AND t4.attribute_id = 'ede47e69-30b0-4499-8ea4-aafbda449609'; -- fractional index of the collection item
 END;
 $$ LANGUAGE plpgsql STRICT STABLE;
