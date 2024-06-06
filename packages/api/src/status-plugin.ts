da6f63f45bfbefb0b0bc65f8db2c5326665f043c
Byron Guina <byron.guina@gmail.com>
2024-06-06 11:07:53 -0700

    fix: only needs a subset of the indexing status

--- packages/api/src/status-plugin.ts
+++ packages/api/src/status-plugin.ts
@@ -1,5 +1,7 @@
 import { gql, makeExtendSchemaPlugin } from 'graphile-utils';
 
+const INITIAL_GEO_BLOCK = 620;
+
 export const IndexingStatusPlugin = makeExtendSchemaPlugin(() => {
   return {
     typeDefs: gql`
@@ -17,51 +19,14 @@ export const IndexingStatusPlugin = makeExtendSchemaPlugin(() => {
       type ChainIndexingStatus {
         network: String!
         chainHeadBlock: Block
-        # earliestBlock: EarliestBlock
-        # latestBlock: Block
-        # lastHealthyBlock: Block
-      }
-
-      type SubgraphError {
-        message: String!
-
-        # Context for the error.
-        block: Block
-        handler: String
-
-        # true means we have certainty that the error is deterministic.
-        deterministic: Boolean!
-      }
-
-      enum Health {
-        "Subgraph syncing normally"
-        healthy
-        "Subgraph syncing but with errors"
-        unhealthy
-        "Subgraph halted due to errors"
-        failed
+        earliestBlock: EarliestBlock
+        latestBlock: Block
+        lastHealthyBlock: Block
       }
 
       type SubgraphIndexingStatus {
         subgraph: String!
-        synced: Boolean!
-
-        # Note that the health can be implied from fatalError and nonFatalErrors:
-        # - If fatalError is non-null, then health is 'failed'.
-        # - Else if nonFatalErrors is non-empty, then health is 'unhealthy'.
-        # - Else health is 'healthy'.
-        health: Health!
-
-        "If the subgraph has failed, this is the error caused it"
-        fatalError: SubgraphError
-
-        "Sorted from first to last, limited to first 1000"
-        nonFatalErrors: [SubgraphError!]!
         chains: [ChainIndexingStatus!]!
-        entityCount: BigInt!
-        node: String
-        paused: Boolean!
-        historyBlocks: Int!
       }
 
       extend type Query {
@@ -76,12 +41,6 @@ export const IndexingStatusPlugin = makeExtendSchemaPlugin(() => {
           return [
             {
               subgraph: 'geo',
-              synced: true,
-              health: 0,
-              fatalError: null,
-              entityCount: 0,
-              node: 'geo',
-              paused: false,
               chains: [
                 {
                   network: 'geo',
@@ -89,9 +48,21 @@ export const IndexingStatusPlugin = makeExtendSchemaPlugin(() => {
                     number: head.number,
                     hash: head.hash,
                   },
+                  earliestBlock: {
+                    number: INITIAL_GEO_BLOCK,
+                  },
+                  latestBlock: {
+                    // @TODO: Fetch from database
+                    number: head.number,
+                    hash: head.hash,
+                  },
+                  latestHealthyBlock: {
+                    // @TODO: Fetch from database
+                    number: head.number,
+                    hash: head.hash,
+                  },
                 },
               ],
-              nonFatalErrors: [],
             },
           ];
         },
