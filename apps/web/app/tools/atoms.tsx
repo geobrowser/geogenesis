451d55e1022ba3830c7a3fa150cb2f13d278b516
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-06-07 17:17:05 +0000

    fix: add atom type

--- apps/web/app/tools/atoms.tsx
+++ apps/web/app/tools/atoms.tsx
@@ -1,5 +1,7 @@
 import { atom } from 'jotai';
 
+import { CreateTripleAction } from '~/core/types';
+
 export const cloneSpaceNameAtom = atom('');
 export const cloneSpaceIdAtom = atom('');
-export const cloneActionsAtom = atom([]);
+export const cloneActionsAtom = atom<Array<CreateTripleAction>>([]);
