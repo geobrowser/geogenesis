73019ef595d8c3cb91edd78f1b8cf61774446322
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-06-20 23:11:48 +0000

    feat: blocks ssr

---
+++ apps/web/partials/editor/server-content.tsx
@@ -0,0 +1,127 @@
+type ServerContentProps = {
+  content: any[];
+};
+
+export const ServerContent = ({ content }: ServerContentProps) => {
+  return (
+    <div className="tiptap ProseMirror !pb-[2rem]">
+      {content.map((block, index) => (
+        <Block key={index} block={block} />
+      ))}
+      <TrailingBreak />
+    </div>
+  );
+};
+
+type BlockProps = {
+  block: any;
+};
+
+const Block = ({ block }: BlockProps) => {
+  switch (block.type) {
+    case 'paragraph': {
+      return (
+        <div className="react-renderer node-paragraph">
+          <div className="whitespace-normal">
+            <p>
+              {block.content.map((block: any, index: number) => (
+                <Block key={index} block={block} />
+              ))}
+            </p>
+          </div>
+        </div>
+      );
+    }
+
+    case 'bulletList': {
+      return (
+        <ul>
+          {block.content.map((block: any, index: number) => (
+            <Block key={index} block={block} />
+          ))}
+        </ul>
+      );
+    }
+
+    case 'orderedList': {
+      return (
+        <ol>
+          {block.content.map((block: any, index: number) => (
+            <Block key={index} block={block} />
+          ))}
+        </ol>
+      );
+    }
+
+    case 'listItem': {
+      return (
+        <li>
+          {block.content.map((block: any, index: number) => (
+            <Block key={index} block={block} />
+          ))}
+        </li>
+      );
+    }
+
+    case 'heading': {
+      const Component = getHeading(block.attrs.level);
+
+      return (
+        <div className="react-renderer node-heading">
+          <Component>
+            {block.content.map((block: any, index: number) => (
+              <Block key={index} block={block} />
+            ))}
+          </Component>
+        </div>
+      );
+    }
+
+    case 'text': {
+      if (block?.marks?.[0]?.type === 'bold') {
+        return <strong>{block.text}</strong>;
+      }
+
+      if (block?.marks?.[0]?.type === 'italic') {
+        return <em>{block.text}</em>;
+      }
+
+      return <>{block.text}</>;
+    }
+
+    case 'image': {
+      return <img src={block.attrs.src} alt="" />;
+    }
+  }
+};
+
+const getHeading = (level: 1 | 2 | 3 | 4 | 5 | 6) => {
+  switch (level) {
+    case 1:
+      return 'h1';
+    case 2:
+      return 'h2';
+    case 3:
+      return 'h3';
+    case 4:
+      return 'h4';
+    case 5:
+      return 'h5';
+    case 6:
+      return 'h6';
+  }
+};
+
+const TrailingBreak = () => {
+  return (
+    <div className="react-renderer node-paragraph">
+      <div className="whitespace-normal">
+        <p className="whitespace-pre">
+          <div>
+            <br className="ProseMirror-trailingBreak" />
+          </div>
+        </p>
+      </div>
+    </div>
+  );
+};
