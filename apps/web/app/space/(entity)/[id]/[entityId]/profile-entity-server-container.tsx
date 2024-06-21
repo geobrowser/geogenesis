0419aaf868543f487105c268678d8fd591d81498
Byron Guina <byron.guina@gmail.com>
2024-06-21 09:20:15 -0700

    fix: add back streaming referenced by

--- apps/web/app/space/(entity)/[id]/[entityId]/profile-entity-server-container.tsx
+++ apps/web/app/space/(entity)/[id]/[entityId]/profile-entity-server-container.tsx
@@ -7,7 +7,8 @@ import { Subgraph } from '~/core/io';
 import { fetchOnchainProfileByEntityId } from '~/core/io/fetch-onchain-profile-by-entity-id';
 import { NavUtils } from '~/core/utils/utils';
 
-// import { EntityReferencedByServerContainer } from '~/partials/entity-page/entity-page-referenced-by-server-container';
+import { EntityReferencedByServerContainer } from '~/partials/entity-page/entity-page-referenced-by-server-container';
+
 import { ProfilePageComponent } from './profile-entity-page';
 
 interface Props {
@@ -57,10 +58,13 @@ export async function ProfileEntityServerContainer({ params }: Props) {
       triples={person.triples}
       spaceId={params.id}
       referencedByComponent={
-        <></>
-        // <React.Suspense fallback={null}>
-        //   <EntityReferencedByServerContainer entityId={person.id} name={person.name} spaceId={params.id} />
-        // </React.Suspense>
+        /* 
+          Some SEO parsers fail to parse meta tags if there's no fallback in a suspense boundary. We don't want to
+          show any referenced by loading states but do want to stream it in
+        */
+        <React.Suspense fallback={<div />}>
+          <EntityReferencedByServerContainer entityId={params.entityId} name={person.name} spaceId={params.id} />
+        </React.Suspense>
       }
     />
   );
