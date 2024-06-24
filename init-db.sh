20d5fb4899405503060860ce909a00561158b8b7
Yaro Shkvorets <shkvorets@gmail.com>
2024-06-24 23:32:44 +0000

    add postgres port env var

--- init-db.sh
+++ init-db.sh
@@ -2,7 +2,7 @@
 
 set -e
 source .env
-GEO_DB_URL=postgres://${GEO_DB_USER}:${GEO_DB_PASSWORD}@localhost:5432/${GEO_DB_NAME}
+GEO_DB_URL=postgres://${GEO_DB_USER}:${GEO_DB_PASSWORD}@localhost:${GEO_DB_PORT}/${GEO_DB_NAME}
 
 echo "Running SQL scripts..."
 psql $GEO_DB_URL < ./packages/substream/sink/sql/init-public.sql
