87586c992fb65be0dcdf28b378ba44ccf85fecde
Byron Guina <byron.guina@gmail.com>
2024-06-26 16:30:04 -0700

    feat: use ipfs proposals content for approved content population
    We don't need to pass the DB proposals since we have all the metadata already in IPFS. Eventually we should move to reading all the IPFS contents from the DB, including ops, since it's a lot faster than querying IPFS again.

--- packages/substream/sink/entries/populate-approved-content-proposal.ts
+++ packages/substream/sink/entries/populate-approved-content-proposal.ts
@@ -6,17 +6,19 @@ import type * as Schema from 'zapatos/schema';
 import { Entities } from '../db';
 import { type SchemaTripleEdit, mapSchemaTriples } from '../events/proposal-processed/map-triples';
 import { populateTriples } from '../events/proposal-processed/populate-triples';
+import type { EditProposal } from '../events/proposals-created/parser';
 import type { BlockEvent, Op } from '../types';
 import { upsertChunked } from '../utils/db';
 import { createVersionId } from '../utils/id';
 import { pool } from '../utils/pool';
 
 export function populateApprovedContentProposal(
-  proposals: Schema.proposals.Selectable[],
+  proposals: EditProposal[],
+  // proposals: Schema.proposals.Selectable[],
   // Since we read from IPFS to get the onchain id we also just pass in the actions
   // so we don't have to query the DB. Also so we know we get the correct order
   // of the actions from IPFS.
-  ops: Op[],
+  // ops: Op[],
   block: BlockEvent
 ) {
   return Effect.gen(function* (awaited) {
@@ -24,12 +26,24 @@ export function populateApprovedContentProposal(
       Effect.all(
         proposals.map(p => {
           return Effect.tryPromise({
-            try: () => db.select('proposed_versions', { proposal_id: p.id }).run(pool),
+            try: () => db.select('proposed_versions', { proposal_id: p.proposalId }).run(pool),
             catch: error => new Error(`Failed to fetch proposed versions. ${(error as Error).message}`),
           });
         })
       )
     );
+
+    // We group by the proposal id since we later write triples for a specific
+    // proposed version. We only want to write the ops that were actually part
+    // of the proposed version being written.
+    //
+    // There might be multiple proposals and proposed versions changing the same
+    // entity id so we need to ensure that we only write the ops relevant to the
+    // specific proposal/proposedVersion.
+    const opsByProposalId = proposals.reduce((acc, p) => {
+      acc.set(p.proposalId, p.ops);
+      return acc;
+    }, new Map<string, Op[]>());
 
     // We might get multiple proposals at once in the same block that change the same set of entities.
     // We need to make  sure that we process the proposals in order to avoid conflicts when writing to
@@ -47,9 +61,6 @@ export function populateApprovedContentProposal(
 
         return newEntity;
       });
-
-      if (entities.some(e => e.id === 'e9b2ca5149f743b7ab1e57f5a65d8009'))
-        console.log('proposed version has duplicate block');
 
       const versions = proposedVersions.map(pv => {
         const newVersion: Schema.versions.Insertable = {
@@ -85,13 +96,13 @@ export function populateApprovedContentProposal(
             // }),
             catch: error => new Error(`Failed to insert bulk entities. ${(error as Error).message}`),
           }),
-          Effect.tryPromise({
-            try: () =>
-              upsertChunked('proposals', proposals, 'id', {
-                updateColumns: db.doNothing,
-              }),
-            catch: error => new Error(`Failed to insert bulk proposals. ${(error as Error).message}`),
-          }),
+          // Effect.tryPromise({
+          //   try: () =>
+          //     upsertChunked('proposals', proposals, 'id', {
+          //       updateColumns: db.doNothing,
+          //     }),
+          //   catch: error => new Error(`Failed to insert bulk proposals. ${(error as Error).message}`),
+          // }),
           Effect.tryPromise({
             try: () =>
               upsertChunked('versions', versions, 'id', {
@@ -106,14 +117,31 @@ export function populateApprovedContentProposal(
         ])
       );
 
-      const opsWithCreatedById = proposedVersions.map(
-        (pv): SchemaTripleEdit => ({
+      /**
+       * We need to map each set of ops with the proposal/proposed version that contains
+       * the ops.
+       *
+       * An entity might change in multiple proposals that are executed within the same block.
+       * The current implementation groups _all_ ops across _all_ proposals/proposedVersions
+       * for a given entity, so we could write the same ops multiple times if there are multiple
+       * proposedVersions that change the same entity.
+       */
+
+      const opsWithCreatedById = proposedVersions.map((pv): SchemaTripleEdit => {
+        const ops = opsByProposalId.get(pv.proposal_id)!.filter(o => o.payload.entityId === pv.entity_id);
+
+        if (pv.entity_id === 'f1b9fd886388436e95b551aafaea77e5') {
+          console.log('ops for types block', { ops: JSON.stringify(ops, null, 2), proposalId: pv.proposal_id });
+        }
+
+        return {
           proposalId: pv.proposal_id,
           createdById: pv.created_by_id,
           spaceId: pv.space_id,
-          ops: ops.filter(o => o.payload.entityId === pv.entity_id),
-        })
-      );
+          // Safe to cast with ! since we know that we set this in the mapping previously
+          ops,
+        };
+      });
 
       const schemaTriples = opsWithCreatedById.map(o => mapSchemaTriples(o, block)).flat();
 
