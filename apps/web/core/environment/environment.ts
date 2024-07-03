b03699959234438f0c87577ef14ec28254b7767b
Byron Guina <byron.guina@gmail.com>
2024-07-03 16:22:04 -0700

    feat: use staging api endpoint

--- apps/web/core/environment/environment.ts
+++ apps/web/core/environment/environment.ts
@@ -56,8 +56,7 @@ export const options: Record<AppEnv, AppConfig> = {
     chainId: '19411',
     rpc: variables.rpcEndpoint,
     ipfs: IPFS_GATEWAY_PATH,
-    // api: 'https://geo-conduit.up.railway.app/graphql',
-    api: 'http://localhost:5001/graphql',
+    api: 'https://geo-conduit.up.railway.app/graphql',
     bundler: `https://api.pimlico.io/v2/geo-testnet/rpc?apikey=${variables.accountAbstractionApiKey}`,
   },
   testnet: {
