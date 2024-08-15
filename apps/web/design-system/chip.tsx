7af4ac980b2b056fcf05f34e74ad298bebc9d9fe
Byron Guina <byron.guina@gmail.com>
2024-08-15 13:39:58 -0700

    feat: real links

--- apps/web/design-system/chip.tsx
+++ apps/web/design-system/chip.tsx
@@ -20,7 +20,7 @@ interface LinkableChipProps {
 }
 
 const linkableChipStyles = cva(
-  'inline-flex min-h-[1.5rem] items-center break-words rounded border border-grey-02 bg-white py-1 pl-1.5 text-left text-metadataMedium !font-normal !leading-[1.125rem] hover:cursor-pointer hover:border-text hover:bg-ctaTertiary hover:text-text focus:cursor-pointer focus:border-text focus:bg-ctaTertiary focus:text-text focus:shadow-inner-lg',
+  'inline-flex min-h-[1.5rem] items-center break-words rounded border border-grey-02 bg-white py-1 pl-1.5 text-left text-metadataMedium !font-normal !leading-[1.125rem] hover:cursor-pointer hover:border-text hover:text-text focus:cursor-pointer focus:border-text focus:bg-ctaTertiary focus:text-text focus:shadow-inner-lg',
   {
     variants: {
       shouldClamp: {
@@ -49,22 +49,22 @@ export function LinkableRelationChip({ href, children }: LinkableChipProps) {
 
   return (
     <div className={linkableChipStyles({ shouldClamp: typeof children === 'string' && children.length >= 42 })}>
-      <div className="flex items-center gap-1">
+      <div className="flex items-center">
         <Link href={href}>{children}</Link>
         <Tooltip.Provider delayDuration={0}>
           <Tooltip.Root>
             <Tooltip.Trigger asChild>
-              <button className="pl-1 pr-1.5 hover:stroke-text">
-                <RelationDots color="grey-03" />
+              <button className="stroke-grey-03 px-1.5 hover:stroke-text">
+                <RelationDots />
               </button>
             </Tooltip.Trigger>
             <Tooltip.Portal>
               <Tooltip.Content
                 sideOffset={2}
-                className="flex items-center gap-2 rounded-[7px] border border-grey-04 bg-white px-1 py-2"
+                className="flex items-center gap-2 rounded-[7px] border border-grey-04 bg-white p-1"
               >
-                <MemberTiny />
-                <MemberTiny />
+                <RelationLinkSmall />
+                <CheckCloseSmall />
               </Tooltip.Content>
             </Tooltip.Portal>
           </Tooltip.Root>
@@ -130,6 +130,18 @@ function RelationDots({ color }: RelationDotsProps) {
     <svg width="2" height="6" viewBox="0 0 2 6" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="1" cy="1" r="1" fill={themeColor} />
       <circle cx="1" cy="5" r="1" fill={themeColor} />
+    </svg>
+  );
+}
+
+function RelationLinkSmall({ color }: RelationDotsProps) {
+  const themeColor = color ? colors.light[color] : 'currentColor';
+
+  return (
+    <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="8.99988" cy="3" r="2" transform="rotate(-180 8.99988 3)" stroke={themeColor} />
+      <rect x="7.49988" y="3.5" width="2.99993" height="1" transform="rotate(-180 7.49988 3.5)" fill={themeColor} />
+      <circle cx="3" cy="3" r="2" transform="rotate(-180 3 3)" stroke={themeColor} />
     </svg>
   );
 }
