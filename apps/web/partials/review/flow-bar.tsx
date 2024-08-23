fa06d9185bc9782274402e6e1d0019c6bfdde579
Byron <26263630+baiirun@users.noreply.github.com>
2024-08-23 12:29:58 -0700

    feat: do not return deleted triples by default, but allow escape hatch (#795)
    This is mostly used by things like diffs or the flowbar

--- apps/web/partials/review/flow-bar.tsx
+++ apps/web/partials/review/flow-bar.tsx
@@ -25,7 +25,7 @@ export const FlowBar = () => {
   const { editable } = useEditable();
   const { isReviewOpen, setIsReviewOpen } = useDiff();
 
-  const triples = useTriples(React.useMemo(() => ({ selector: t => t.hasBeenPublished === false }), []));
+  const triples = useTriples(React.useMemo(() => ({ selector: t => t.hasBeenPublished === false, includeDeleted: true }), []));
 
   const opsCount = triples.length;
 
