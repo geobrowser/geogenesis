740e61b2074fbb81e063d99b07b5043ca6bfe467
Byron Guina <byron.guina@gmail.com>
2024-10-24 11:39:46 -0700

    fix: always show non-editing-aware items in table block context menu

--- apps/web/partials/blocks/table/table-block-context-menu.tsx
+++ apps/web/partials/blocks/table/table-block-context-menu.tsx
@@ -302,25 +302,25 @@ export function TableBlockContextMenu({
                       </div>
                     }
                   /> */}
-                  <MenuItem>
-                    <Link
-                      href={NavUtils.toEntity(spaceId, entityId)}
-                      className="flex w-full items-center justify-between gap-2"
-                    >
-                      <span>View config</span>
-                      <Cog />
-                    </Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <button onClick={onCopyBlockId} className="flex w-full items-center justify-between gap-2">
-                      <span>Copy block ID</span>
-                      <Copy />
-                    </button>
-                  </MenuItem>
                 </>
               )}
             </>
           )}
+          <MenuItem>
+            <Link
+              href={NavUtils.toEntity(spaceId, entityId)}
+              className="flex w-full items-center justify-between gap-2"
+            >
+              <span>View config</span>
+              <Cog />
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <button onClick={onCopyBlockId} className="flex w-full items-center justify-between gap-2">
+              <span>Copy block ID</span>
+              <Copy />
+            </button>
+          </MenuItem>
           {isEditingDataSource && <DataBlockSourceMenu onBack={() => setIsEditingDataSource(false)} />}
           {isEditingColumns && (
             <>
