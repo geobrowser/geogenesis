4aa10d2218fd9a8b615ce929d56b1b12c3ef44ad
Byron Guina <byron.guina@gmail.com>
2024-11-26 10:16:23 -0800

    Revert "feat: bootstrap nonprofit team page"
    This reverts commit 982bc10cf7cbeb52c2f1ccf5cb7acd285e51968e.

--- apps/web/core/environment/environment.ts
+++ apps/web/core/environment/environment.ts
@@ -11,7 +11,7 @@ export type AppConfig = {
   bundler: string;
 };
 
-type IVars = Readonly<{
+export type IVars = Readonly<{
   liveBlocksPublicKey: string;
   appEnv: string;
   walletConnectProjectId: string;
@@ -33,6 +33,8 @@ export const variables: IVars = {
   accountAbstractionApiKey: process.env.NEXT_PUBLIC_PIMLICO_API_KEY!,
 };
 
+export const DEFAULT_ENV: AppEnv = 'production';
+
 // @TODO: This eventually completely comes from our environment instead of hardcoded here.
 // We can ensure our env matches the right schema in `make` above.
 export const options: Record<AppEnv, AppConfig> = {
@@ -47,7 +49,7 @@ export const options: Record<AppEnv, AppConfig> = {
     chainId: '19411',
     rpc: variables.rpcEndpoint,
     ipfs: IPFS_GATEWAY_PATH,
-    api: 'http://localhost:5001/graphql',
+    api: 'https://geo-conduit.up.railway.app/graphql',
     bundler: `https://api.pimlico.io/v2/geo-testnet/rpc?apikey=${variables.accountAbstractionApiKey}`,
   },
   testnet: {
