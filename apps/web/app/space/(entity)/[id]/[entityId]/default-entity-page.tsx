82af0266b5eb4401d54f984d606628ffe3250bf6
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-11-27 18:03:36 +0000

    fix: redirect

--- apps/web/app/space/(entity)/[id]/[entityId]/default-entity-page.tsx
+++ apps/web/app/space/(entity)/[id]/[entityId]/default-entity-page.tsx
@@ -87,8 +87,7 @@ const getData = async (spaceId: string, entityId: string) => {
   // Redirect from space configuration page to space page
   if (entity?.types.some(type => type.id === SYSTEM_IDS.SPACE_CONFIGURATION) && nameTripleSpace) {
     // But don't redirect for space configuration templates in the root space
-
-    if (spaceId !== SYSTEM_IDS.ROOT_SPACE || entityId === SYSTEM_IDS.ROOT_SPACE_CONFIGURATION) {
+    if (spaceId !== SYSTEM_IDS.ROOT_SPACE_ID) {
       // Uncomment to navigate to space configuration templates in local development
       // if (process.env.NODE_ENV === 'development') return;
 
