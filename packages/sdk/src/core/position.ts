5eee5d3afa51ed6808e9fa4d7e403566242b7a2d
Byron Guina <byron.guina@gmail.com>
2024-12-02 11:42:58 -0800

    feat: use empty space for relation ordering namespace id
    We can pass an id to PositionSource. This id prefixes the position string. It's useful for collaborative application, but we don't need it for our uses atm. Removing the prefix makes our index storage more efficient for now.

--- packages/sdk/src/core/position.ts
+++ packages/sdk/src/core/position.ts
@@ -1,12 +1,10 @@
 import { PositionSource } from 'position-strings';
 
-import { GEO } from './ids/network.js';
-
 /**
  * We use the Geo network id as the source id for all
  * relations consumed by the sdk.
  */
-export const Position = new PositionSource({ ID: GEO });
+export const Position = new PositionSource({ ID: '' });
 export const PositionRange = {
   FIRST: PositionSource.FIRST,
   LAST: PositionSource.LAST,
