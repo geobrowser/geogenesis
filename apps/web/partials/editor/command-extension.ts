77fd223fb6fd22be84bf8c6d970726386be9ee60
Byron Guina <byron.guina@gmail.com>
2024-12-19 14:36:20 -0800

    fix: imports

--- apps/web/partials/editor/command-extension.ts
+++ apps/web/partials/editor/command-extension.ts
@@ -5,7 +5,7 @@ import tippy from 'tippy.js';
 import type { Instance } from 'tippy.js';
 
 import { CommandSuggestionItem, commandItems } from './command-items';
-import { CommandList, CommandListRef } from './command-list';
+import { CommandList } from './command-list';
 
 export const CommandExtension = Extension.create<{
   suggestion: Omit<SuggestionOptions<CommandSuggestionItem>, 'editor'>;
