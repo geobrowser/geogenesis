7658923cc62214c6d3328066a93f6bc591f09a2e
Byron Guina <byron.guina@gmail.com>
2024-12-19 12:36:57 -0800

    fix: unused log

--- packages/substream/index.ts
+++ packages/substream/index.ts
@@ -48,7 +48,6 @@ const main = Effect.gen(function* (_) {
       process.exit(1);
     }
 
-    console.log('bootstrapping test');
     const testBootstrap = yield* _(pipe(bootstrapTest, Effect.either));
 
     if (Either.isLeft(testBootstrap)) {
