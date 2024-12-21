4493c9c41c61cda040e7cc33be1c3010f863e54b
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-12-21 08:22:10 +0100

    feat: image blocks (#905)
    * feat: image block
    
    * fix: set image url triple to correct entity id
    
    * fix: remove comment
    
    * fix: unused console log
    
    ---------
    
    Co-authored-by: Byron Guina <byron.guina@gmail.com>

--- apps/web/core/state/editor/block-types.ts
+++ apps/web/core/state/editor/block-types.ts
@@ -4,7 +4,7 @@ import { INITIAL_RELATION_INDEX_VALUE } from '@geogenesis/sdk/constants';
 import { StoreRelation } from '~/core/database/types';
 import { EntityId } from '~/core/io/schema';
 
-type BlockTypeId = typeof SYSTEM_IDS.TEXT_BLOCK | typeof SYSTEM_IDS.IMAGE_BLOCK | typeof SYSTEM_IDS.DATA_BLOCK;
+type BlockTypeId = typeof SYSTEM_IDS.TEXT_BLOCK | typeof SYSTEM_IDS.IMAGE_TYPE | typeof SYSTEM_IDS.DATA_BLOCK;
 
 export function getRelationForBlockType(
   fromBlockEntityId: string,
