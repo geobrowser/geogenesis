f3647fa6fd3e33c359afd80b03c1a61d52c45911
Byron Guina <byron.guina@gmail.com>
2025-01-06 16:08:07 -0800

    fix: correct slug

--- apps/web/app/space/[id]/ontology/page.tsx
+++ apps/web/app/space/[id]/ontology/page.tsx
@@ -3,5 +3,5 @@ import { Tab } from '~/partials/tab/tab';
 import type { PageProps } from '../types';
 
 export default async function Page(props: PageProps) {
-  return <Tab slug="education" {...props} />;
+  return <Tab slug="ontology" {...props} />;
 }
