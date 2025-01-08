ee95707e25eee9f54f6baef8a0c282478dd56bde
Byron Guina <byron.guina@gmail.com>
2025-01-08 10:32:52 -0800

    fix: correct env

--- apps/web/core/environment/environment.ts
+++ apps/web/core/environment/environment.ts
@@ -60,8 +60,7 @@ export const options: Record<AppEnv, AppConfig> = {
     chainId: '80451',
     rpc: variables.rpcEndpoint,
     ipfs: IPFS_GATEWAY_PATH,
-    // api: 'https://geo-mainnet.up.railway.app/graphql',
-    api: 'http://localhost:5001/graphql',
+    api: 'https://geo-mainnet.up.railway.app/graphql',
     bundler: `https://api.pimlico.io/v2/80451/rpc?apikey=${variables.accountAbstractionApiKey}`,
   },
   testnet: {
