968c68ece9d4450b80e5620badd3ae3183677f77
Byron Guina <byron.guina@gmail.com>
2025-01-08 18:44:47 -0800

    fix: subspace query

--- apps/web/partials/space-page/add-subspace-dialog.tsx
+++ apps/web/partials/space-page/add-subspace-dialog.tsx
@@ -57,9 +57,9 @@ interface ContentProps {
 const subspacesQuery = (name: string, notIn: string[]) => `
   {
     spaces(
-            filter: { spacesMetadatum: { version: { name: { includesInsensitive: "${name}" } } } }, id: { notIn: ${JSON.stringify(
-              notIn
-            )} } }
+      filter: { spacesMetadatum: {version: {name: {includesInsensitive: "${name}"}}} id: {notIn: ${JSON.stringify(
+        notIn
+      )}} }
       first: 10
     ) {
       nodes {
