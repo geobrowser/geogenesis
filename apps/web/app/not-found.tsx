c74e550d6633cf7895338c5e66a0846aa324b40b
Byron Guina <byron.guina@gmail.com>
2025-01-28 15:44:09 -0800

    fix: providers in 404 page

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { Providers } from '~/core/providers';
 import { NavUtils } from '~/core/utils/utils';
 
 import { Button } from '~/design-system/button';
@@ -9,37 +10,39 @@ import { Text } from '~/design-system/text';
 
 export default function Custom404() {
   return (
-    <div className="relative flex min-h-[75vh] flex-col items-center justify-center">
-      <div className="z-10 flex flex-col items-center">
-        <img src="/404.svg" alt="Image of a 404 error." />
-        <Spacer height={24} />
-        <Text as="h1" variant="bodySemibold">
-          This page could not be found :-(
-        </Text>
-        <Spacer height={16} />
-        <Button>
-          <Link href={NavUtils.toRoot()}>
-            <Text variant="button" color="white">
-              Go to root space
-            </Text>
-          </Link>
-        </Button>
+    <Providers>
+      <div className="relative flex min-h-[75vh] flex-col items-center justify-center">
+        <div className="z-10 flex flex-col items-center">
+          <img src="/404.svg" alt="Image of a 404 error." />
+          <Spacer height={24} />
+          <Text as="h1" variant="bodySemibold">
+            This page could not be found :-(
+          </Text>
+          <Spacer height={16} />
+          <Button>
+            <Link href={NavUtils.toRoot()}>
+              <Text variant="button" color="white">
+                Go to root space
+              </Text>
+            </Link>
+          </Button>
+        </div>
+        <img
+          src="/405.svg"
+          style={{ position: 'absolute', opacity: 1, zIndex: 5 }}
+          alt="Background image of a 404 error"
+        />
+        <img
+          src="/406.svg"
+          style={{ position: 'absolute', opacity: 1, zIndex: 4 }}
+          alt="Background image of a 404 error"
+        />
+        <img
+          src="/407.svg"
+          style={{ position: 'absolute', opacity: 1, zIndex: 3 }}
+          alt="Background image of a 404 error"
+        />
       </div>
-      <img
-        src="/405.svg"
-        style={{ position: 'absolute', opacity: 1, zIndex: 5 }}
-        alt="Background image of a 404 error"
-      />
-      <img
-        src="/406.svg"
-        style={{ position: 'absolute', opacity: 1, zIndex: 4 }}
-        alt="Background image of a 404 error"
-      />
-      <img
-        src="/407.svg"
-        style={{ position: 'absolute', opacity: 1, zIndex: 3 }}
-        alt="Background image of a 404 error"
-      />
-    </div>
+    </Providers>
   );
 }
