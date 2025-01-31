fbd3256085432d3eee7daf598321397a0e41ee5c
Byron Guina <byron.guina@gmail.com>
2025-01-31 14:43:04 -0800

    fix: dialog rendering
    Framer motion upgrades broke dialog rendering. Removing motion for now.

--- apps/web/design-system/dialog.tsx
+++ apps/web/design-system/dialog.tsx
@@ -1,15 +1,11 @@
 'use client';
 
 import { Content, Overlay, Portal, Root, Trigger } from '@radix-ui/react-dialog';
-import { motion } from 'framer-motion';
 
 import * as React from 'react';
 
 import { SquareButton } from '~/design-system/button';
 import { Close } from '~/design-system/icons/close';
-
-const MotionContent = motion(Content);
-const MotionOverlay = motion(Overlay);
 
 interface Props {
   trigger: React.ReactNode;
@@ -25,20 +21,9 @@ export function Dialog(props: Props) {
       <Trigger className="w-full">{props.trigger}</Trigger>
 
       <Portal>
-        <MotionOverlay
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 0.2 }}
-          transition={{ type: 'tween', ease: 'easeInOut', duration: 0.15, opacity: { duration: 0.1 } }}
-          className="fixed inset-0 z-100 bg-text"
-        />
+        <Overlay className="fixed inset-0 z-100 bg-text bg-opacity-20" />
 
-        <MotionContent
-          initial={{ opacity: 0, scale: 0.95 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ type: 'spring', bounce: 0, duration: 0.15 }}
-          className="fixed inset-0 z-[101] flex items-start justify-center focus:outline-none"
-        >
+        <Content className="fixed inset-0 z-[101] flex items-start justify-center focus:outline-none">
           <div className="mt-40 inline-flex max-h-[415px] max-w-[586px] flex-col gap-3 overflow-y-auto rounded-lg bg-white p-4">
             <div className="flex items-center justify-between">
               {props.header}
@@ -47,7 +32,7 @@ export function Dialog(props: Props) {
 
             {props.content}
           </div>
-        </MotionContent>
+        </Content>
       </Portal>
     </Root>
   );
