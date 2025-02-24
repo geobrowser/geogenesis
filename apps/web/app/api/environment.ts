d917179a142d8fd913531ea17547788864791db3
mikallojjus <74614819+mikallojjus@users.noreply.github.com>
2025-02-24 21:25:06 +0200

    chore: telemetry env variables made optional (#1099)
    * chore: make telemetry env variables optional
    
    * chore: add .env.example to apps/web
    
    * chore: fix EOF

--- apps/web/app/api/environment.ts
+++ apps/web/app/api/environment.ts
@@ -2,12 +2,12 @@ import { GEO_PK, TELEMETRY_TOKEN, TELEMETRY_URL } from './config';
 
 type ServerEnvironment = {
   geoPk: string;
-  telemetryUrl: string;
-  telemetryApiKey: string;
+  telemetryUrl?: string;
+  telemetryApiKey?: string;
 };
 
 export const ServerEnvironment: ServerEnvironment = {
   geoPk: GEO_PK!,
-  telemetryUrl: TELEMETRY_URL!,
-  telemetryApiKey: TELEMETRY_TOKEN!,
+  telemetryUrl: TELEMETRY_URL,
+  telemetryApiKey: TELEMETRY_TOKEN,
 };
