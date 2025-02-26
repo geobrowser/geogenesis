7c53d5958e92c7e4610205fcdd002efcd7d04912
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2025-02-26 18:38:46 +0000

    fix: remove unused import

--- apps/web/partials/search/dialog.tsx
+++ apps/web/partials/search/dialog.tsx
@@ -2,15 +2,22 @@ import { A } from '@mobily/ts-belt';
 import cx from 'classnames';
 import { Command } from 'cmdk';
 import { AnimatePresence, motion } from 'framer-motion';
+import Fuse from 'fuse.js';
 import { useRouter } from 'next/navigation';
 
 import { useCallback, useState } from 'react';
+import * as React from 'react';
 
 import { useSearch } from '~/core/hooks/use-search';
+import { useSmartAccount } from '~/core/hooks/use-smart-account';
+import { useSpacesWhereMember } from '~/core/hooks/use-spaces-where-member';
+import { EntityId } from '~/core/io/schema';
+import { getImagePath, validateEntityId } from '~/core/utils/utils';
 import { NavUtils } from '~/core/utils/utils';
 
 import { ResultContent, ResultsList, SpaceContent } from '~/design-system/autocomplete/results-list';
 import { Dots } from '~/design-system/dots';
+import { ArrowLeft } from '~/design-system/icons/arrow-left';
 import { LeftArrowLong } from '~/design-system/icons/left-arrow-long';
 import { Search } from '~/design-system/icons/search';
 import { Input } from '~/design-system/input';
@@ -20,6 +27,8 @@ interface Props {
   open: boolean;
   onDone: () => void;
 }
+
+type View = 'selectEntity' | 'selectSpace' | 'createEntity';
 
 export const SearchDialog = ({ open, onDone }: Props) => {
   const router = useRouter();
@@ -34,6 +43,17 @@ export const SearchDialog = ({ open, onDone }: Props) => {
     onDone();
   }, [autocomplete, onDone]);
 
+  const isValidEntityId = validateEntityId(autocomplete.query);
+
+  const [isCreatingNewEntity, setIsCreatingNewEntity] = useState<boolean>(false);
+
+  let view: View = 'selectEntity';
+  if (openSpacesIndex !== null && selectedEntity) {
+    view = 'selectSpace';
+  } else if (isValidEntityId && isCreatingNewEntity) {
+    view = 'createEntity';
+  }
+
   if (!open) return null;
 
   return (
@@ -41,7 +61,18 @@ export const SearchDialog = ({ open, onDone }: Props) => {
       <div className="pointer-events-none fixed inset-0 z-100 flex h-full w-full items-start justify-center">
         <div className="pointer-events-auto mt-32 w-full max-w-[434px] overflow-hidden rounded-lg border border-grey-02 bg-white shadow-dropdown">
           <Command.List>
-            {openSpacesIndex === null && (
+            {view === 'createEntity' && (
+              <CreateNewEntityInSpace
+                entityId={autocomplete.query as EntityId}
+                setIsCreatingNewEntity={setIsCreatingNewEntity}
+                onDone={() => {
+                  autocomplete.onQueryChange('');
+                  setIsCreatingNewEntity(false);
+                  onDone();
+                }}
+              />
+            )}
+            {view === 'selectEntity' && (
               <>
                 <div className={cx('relative p-1', A.isNotEmpty(autocomplete.results) && 'border-b border-grey-02')}>
                   <AnimatePresence mode="wait">
@@ -79,9 +110,15 @@ export const SearchDialog = ({ open, onDone }: Props) => {
                 </div>
                 <ResizableContainer duration={0.15}>
                   <ResultsList>
-                    {autocomplete.isEmpty && (
-                      <Command.Empty className="px-2 pb-2">No results found for {autocomplete.query}</Command.Empty>
-                    )}
+                    {autocomplete.isEmpty ? (
+                      isValidEntityId ? (
+                        <div className="px-2 pb-1">
+                          <EntityIdNotFound setIsCreatingNewEntity={setIsCreatingNewEntity} />
+                        </div>
+                      ) : (
+                        <Command.Empty className="px-2 pb-2">No results found for {autocomplete.query}</Command.Empty>
+                      )
+                    ) : null}
                     {autocomplete.results.map((result, i) => (
                       <motion.div
                         key={result.id}
@@ -117,7 +154,7 @@ export const SearchDialog = ({ open, onDone }: Props) => {
                 </ResizableContainer>
               </>
             )}
-            {openSpacesIndex !== null && selectedEntity && (
+            {view === 'selectSpace' && (
               <div>
                 {/*
                   retains the focus trap inside the dialog even when the input above isn't rendered
@@ -134,7 +171,7 @@ export const SearchDialog = ({ open, onDone }: Props) => {
                   <div className="ml-2 text-footnoteMedium text-grey-04">Back</div>
                 </Command.Item>
                 <div>
-                  {selectedEntity.spaces.map((space, i) => {
+                  {selectedEntity?.spaces.map((space, i) => {
                     return (
                       <motion.div
                         initial={{ opacity: 0, y: -5 }}
@@ -170,5 +207,84 @@ export const SearchDialog = ({ open, onDone }: Props) => {
         </div>
       </div>
     </Command.Dialog>
+  );
+};
+
+type EntityIdNotFoundProps = {
+  setIsCreatingNewEntity: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const EntityIdNotFound = ({ setIsCreatingNewEntity }: EntityIdNotFoundProps) => {
+  return (
+    <div className="flex items-center justify-between gap-2">
+      <div>No entity exists with this ID</div>
+      <div>
+        <button onClick={() => setIsCreatingNewEntity(true)} className="text-ctaPrimary">
+          Create entity
+        </button>
+      </div>
+    </div>
+  );
+};
+
+type CreateNewEntityInSpaceProps = {
+  entityId: EntityId;
+  setIsCreatingNewEntity: React.Dispatch<React.SetStateAction<boolean>>;
+  onDone?: () => void;
+};
+
+const CreateNewEntityInSpace = ({ entityId, setIsCreatingNewEntity, onDone }: CreateNewEntityInSpaceProps) => {
+  const { smartAccount } = useSmartAccount();
+  const address = smartAccount?.account.address;
+  const spaces = useSpacesWhereMember(address);
+
+  const [query, setQuery] = useState<string>('');
+
+  const fuseOptions = {
+    keys: ['spaceConfig.name', 'spaceConfig.description'],
+  };
+
+  const fuse = new Fuse(spaces, fuseOptions);
+
+  const renderedSpaces = query.length === 0 ? spaces : fuse.search(query).map(result => result.item);
+
+  return (
+    <div>
+      <div className="border-grey flex items-center justify-between border-b border-grey-02">
+        <div className="flex-1 p-2">
+          <button onClick={() => setIsCreatingNewEntity(false)}>
+            <ArrowLeft />
+          </button>
+        </div>
+        <div className="flex-[4] p-2 text-center text-button text-text">Select space to create entity in</div>
+        <div className="flex-1"></div>
+      </div>
+      <div className="p-1">
+        <Input value={query} onChange={event => setQuery(event.target.value)} withSearchIcon />
+      </div>
+      <div className="max-h-[190px] space-y-1 overflow-auto p-1">
+        {renderedSpaces.map(space => {
+          return (
+            <Command.Item
+              key={space.id}
+              onSelect={() => {
+                NavUtils.toEntity(space.id, entityId);
+                onDone?.();
+              }}
+              className="flex cursor-pointer items-center gap-2 rounded p-1 transition-colors duration-150 ease-in-out hover:bg-grey-01"
+            >
+              <div className="relative size-4 rounded bg-grey-01">
+                <img
+                  src={getImagePath(space.spaceConfig.image)}
+                  alt=""
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
+              <div className="text-button text-text">{space.spaceConfig.name}</div>
+            </Command.Item>
+          );
+        })}
+      </div>
+    </div>
   );
 };
