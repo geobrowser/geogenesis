b5d68588e0fde300faa09b12849899548c6ed3a7
Byron Guina <byron.guina@gmail.com>
2025-03-26 11:51:58 -0700

    misc

--- apps/web/design-system/icons/filter.tsx
+++ apps/web/design-system/icons/filter.tsx
@@ -11,12 +11,11 @@ export function Filter({ color }: Props) {
 
   return (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M1.5 0V16" stroke={themeColor} />
-      <path d="M8 0L8 16" stroke={themeColor} />
-      <path d="M14.5 1V16" stroke={themeColor} />
-      <circle cx="1.5" cy="5.5" r="1.5" fill={themeColor} />
-      <circle cx="8" cy="11.5" r="1.5" fill={themeColor} />
-      <circle cx="14.5" cy="1.5" r="1.5" fill={themeColor} />
+      <path
+        d="M3.64681 0.5H12.3532C13.5374 0.5 14.2546 1.80779 13.618 2.80633L10.3919 7.86698C10.136 8.26847 10 8.73472 10 9.21086V14C10 14.8284 9.32843 15.5 8.5 15.5H7.5C6.67157 15.5 6 14.8284 6 14V9.21086C6 8.73472 5.86403 8.26848 5.60808 7.86698L2.38196 2.80633C1.74541 1.80779 2.46263 0.5 3.64681 0.5Z"
+        stroke={themeColor}
+        strokeLinecap="round"
+      />
     </svg>
   );
 }
