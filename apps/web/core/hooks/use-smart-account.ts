5e1b6bddac8e75f503be89a2369fa23bf8a79243
Byron Guina <byron.guina@gmail.com>
2025-04-02 14:38:53 -0700

    wip

--- apps/web/core/hooks/use-smart-account.ts
+++ apps/web/core/hooks/use-smart-account.ts
@@ -2,7 +2,7 @@
 
 import { useQuery } from '@tanstack/react-query';
 import { createSmartAccountClient } from 'permissionless';
-import { toSafeSmartAccount } from 'permissionless/accounts';
+import { toSafeSmartAccount, toSimpleSmartAccount } from 'permissionless/accounts';
 import { createPimlicoClient } from 'permissionless/clients/pimlico';
 import { useCookies } from 'react-cookie';
 import { createPublicClient, http } from 'viem';
@@ -13,6 +13,9 @@ import { useWalletClient } from 'wagmi';
 import { Cookie, WALLET_ADDRESS } from '../cookie';
 import { Environment } from '../environment';
 import { GEOGENESIS } from '../wallet/geo-chain';
+
+const ENTRY_POINT_07 =
+  Environment.variables.appEnv === 'production' ? entryPoint07Address : '0xEB0a534440145D5A3aa512352FDDCf85453182b9';
 
 export function useSmartAccount() {
   const { data: walletClient, isLoading: isLoadingWallet } = useWalletClient();
@@ -33,28 +36,41 @@ export function useSmartAccount() {
         chain: GEOGENESIS,
       });
 
-      const safeAccount = await toSafeSmartAccount({
-        client: publicClient,
-        owners: [walletClient],
-        entryPoint: {
-          version: '0.7',
-          address: entryPoint07Address,
-        },
-        version: '1.4.1',
-      });
+      console.log('Environment.variables.appEnv', Environment.variables.appEnv);
+
+      const smartAccountImplementation =
+        Environment.variables.appEnv === 'production'
+          ? await toSafeSmartAccount({
+              client: publicClient,
+              owners: [walletClient],
+              entryPoint: {
+                version: '0.7',
+                address: ENTRY_POINT_07,
+              },
+              version: '1.4.1',
+            })
+          : await toSimpleSmartAccount({
+              client: publicClient,
+              owner: walletClient,
+              factoryAddress: '0x91E60e0613810449d098b0b5Ec8b51A0FE8c8985',
+              entryPoint: {
+                version: '0.7',
+                address: ENTRY_POINT_07,
+              },
+            });
 
       const paymasterClient = createPimlicoClient({
         transport: bundlerTransport,
         chain: GEOGENESIS,
         entryPoint: {
-          address: entryPoint07Address,
+          address: ENTRY_POINT_07,
           version: '0.7',
         },
       });
 
       const smartAccount = createSmartAccountClient({
         chain: GEOGENESIS,
-        account: safeAccount,
+        account: smartAccountImplementation,
         paymaster: paymasterClient,
         bundlerTransport,
         userOperation: {
