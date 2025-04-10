a9d32029c301b31f60379d5ce2cd402511977297
Byron Guina <byron.guina@gmail.com>
2025-04-10 11:34:24 -0500

    fix: burner app id

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     environment: 'jsdom',
     env: {
       NEXT_PUBLIC_APP_ENV: 'production',
-      NEXT_PUBLIC_PRIVY_APP_ID: 'a',
+      NEXT_PUBLIC_PRIVY_APP_ID: 'clpsvsqpt005fl70fe775owo5',
       NEXT_PUBLIC_GEOGENESIS_RPC: 'a',
       NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: 'a',
       NEXT_PUBLIC_PIMLICO_API_KEY: 'a',
