0e8d03966eee38f457efb0f27da15e5c0aa53b3a
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2025-04-14 14:37:47 -0700

    feat: advanced select entity (#1163)

--- apps/web/partials/entity-page/editable-entity-page.tsx
+++ apps/web/partials/entity-page/editable-entity-page.tsx
@@ -261,7 +261,7 @@ function RelationsGroup({ relations, properties }: RelationsGroupProps) {
   const typeOfName = relations[0].attributeName;
   const typeOfRenderableType = relations[0].type;
   const property = properties?.[typeOfId];
-  const filterByTypes = property?.relationValueTypes?.map(r => r.typeId);
+  const relationValueTypes = property?.relationValueTypes;
 
   return (
     <div className="flex flex-wrap items-center gap-2">
@@ -337,8 +337,9 @@ function RelationsGroup({ relations, properties }: RelationsGroupProps) {
           return (
             <div key={`relation-select-entity-${relationId}`} data-testid="select-entity" className="w-full">
               <SelectEntity
+                key={JSON.stringify(relationValueTypes)}
                 spaceId={spaceId}
-                allowedTypes={filterByTypes ? filterByTypes : undefined}
+                relationValueTypes={relationValueTypes ? relationValueTypes : undefined}
                 onCreateEntity={result => {
                   if (property?.relationValueTypeId) {
                     send({
@@ -443,8 +444,9 @@ function RelationsGroup({ relations, properties }: RelationsGroupProps) {
       {!hasPlaceholders && typeOfRenderableType === 'RELATION' && (
         <div className="mt-1">
           <SelectEntityAsPopover
+            key={JSON.stringify(relationValueTypes)}
             trigger={<SquareButton icon={<Create />} />}
-            allowedTypes={filterByTypes ? filterByTypes : undefined}
+            relationValueTypes={relationValueTypes ? relationValueTypes : undefined}
             onCreateEntity={result => {
               if (property?.relationValueTypeId) {
                 send({
