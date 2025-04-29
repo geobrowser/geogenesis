94af0c16ac815c9d1d63bb199dfe23c2a79f4461
Bryan Doss <bryan.w.doss@gmail.com>
2025-04-29 14:02:01 -0400

    Cleanup comments

--- apps/web/partials/entity-page/entity-page-cover.tsx
+++ apps/web/partials/entity-page/entity-page-cover.tsx
@@ -25,49 +25,4 @@ export const EntityPageCover = ({
   const coverUrl = Entities.cover(relations) ?? serverCoverUrl;
 
   return <EditableCoverAvatarHeader avatarUrl={avatarUrl} triples={triples} coverUrl={coverUrl} />;
-
-  // if (coverUrl) {
-  //   return (
-  //     <div className={cx('relative mx-auto -mt-6 h-[320px] w-full max-w-[1192px]', avatarUrl ? 'mb-20' : 'mb-8')}>
-  //       <div className="relative h-full w-full overflow-hidden rounded-lg bg-grey-01">
-  //         <Image
-  //           src={getImagePath(coverUrl)}
-  //           layout="fill"
-  //           objectFit="cover"
-  //           priority
-  //           className="h-full w-full"
-  //           alt=""
-  //         />
-  //       </div>
-  //       {avatarUrl && (
-  //         <div className="absolute bottom-0 left-0 right-0">
-  //           <div className="mx-auto w-full max-w-[880px]">
-  //             <div className="relative h-[80px] w-[80px] translate-y-1/2 overflow-hidden rounded-lg border border-white bg-grey-01 shadow-lg">
-  //               <Image
-  //                 src={getImagePath(avatarUrl)}
-  //                 layout="fill"
-  //                 objectFit="cover"
-  //                 priority
-  //                 className="h-full w-full"
-  //                 alt=""
-  //               />
-  //             </div>
-  //           </div>
-  //         </div>
-  //       )}
-  //     </div>
-  //   );
-  // }
-
-  // if (avatarUrl) {
-  //   return (
-  //     <div className="mx-auto mb-10 w-[880px]">
-  //       <div className="relative h-[80px] w-[80px] overflow-hidden rounded-lg border border-white bg-grey-01 shadow-lg">
-  //         <Image src={getImagePath(avatarUrl)} layout="fill" objectFit="cover" className="h-full w-full" alt="" />
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
-  return null;
 };
