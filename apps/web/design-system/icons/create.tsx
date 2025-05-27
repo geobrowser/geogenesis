d7c49c766b0d42bec47ea1712c0b440e50ee2dff
Bryan <32337174+b-d055@users.noreply.github.com>
2025-05-27 12:06:11 -0400

    Feat/avatar cover inline (#1183)
    * added cover placeholder to upload cover image
    
    * addde cover-avatar upload/delete feature (editable entity page)
    
    * changed position of relation (types) of entity
    
    * added cover/avater placeholder to space page
    
    * changed cover image height when image is uploaded in editing mode
    
    * merged with latest chages from master
    
    * Style cleanup
    
    * Add jotai family for shared placeholder renderables and cleanup system content IDs
    
    * Debug cleanup
    
    * Cleanup comments
    
    * style revert
    
    * More ID cleanup and linting
    
    * Remove comments, fix system IDs, cleanup
    
    * Test commit
    
    * Make avatar and cover areas fully clickable to trigger upload
    
    * Improve avatar cover upload UX
    
    * Prop & lint cleanup
    
    * Replace placeholder avatar, fix pladerholder select/popover relation
    
    * Updating avatar hover placeholder
    
    * Build cleanup
    
    * Add global state for editor having content to hide/show properties, add types button in cover/avatar header
    
    * Remove unused props and comment
    
    * Adding back change from 70767c10aa139721e4f829b0be4ad4c299813e06
    
    ---------
    
    Co-authored-by: Ivan Velychko <lviv08@gmail.com>

--- apps/web/design-system/icons/create.tsx
+++ apps/web/design-system/icons/create.tsx
@@ -4,13 +4,21 @@ import { ColorName, colors } from '~/design-system/theme/colors';
 
 interface Props {
   color?: ColorName;
+  className?: string;
 }
 
-export function Create({ color }: Props) {
+export function Create({ color, className }: Props) {
   const themeColor = color ? colors.light[color] : 'currentColor';
 
   return (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      className={className ? className : ''}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path d="M8 0V16" stroke={themeColor} />
       <path d="M0 8L16 8" stroke={themeColor} />
     </svg>
