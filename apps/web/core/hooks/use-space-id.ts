1ec05da4984d63e5af18ce321ec6faa3b02aad4e
Byron Guina <byron.guina@gmail.com>
2025-06-24 10:28:33 -0700

    fix: query root space

--- apps/web/core/hooks/use-space-id.ts
+++ apps/web/core/hooks/use-space-id.ts
@@ -1,14 +1,14 @@
-import { SystemIds } from '@graphprotocol/grc-20';
 import { useParams } from 'next/navigation';
 
 import { usePathSegments } from '~/core/hooks/use-path-segments';
+import { ROOT_SPACE } from '../constants';
 
 export const useSpaceId = () => {
   const params = useParams();
   const segment = usePathSegments();
 
   if (segment[0] === 'root') {
-    return SystemIds.ROOT_SPACE_ID;
+    return ROOT_SPACE;
   }
 
   const spaceId = params?.['id'] as string | undefined;
