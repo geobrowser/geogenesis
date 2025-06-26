d7ffab3cb71c775cefe5ebea38c5df6c359dec5c
Byron <26263630+baiirun@users.noreply.github.com>
2025-06-26 10:44:41 -0700

    feat: cache as view for entity sync store (#1234)
    * feat: only sync entities that haven't already been synced
    
    * feat: update cache directly for store results
    
    * fix: can useEntity in useBlocks now
    
    * fix: docs
    
    * fix: unused effect comparison
    
    * feat: tracking
    
    * fix: first, skip in query keys
    
    * feat: don't sync if no ids after filtering

--- apps/web/core/state/persistence.tsx
+++ apps/web/core/state/persistence.tsx
@@ -8,36 +8,32 @@ import { store } from './jotai-store';
 
 export const Persistence = () => {
   React.useEffect(() => {
-    const unsubValues = store.sub(localValuesAtom, async () => {
-      const newValues = store.get(localValuesAtom);
-
-      // Dexie docs recommend putting all batched operations in a transaction scope
-      // https://dexie.org/docs/Tutorial/Best-Practices#5-use-transaction-scopes-whenever-you-plan-to-make-more-than-one-operation
-      db.transaction('rw', db.values, () => {
-        db.values.clear();
-        // Dexie docs recommend returning the last promise used in a transaction.
-        // We don't need to await the promises in a transaction, either.
-        return db.values.bulkPut(newValues.filter(t => !t.hasBeenPublished));
-      });
-    });
-
-    const unsubRelations = store.sub(localRelationsAtom, async () => {
-      const newRelations = store.get(localRelationsAtom);
-
-      // Dexie docs recommend putting all batched operations in a transaction scope
-      // https://dexie.org/docs/Tutorial/Best-Practices#5-use-transaction-scopes-whenever-you-plan-to-make-more-than-one-operation
-      db.transaction('rw', db.relations, () => {
-        db.relations.clear();
-        // Dexie docs recommend returning the last promise used in a transaction.
-        // We don't need to await the promises in a transaction, either.
-        return db.relations.bulkPut(newRelations.filter(r => !r.hasBeenPublished));
-      });
-    });
-
-    return () => {
-      unsubValues();
-      unsubRelations();
-    };
+    // const unsubValues = store.sub(localValuesAtom, async () => {
+    //   const newValues = store.get(localValuesAtom);
+    //   // Dexie docs recommend putting all batched operations in a transaction scope
+    //   // https://dexie.org/docs/Tutorial/Best-Practices#5-use-transaction-scopes-whenever-you-plan-to-make-more-than-one-operation
+    //   db.transaction('rw', db.values, () => {
+    //     db.values.clear();
+    //     // Dexie docs recommend returning the last promise used in a transaction.
+    //     // We don't need to await the promises in a transaction, either.
+    //     return db.values.bulkPut(newValues.filter(t => !t.hasBeenPublished));
+    //   });
+    // });
+    // const unsubRelations = store.sub(localRelationsAtom, async () => {
+    //   const newRelations = store.get(localRelationsAtom);
+    //   // Dexie docs recommend putting all batched operations in a transaction scope
+    //   // https://dexie.org/docs/Tutorial/Best-Practices#5-use-transaction-scopes-whenever-you-plan-to-make-more-than-one-operation
+    //   db.transaction('rw', db.relations, () => {
+    //     db.relations.clear();
+    //     // Dexie docs recommend returning the last promise used in a transaction.
+    //     // We don't need to await the promises in a transaction, either.
+    //     return db.relations.bulkPut(newRelations.filter(r => !r.hasBeenPublished));
+    //   });
+    // });
+    // return () => {
+    //   unsubValues();
+    //   unsubRelations();
+    // };
   }, []);
 
   return null;
