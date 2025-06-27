1e759fc3256cf97a20b3f246a2bc0fdb53d1fc4f
Byron Guina <byron.guina@gmail.com>
2025-06-27 11:16:35 -0700

    fix: prod API url

--- apps/web/core/environment/environment.ts
+++ apps/web/core/environment/environment.ts
@@ -57,7 +57,7 @@ export const options: Record<AppEnv, AppConfig> = {
     chainId: '80451',
     rpc: variables.rpcEndpoint,
     ipfs: IPFS_GATEWAY_PATH,
-    api: 'https://hypergraph-v2-testnet.up.railway.app/graphql',
+    api: 'https://api-testnet.geobrowser.io/graphql',
     bundler: `https://api.pimlico.io/v2/80451/rpc?apikey=${variables.accountAbstractionApiKey}`,
   },
   testnet: {
