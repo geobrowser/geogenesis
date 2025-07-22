919f6796ef29e48c665517608f9da0fdef373ae7
Ivan Velychko <lviv08@gmail.com>
2025-07-22 16:17:35 +0300

    feat: add quering spaces by name (select source section)

--- apps/web/core/hooks/use-spaces-query.ts
+++ apps/web/core/hooks/use-spaces-query.ts
@@ -1,174 +1,80 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Effect, Either } from 'effect';
 
 import { useState } from 'react';
 
-import { Environment } from '~/core/environment';
 import { useDebouncedValue } from '~/core/hooks/use-debounced-value';
-import { graphql } from '~/core/io/subgraph/graphql';
+import { Subgraph } from '~/core/io';
 
-import { SpaceDto } from '../io/dto/spaces';
-import { RemoteSpace } from '../io/v2/v2.schema';
+import { E } from '../sync/orm';
+import { useSyncEngine } from '../sync/use-sync-engine';
 
-type NetworkResult = {
-  spaces: Array<RemoteSpace>;
-};
-
-const mainSpacesIds = JSON.stringify([
-  'b2565802-3118-47be-91f2-e59170735bac',
-  'b42fa1af-1d67-4058-a6f1-4be5d7360caf',
-  'dabe3133-4334-47a0-85c5-f965a3a94d4c',
-]);
-
-// @TODO: Support filtering by page name
-const spacesQuery = (name: string) => `
-  {
-    spaces(
-      filter : 
-        {id: 
-          {in: ${mainSpacesIds}}
-        }
-    ) {
-      id
-      type
-      daoAddress
-      spaceAddress
-      mainVotingAddress
-      membershipAddress
-      personalAddress
-
-      membersList {
-        address
-      }
-
-      editorsList {
-        address
-      }
-
-      page  {
-        id
-        name
-        description
-        spaceIds
-
-        types {
-          id
-          name
-        }
-
-        valuesList {
-          spaceId
-          property {
-            id
-            name
-            dataType
-            renderableType
-            relationValueTypes {
-              id
-              name
-            }
-          }
-          value
-          language
-          unit
-        }
-
-        relationsList {
-          id
-          spaceId
-          position
-          verified
-          entityId
-
-          fromEntity {
-            id
-            name
-          }
-
-          toEntity {
-            id
-            name
-            types {
-              id
-              name
-            }
-            valuesList {
-              propertyId
-              value
-            }
-          }
-
-          toSpaceId
-
-          type {
-            id
-            name
-            renderableType
-          }
-        }
-      }
-    }
-  }
-`;
+const filterByTypes = ['362c1dbd-dc64-44bb-a3c4-652f38a642d7']; //Filter only space type entities
 
 export function useSpacesQuery() {
   const [query, setQuery] = useState('');
   const debouncedQuery = useDebouncedValue(query, 200);
 
-  const { data } = useQuery({
-    queryKey: ['spaces-by-name', debouncedQuery],
-    queryFn: async ({ signal }) => {
-      const queryEffect = graphql<NetworkResult>({
-        query: spacesQuery(debouncedQuery),
-        endpoint: Environment.getConfig().api,
-        signal,
-      });
+  const { store } = useSyncEngine();
+  const cache = useQueryClient();
 
-      const resultOrError = await Effect.runPromise(Effect.either(queryEffect));
+  const { data: fuzzyMatchedSpaces = [] } = useQuery({
+    queryKey: ['spaces-by-name', debouncedQuery],
+    queryFn: async () => {
+      const fetchResultsEffect = Effect.either(
+        Effect.tryPromise({
+          try: async () =>
+            await E.findFuzzy({
+              store,
+              cache,
+              where: {
+                name: {
+                  fuzzy: debouncedQuery,
+                },
+                types: filterByTypes?.map(t => {
+                  return {
+                    id: {
+                      equals: t,
+                    },
+                  };
+                }),
+              },
+              first: 10,
+              skip: 0,
+            }),
+          catch: error => {
+            console.error('error', error);
+            return new Subgraph.Errors.AbortError();
+          },
+        })
+      );
+
+      const resultOrError = await Effect.runPromise(fetchResultsEffect);
 
       if (Either.isLeft(resultOrError)) {
-        console.log(resultOrError);
         const error = resultOrError.left;
 
         switch (error._tag) {
           case 'AbortError':
-            // Right now we re-throw AbortErrors and let the callers handle it. Eventually we want
-            // the caller to consume the error channel as an effect. We throw here the typical JS
-            // way so we don't infect more of the codebase with the effect runtime.
-            throw error;
-          case 'GraphqlRuntimeError':
-            console.error(
-              `Encountered runtime graphql error in spaces-by-name.
-
-              queryString: ${spacesQuery(query)}
-              `,
-              error.message
-            );
-
-            return {
-              spaces: [],
-            };
-
+            console.log(`abort error`);
+            return [];
           default:
-            console.error(`${error._tag}: Unable to fetch spaces in spaces-by-name`);
-
-            return {
-              spaces: [],
-            };
+            console.error('useSearch error:', String(error));
+            throw error;
         }
       }
 
-      return {
-        spaces: resultOrError.right.spaces
-          .map(rSpace => SpaceDto(rSpace))
-          .filter(item => item.entity.name?.toLowerCase().includes(debouncedQuery.toLowerCase())),
-      };
+      return resultOrError.right;
     },
   });
 
-  if (!data) {
+  const spaces = fuzzyMatchedSpaces.map(space => {
+    return { id: space.spaces[0].spaceId, name: space.name, image: space.spaces[0].image };
+  });
+
+  if (!fuzzyMatchedSpaces) {
     return {
       query,
       setQuery,
@@ -179,6 +85,6 @@ export function useSpacesQuery() {
   return {
     query,
     setQuery,
-    spaces: data.spaces,
+    spaces,
   };
 }
