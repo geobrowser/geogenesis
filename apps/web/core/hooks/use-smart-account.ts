da314155f76e92310cf8bbaf21f7d4f0cf8b842e
Byron Guina <byron.guina@gmail.com>
2025-09-10 10:56:29 -0700

    feat: migrate use-smart-account

--- apps/web/core/hooks/use-smart-account.ts
+++ apps/web/core/hooks/use-smart-account.ts
@@ -1,14 +1,8 @@
 'use client';
 
+import { generateSmartAccount, useWalletClient } from '@geogenesis/auth';
 import { useQuery } from '@tanstack/react-query';
-import { createSmartAccountClient } from 'permissionless';
-import { ToSafeSmartAccountParameters, toSafeSmartAccount } from 'permissionless/accounts';
-import { createPimlicoClient } from 'permissionless/clients/pimlico';
 import { useCookies } from 'react-cookie';
-import { createPublicClient, http } from 'viem';
-import { entryPoint07Address } from 'viem/account-abstraction';
-
-import { useWalletClient } from 'wagmi';
 
 import { Cookie, WALLET_ADDRESS } from '../cookie';
 import { Environment } from '../environment';
@@ -27,57 +21,11 @@ export function useSmartAccount() {
 
       const config = Environment.getConfig();
 
-      const transport = http(config.rpc);
-      const publicClient = createPublicClient({
-        transport,
+      const smartAccount = await generateSmartAccount({
+        bundlerUrl: config.bundler,
+        rpcUrl: config.rpc,
         chain: GEOGENESIS,
-      });
-
-      const safeAccountParams: ToSafeSmartAccountParameters<'0.7', undefined> = {
-        client: publicClient,
-        owners: [walletClient],
-        entryPoint: {
-          address: entryPoint07Address,
-          version: '0.7',
-        },
-        version: '1.4.1',
-      };
-
-      // TESTNET
-      if (GEOGENESIS.id === 19411) {
-        // Custom SAFE Addresses
-        // TODO: remove this once we have the smart sessions module deployed on testnet
-        // (and the canonical addresses are deployed)
-        safeAccountParams.safeModuleSetupAddress = '0x2dd68b007B46fBe91B9A7c3EDa5A7a1063cB5b47';
-        safeAccountParams.safe4337ModuleAddress = '0x75cf11467937ce3F2f357CE24ffc3DBF8fD5c226';
-        safeAccountParams.safeProxyFactoryAddress = '0xd9d2Ba03a7754250FDD71333F444636471CACBC4';
-        safeAccountParams.safeSingletonAddress = '0x639245e8476E03e789a244f279b5843b9633b2E7';
-        safeAccountParams.multiSendAddress = '0x7B21BBDBdE8D01Df591fdc2dc0bE9956Dde1e16C';
-        safeAccountParams.multiSendCallOnlyAddress = '0x32228dDEA8b9A2bd7f2d71A958fF241D79ca5eEC';
-      }
-
-      const safeAccount = await toSafeSmartAccount(safeAccountParams);
-
-      const bundlerTransport = http(config.bundler);
-      const paymasterClient = createPimlicoClient({
-        transport: bundlerTransport,
-        chain: GEOGENESIS,
-        entryPoint: {
-          address: entryPoint07Address,
-          version: '0.7',
-        },
-      });
-
-      const smartAccount = createSmartAccountClient({
-        chain: GEOGENESIS,
-        account: safeAccount,
-        paymaster: paymasterClient,
-        bundlerTransport,
-        userOperation: {
-          estimateFeesPerGas: async () => {
-            return (await paymasterClient.getUserOperationGasPrice()).fast;
-          },
-        },
+        walletClient,
       });
 
       if (!cookies.walletAddress || cookies.walletAddress !== smartAccount.account.address) {
--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,5 +1,6 @@
 export * from "@privy-io/react-auth";
 export * from "@privy-io/wagmi";
+export { useWalletClient } from 'wagmi'
 
 export { generateSmartAccount } from "./src/account.js";
 export { getGeoChain } from "./src/chain.js";
