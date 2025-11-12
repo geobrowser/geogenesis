d3b3e1b3df5f2b0149f90ccc81ef5182b5ad1687
Bryan <32337174+b-d055@users.noreply.github.com>
2025-11-12 15:44:13 -0500

    fix: add shadow to search component and fix radius difference (#1325)
    Co-authored-by: Ivan Velychko <lviv08@gmail.com>

--- apps/web/design-system/select-entity.tsx
+++ apps/web/design-system/select-entity.tsx
@@ -274,7 +274,7 @@ export const SelectEntity = ({
             <div className={cx(variant === 'fixed' && 'pt-1', width === 'full' && 'w-full')}>
               <div
                 className={cx(
-                  '-ml-px overflow-hidden rounded-lg border border-grey-02 bg-white shadow-lg',
+                  '-ml-px overflow-hidden rounded-md border border-grey-02 bg-white shadow-lg',
                   width === 'clamped' ? 'w-[400px]' : '-mr-px',
                   withSearchIcon && 'rounded-t-none'
                 )}
@@ -648,7 +648,7 @@ const containerStyles = cva('relative', {
       full: 'w-full',
     },
     floating: {
-      true: 'rounded-md border border-divider bg-white',
+      true: 'rounded-md border border-divider bg-white shadow-lg',
     },
     isQueried: {
       true: 'rounded-b-none',
