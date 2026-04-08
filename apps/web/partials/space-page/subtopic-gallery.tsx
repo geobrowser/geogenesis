485fe4970d147b03c4e8a1b706d387988f371510
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2026-04-08 18:03:11 +0000

    fix: subtopics

--- apps/web/partials/space-page/subtopic-gallery.tsx
+++ apps/web/partials/space-page/subtopic-gallery.tsx
@@ -1,5 +1,6 @@
 import { PLACEHOLDER_SPACE_IMAGE } from '~/core/constants';
 import type { TopicUsage } from '~/core/io/subgraph/topic-space-usage';
+import { Spaces } from '~/core/utils/space';
 import { NavUtils } from '~/core/utils/utils';
 
 import { GeoImage } from '~/design-system/geo-image';
@@ -17,28 +18,35 @@ function getSubtopicCardImage(subtopic: TopicUsage) {
 }
 
 export function SubtopicGallery({ spaceId, subtopics }: SubtopicGalleryProps) {
-  if (subtopics.length === 0) {
+  const subspaces = subtopics.filter(s => s.spacesCount > 0);
+
+  if (subspaces.length === 0) {
     return null;
   }
 
   return (
     <>
-      <Slider label="Subtopics" labelClassName="text-mediumTitle font-medium">
-        {subtopics.map(subtopic => (
-          <Link key={subtopic.id} href={NavUtils.toEntity(spaceId, subtopic.id)} className="group">
-            <div className="relative aspect-2/1 w-full overflow-clip rounded-[12px] bg-grey-01">
-              <GeoImage
-                value={getSubtopicCardImage(subtopic)}
-                className="transition-transform duration-150 ease-in-out group-hover:scale-105"
-                style={{ objectFit: 'cover' }}
-                priority
-                fill
-                alt={subtopic.name}
-              />
-            </div>
-            <div className="mt-3 px-1.5 text-smallTitle font-medium text-text">{subtopic.name}</div>
-          </Link>
-        ))}
+      <Slider label="Subspaces" labelClassName="text-mediumTitle font-medium">
+        {subspaces.map(subtopic => {
+          const topSpaceId = Spaces.getTopRankedSpaceId(subtopic.spaces.map(s => s.id));
+          const href = topSpaceId ? NavUtils.toSpace(topSpaceId) : NavUtils.toEntity(spaceId, subtopic.id);
+
+          return (
+            <Link key={subtopic.id} href={href} className="group">
+              <div className="relative aspect-2/1 w-full overflow-clip rounded-[12px] bg-grey-01">
+                <GeoImage
+                  value={getSubtopicCardImage(subtopic)}
+                  className="transition-transform duration-150 ease-in-out group-hover:scale-105"
+                  style={{ objectFit: 'cover' }}
+                  priority
+                  fill
+                  alt={subtopic.name}
+                />
+              </div>
+              <div className="mt-3 px-1.5 text-smallTitle font-medium text-text">{subtopic.name}</div>
+            </Link>
+          );
+        })}
       </Slider>
       <Spacer height={40} />
     </>
