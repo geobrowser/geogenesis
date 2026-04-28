711551667cb55ac1db74bcab019110289c96f10b
Preston Mantel <preston.mantel@gmail.com>
2026-04-28 16:31:25 -0700

    fix: update data block picker icons

--- apps/web/design-system/icons/editor-collection.tsx
+++ apps/web/design-system/icons/editor-collection.tsx
@@ -1,18 +1,18 @@
 export const EditorCollection = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" opacity="0.72">
-    <rect x="1.25" y="1.25" width="13.5" height="13.5" rx="3.75" stroke="currentColor" strokeWidth="1.5" />
+    <rect x="2.75" y="2.75" width="10.5" height="10.5" rx="2.25" stroke="currentColor" strokeWidth="1.5" />
     <path
-      d="M4.25 4.75C4.25 3.92157 4.92157 3.25 5.75 3.25H6.35C6.35 4.07843 5.67843 4.75 4.85 4.75H4.25Z"
+      d="M5.25 6.25C5.25 5.69772 5.69772 5.25 6.25 5.25H7C7 5.80228 6.55228 6.25 6 6.25H5.25Z"
       stroke="currentColor"
-      strokeWidth="1.5"
+      strokeWidth="1.25"
       strokeLinejoin="round"
     />
-    <circle cx="10.35" cy="4.95" r="1.35" stroke="currentColor" strokeWidth="1.5" />
-    <path d="M5 11.75L3.7 9.5L6.3 9.5L5 11.75Z" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
+    <circle cx="10" cy="5.75" r="1" stroke="currentColor" strokeWidth="1.25" />
+    <path d="M6.25 10.75L5.25 9H7.25L6.25 10.75Z" stroke="currentColor" strokeWidth="1.25" strokeLinejoin="round" />
     <path
-      d="M9.25 9.25H11.95C11.95 10.7409 10.7409 11.95 9.25 11.95V9.25Z"
+      d="M9.25 9H11C11 9.9665 10.2165 10.75 9.25 10.75V9Z"
       stroke="currentColor"
-      strokeWidth="1.5"
+      strokeWidth="1.25"
       strokeLinejoin="round"
     />
   </svg>
--- apps/web/design-system/icons/editor-query.tsx
+++ apps/web/design-system/icons/editor-query.tsx
@@ -1,11 +1,9 @@
 export const EditorQuery = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" opacity="0.72">
-    <rect x="1.25" y="1.25" width="13.5" height="13.5" rx="3.75" stroke="currentColor" strokeWidth="1.5" />
-    <path d="M1.85 4.65H14.15" stroke="currentColor" strokeWidth="1.5" />
-    <path d="M1.85 7.35H14.15" stroke="currentColor" strokeWidth="1.5" />
-    <path d="M4.65 1.85V14.15" stroke="currentColor" strokeWidth="1.5" />
-    <path d="M7.35 1.85V14.15" stroke="currentColor" strokeWidth="1.5" />
-    <circle cx="8.6" cy="8.6" r="1.7" stroke="currentColor" strokeWidth="1.5" />
-    <path d="M9.85 9.85L11.4 11.4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+    <rect x="2.75" y="2.75" width="8.5" height="8.5" rx="1.75" stroke="currentColor" strokeWidth="1.5" />
+    <path d="M5.25 5.75H8.75" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" />
+    <path d="M5.25 8.25H7.25" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" />
+    <circle cx="10.25" cy="10.25" r="2.25" fill="white" stroke="currentColor" strokeWidth="1.5" />
+    <path d="M11.875 11.875L13.5 13.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
   </svg>
 );
