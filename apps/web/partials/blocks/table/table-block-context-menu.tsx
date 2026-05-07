f3ddcb6400fb046d75e1b9f2accc24953a3725d1
Preston Mantel <preston.mantel@gmail.com>
2026-05-07 10:17:10 -0700

    chore(data-block): rename context menu labels
    Rename 'View config' to 'Go to block' and 'View block relation' to 'Go to
    relation' for clarity in the data block ... menu.

--- apps/web/partials/blocks/table/table-block-context-menu.tsx
+++ apps/web/partials/blocks/table/table-block-context-menu.tsx
@@ -112,13 +112,13 @@ export function TableBlockContextMenu() {
               </MenuItem>
               <MenuItem href={NavUtils.toEntity(spaceId, entityId)}>
                 <div className="flex w-full items-center justify-between gap-2">
-                  <span>View config</span>
+                  <span>Go to block</span>
                   <Cog />
                 </div>
               </MenuItem>
               <MenuItem href={NavUtils.toEntity(spaceId, relationId)}>
                 <div className="flex w-full items-center justify-between gap-2">
-                  <span>View block relation</span>
+                  <span>Go to relation</span>
                   <Relation />
                 </div>
               </MenuItem>
