92d7abcb30f02ca9115eeabc1ecb9f1dc579f8af
Preston Mantel <preston.mantel@gmail.com>
2026-06-12 11:22:09 -0700

    fix(entity-menu): make Switch icon arrows horizontal

--- apps/web/design-system/icons/switch.tsx
+++ apps/web/design-system/icons/switch.tsx
@@ -11,10 +11,10 @@ export function Switch({ color }: Props) {
 
   return (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M3.5 0.5L3.5 14.5" stroke={themeColor} strokeLinecap="round" />
-      <path d="M0.5 12.25L3.5 15.25L6.5 12.25" stroke={themeColor} strokeLinecap="round" />
-      <path d="M12.5 1.5L12.5 15.5" stroke={themeColor} strokeLinecap="round" />
-      <path d="M9.5 4.5L12.5 1.5L15.5 4.5" stroke={themeColor} strokeLinecap="round" />
+      <path d="M1.5 3.5L15.5 3.5" stroke={themeColor} strokeLinecap="round" />
+      <path d="M12.5 0.5L15.5 3.5L12.5 6.5" stroke={themeColor} strokeLinecap="round" />
+      <path d="M0.5 12.5L14.5 12.5" stroke={themeColor} strokeLinecap="round" />
+      <path d="M3.5 9.5L0.5 12.5L3.5 15.5" stroke={themeColor} strokeLinecap="round" />
     </svg>
   );
 }
