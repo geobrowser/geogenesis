1decd46d0ccc2b0523b94b720f6b4a4148189f43
Juan Mardikian <developermardikian@gmail.com>
2026-06-11 22:55:43 -0400

    feat: resolve conflicts

--- apps/web/app/space/(entity)/[id]/[entityId]/ranking-compose/page.tsx
+++ apps/web/app/space/(entity)/[id]/[entityId]/ranking-compose/page.tsx
@@ -31,8 +31,6 @@ export default function RankingComposePage() {
   const rankingEndDate = searchParams?.get('rankingEndDate') ?? '';
   const mode: RankingComposeMode = searchParams?.get('mode') === 'view' ? 'view' : 'edit';
 
-  useBrowseSidebarFullscreenRoute();
-
   const { hasValidParams, isLoading, parentEntityId, blocks, blockRelations } = useRankingComposePage({
     spaceId,
     blockEntityId: dataBlockEntityId,
