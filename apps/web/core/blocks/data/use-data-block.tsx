7ae0d80e4523ece56ce681cd042f36aa2027c7c0
Preston Mantel <preston.mantel@gmail.com>
2026-07-20 21:28:48 -0700

    fix: include unscored entities in score sorts

--- apps/web/core/blocks/data/use-data-block.tsx
+++ apps/web/core/blocks/data/use-data-block.tsx
@@ -10,7 +10,7 @@ import { WhereCondition } from '~/core/sync/experimental_query-layer';
 import { useMutate } from '~/core/sync/use-mutate';
 import { useQueryEntities, useQueryEntity } from '~/core/sync/use-store';
 import { Cell, Property, Row } from '~/core/types';
-import { propertyForSort } from '~/core/utils/column-sort';
+import { propertyForSort, shouldIncludeWithoutValueForPropertySort } from '~/core/utils/column-sort';
 import { sortRows } from '~/core/utils/utils';
 
 import { useProperties } from '../../hooks/use-properties';
@@ -144,6 +144,7 @@ export function useDataBlock(options?: UseDataBlockOptions) {
       propertyId: sortState.columnId,
       direction: sortState.direction,
       dataType: property?.dataType?.toLowerCase(),
+      includeWithoutValue: shouldIncludeWithoutValueForPropertySort(sortState.columnId),
     };
   }, [sortState, propertiesSchema, filterableProperties]);
 
