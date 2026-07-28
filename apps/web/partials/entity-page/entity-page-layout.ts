41f8dd082689d7e273c3ad9939b854568b71b3b0
jwalkingjew <preston.mantel@gmail.com>
2026-07-27 17:39:27 -0700

    fix: restore readable entity content widths (#2017)
    * fix: restore readable entity content widths
    
    * fix: align space header with sidebar layout

---
+++ apps/web/partials/entity-page/entity-page-layout.ts
@@ -0,0 +1,5 @@
+export const ENTITY_PAGE_COVER_MAX_WIDTH = 1192;
+export const ENTITY_PAGE_CONTENT_MAX_WIDTH = 900;
+export const ENTITY_PAGE_WITH_SIDEBAR_MAX_WIDTH = 1142;
+
+export type EntityPageContentVariant = 'content' | 'with-sidebar';
